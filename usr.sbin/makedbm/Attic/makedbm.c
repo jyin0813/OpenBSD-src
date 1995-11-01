e81910dc84f4228031d23cbbdfcabc4da129dacf
deraadt <deraadt@openbsd.org>
1995-11-01 23:55:41 +0000

    + full set of ypserv tools that compile

--- usr.sbin/makedbm/Attic/makedbm.c
+++ usr.sbin/makedbm/Attic/makedbm.c
@@ -28,7 +28,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$Id: makedbm.c,v 1.6 1994/07/30 21:20:18 moj Exp $";
+static char rcsid[] = "$Id: makedbm.c,v 1.1 1995/10/23 07:46:14 deraadt Exp deraadt $";
 #endif
 
 #include <stdio.h>

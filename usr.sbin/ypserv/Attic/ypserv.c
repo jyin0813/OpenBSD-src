25cc8d42c6ae083060888c906107daaa12df2fc0
deraadt <deraadt@openbsd.org>
1995-11-01 23:56:12 +0000

    + full set of ypserv tools that compile

--- usr.sbin/ypserv/Attic/ypserv.c
+++ usr.sbin/ypserv/Attic/ypserv.c
@@ -28,7 +28,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$Id: ypserv.c,v 1.8 1994/08/29 19:07:00 moj Exp $";
+static char rcsid[] = "$Id: ypserv.c,v 1.1 1995/10/23 07:46:44 deraadt Exp deraadt $";
 #endif
 
 #include <stdio.h>

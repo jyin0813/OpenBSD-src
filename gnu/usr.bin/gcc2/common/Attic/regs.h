c23aee7579fd305397c4f1860e1d4c0dbe3afa96
niklas <niklas@openbsd.org>
1996-01-12 05:31:57 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/common/Attic/regs.h
+++ gnu/usr.bin/gcc2/common/Attic/regs.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
 
-	$Id: regs.h,v 1.2 1993/08/02 17:35:34 mycroft Exp $
+	$Id: regs.h,v 1.1 1995/10/18 08:39:43 deraadt Exp niklas $
 */
 
 

fda60c58393d923fdbbcd1253464bc5deab4ebdf
niklas <niklas@openbsd.org>
1996-01-12 05:29:07 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/arch/sparc/Attic/tm.h
+++ gnu/usr.bin/gcc2/arch/sparc/Attic/tm.h
@@ -1,5 +1,5 @@
 /* Configuration for NetBSD Sparc */
-/* $Id: tm.h,v 1.9 1995/06/05 01:59:30 pk Exp $ */
+/* $Id: tm.h,v 1.1 1995/10/18 08:39:26 deraadt Exp niklas $ */
 
 #include "sparc/sparc.h"
 

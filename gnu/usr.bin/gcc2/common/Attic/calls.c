00cdcb4da8ef4c9071de8a9a69d1dced6bc0f803
niklas <niklas@openbsd.org>
1996-01-12 05:30:46 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/common/Attic/calls.c
+++ gnu/usr.bin/gcc2/common/Attic/calls.c
@@ -18,7 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: calls.c,v 1.2 1993/08/02 17:33:37 mycroft Exp $";
+static char rcsid[] = "$Id: calls.c,v 1.1 1995/10/18 08:39:35 deraadt Exp niklas $";
 #endif /* not lint */
 
 #include "config.h"

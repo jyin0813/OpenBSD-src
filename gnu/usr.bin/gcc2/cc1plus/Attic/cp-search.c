cb362376dabe71723f1027cda1fd0e3745253dea
niklas <niklas@openbsd.org>
1996-01-12 05:30:24 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/cc1plus/Attic/cp-search.c
+++ gnu/usr.bin/gcc2/cc1plus/Attic/cp-search.c
@@ -20,7 +20,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: cp-search.c,v 1.2 1993/08/02 17:32:11 mycroft Exp $";
+static char rcsid[] = "$Id: cp-search.c,v 1.1 1995/10/18 08:39:33 deraadt Exp niklas $";
 #endif /* not lint */
 
 #if 0

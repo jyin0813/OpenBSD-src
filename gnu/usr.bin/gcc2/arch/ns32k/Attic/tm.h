707f54de1ee7fe695bec698457e07500be0025b1
niklas <niklas@openbsd.org>
1996-01-12 05:28:39 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/arch/ns32k/Attic/tm.h
+++ gnu/usr.bin/gcc2/arch/ns32k/Attic/tm.h
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: tm.h,v 1.6 1995/06/05 01:59:27 pk Exp $
+	$Id: tm.h,v 1.1 1995/10/18 08:39:23 deraadt Exp niklas $
 */
 
 #if 0

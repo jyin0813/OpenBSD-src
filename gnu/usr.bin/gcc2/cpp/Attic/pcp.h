833820a342ad2bfac6acdd772605b78c66ffc985
niklas <niklas@openbsd.org>
1996-01-12 05:32:41 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/cpp/Attic/pcp.h
+++ gnu/usr.bin/gcc2/cpp/Attic/pcp.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
 
-	$Id: pcp.h,v 1.2 1993/08/02 17:36:38 mycroft Exp $
+	$Id: pcp.h,v 1.1 1995/10/18 08:39:48 deraadt Exp niklas $
 */
 
 

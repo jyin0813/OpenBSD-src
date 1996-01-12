51d7d303e9e808e7a614e23b3184926796f0ce60
niklas <niklas@openbsd.org>
1996-01-12 02:41:15 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/arch/i386/Attic/config.h
+++ gnu/usr.bin/gcc2/arch/i386/Attic/config.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
 
-	$Id: config.h,v 1.2 1993/10/03 13:57:56 pk Exp $
+	$Id: config.h,v 1.1 1995/10/18 08:39:17 deraadt Exp niklas $
 */
 
 #ifndef i386

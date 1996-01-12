12863c0f77cc879c73770ff295ad81687ed0e86d
niklas <niklas@openbsd.org>
1996-01-12 05:29:40 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/cc1obj/Attic/objc-act.c
+++ gnu/usr.bin/gcc2/cc1obj/Attic/objc-act.c
@@ -19,7 +19,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: objc-act.c,v 1.2 1993/08/02 17:30:37 mycroft Exp $";
+static char rcsid[] = "$Id: objc-act.c,v 1.1 1995/10/18 08:39:29 deraadt Exp niklas $";
 #endif /* not lint */
 
 /* Purpose: This module implements the Objective-C 4.0 language.

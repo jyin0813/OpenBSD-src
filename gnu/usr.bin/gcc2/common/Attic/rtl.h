6eb27ae691f08d471c65bd21dbfad2267be770f8
niklas <niklas@openbsd.org>
1996-01-12 05:32:09 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/common/Attic/rtl.h
+++ gnu/usr.bin/gcc2/common/Attic/rtl.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
 
-	$Id: rtl.h,v 1.2 1993/08/02 17:35:58 mycroft Exp $
+	$Id: rtl.h,v 1.1 1995/10/18 08:39:45 deraadt Exp niklas $
 */
 
 

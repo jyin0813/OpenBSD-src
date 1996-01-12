cf1f95653e2601a73f8cbf9e39f080b074d8e6b2
niklas <niklas@openbsd.org>
1996-01-12 05:32:31 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/common/Attic/xcoffout.c
+++ gnu/usr.bin/gcc2/common/Attic/xcoffout.c
@@ -18,7 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: xcoffout.c,v 1.2 1993/08/02 17:36:25 mycroft Exp $";
+static char rcsid[] = "$Id: xcoffout.c,v 1.1 1995/10/18 08:39:47 deraadt Exp niklas $";
 #endif /* not lint */
 
 /* Output xcoff-format symbol table data.  The main functionality is contained

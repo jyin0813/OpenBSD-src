e20dd6dcfabcbf7962cd4424eb6bd695df01c00b
niklas <niklas@openbsd.org>
1996-01-12 05:29:23 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/cc1/Attic/c-iterate.c
+++ gnu/usr.bin/gcc2/cc1/Attic/c-iterate.c
@@ -18,7 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: c-iterate.c,v 1.2 1993/08/02 18:26:02 mycroft Exp $";
+static char rcsid[] = "$Id: c-iterate.c,v 1.1 1995/10/18 08:39:27 deraadt Exp niklas $";
 #endif /* not lint */
 
 /* This file is part of the C front end.

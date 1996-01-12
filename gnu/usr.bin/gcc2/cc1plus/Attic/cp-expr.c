c23d89189d19d47ec2c7c9677a31ab6751e2ccc2
niklas <niklas@openbsd.org>
1996-01-12 05:30:05 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/cc1plus/Attic/cp-expr.c
+++ gnu/usr.bin/gcc2/cc1plus/Attic/cp-expr.c
@@ -19,7 +19,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: cp-expr.c,v 1.2 1993/08/02 17:31:45 mycroft Exp $";
+static char rcsid[] = "$Id: cp-expr.c,v 1.1 1995/10/18 08:39:31 deraadt Exp niklas $";
 #endif /* not lint */
 
 #include "config.h"

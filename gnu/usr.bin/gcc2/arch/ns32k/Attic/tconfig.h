670ae9dc74a1520c09cae5acf9a869ed8b959ed1
niklas <niklas@openbsd.org>
1996-01-12 05:28:38 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/arch/ns32k/Attic/tconfig.h
+++ gnu/usr.bin/gcc2/arch/ns32k/Attic/tconfig.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: tconfig.h,v 1.2 1994/06/09 18:59:09 phil Exp $
+	$Id: tconfig.h,v 1.1 1995/10/18 08:39:23 deraadt Exp niklas $
 */
 
 /* #defines that need visibility everywhere.  */

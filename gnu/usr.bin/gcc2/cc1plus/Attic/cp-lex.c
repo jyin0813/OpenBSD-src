680e400462c36bcee8371dec985b8e94bb15ebf6
niklas <niklas@openbsd.org>
1996-01-12 05:30:11 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/cc1plus/Attic/cp-lex.c
+++ gnu/usr.bin/gcc2/cc1plus/Attic/cp-lex.c
@@ -19,7 +19,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: cp-lex.c,v 1.2 1993/08/02 17:31:53 mycroft Exp $";
+static char rcsid[] = "$Id: cp-lex.c,v 1.1 1995/10/18 08:39:32 deraadt Exp niklas $";
 #endif /* not lint */
 
 /* This file is the lexical analyzer for GNU C++.  */

64f857578505a26ac2205d87601e9d38282a2647
niklas <niklas@openbsd.org>
1996-01-12 05:33:01 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/libobjc/Attic/sarray.c
+++ gnu/usr.bin/gcc2/libobjc/Attic/sarray.c
@@ -18,7 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: sarray.c,v 1.2 1993/08/02 17:39:12 mycroft Exp $";
+static char rcsid[] = "$Id: sarray.c,v 1.1 1995/10/18 08:39:50 deraadt Exp niklas $";
 #endif /* not lint */
 
 /* As a special exception, if you link this library with files

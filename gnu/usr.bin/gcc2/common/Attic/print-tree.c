c059251e71efd50ea11ce589a52cfd05231ba2ab
niklas <niklas@openbsd.org>
1996-01-12 05:31:48 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/common/Attic/print-tree.c
+++ gnu/usr.bin/gcc2/common/Attic/print-tree.c
@@ -18,7 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: print-tree.c,v 1.2 1993/08/02 17:35:22 mycroft Exp $";
+static char rcsid[] = "$Id: print-tree.c,v 1.1 1995/10/18 08:39:42 deraadt Exp niklas $";
 #endif /* not lint */
 
 #include "config.h"

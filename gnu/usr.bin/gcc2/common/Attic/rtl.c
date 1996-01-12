a2f85ef6751eb2a132cf8224b4fe4ee4e39c6966
niklas <niklas@openbsd.org>
1996-01-12 05:32:07 +0000

    Remove obsolete GCC

--- gnu/usr.bin/gcc2/common/Attic/rtl.c
+++ gnu/usr.bin/gcc2/common/Attic/rtl.c
@@ -18,7 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: rtl.c,v 1.2 1993/08/02 17:35:56 mycroft Exp $";
+static char rcsid[] = "$Id: rtl.c,v 1.1 1995/10/18 08:39:45 deraadt Exp niklas $";
 #endif /* not lint */
 
 #include "config.h"

610e33623c639c56334e323be941ecc3f80b8e13
dm <dm@openbsd.org>
1996-02-20 03:54:04 +0000

    netbsd: bind 4.9.3

--- usr.sbin/named/Attic/db_lookup.c
+++ usr.sbin/named/Attic/db_lookup.c
@@ -33,7 +33,7 @@
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)db_lookup.c	4.18 (Berkeley) 3/21/91";*/
-static char rcsid[] = "$Id: db_lookup.c,v 1.2 1993/08/01 17:57:30 mycroft Exp $";
+static char rcsid[] = "$Id: db_lookup.c,v 1.1 1995/10/18 08:47:50 deraadt Exp dm $";
 #endif /* not lint */
 
 /*

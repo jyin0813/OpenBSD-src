5102351ad26d2e3e0b097d98b4d128805783db7c
dm <dm@openbsd.org>
1996-02-20 03:54:36 +0000

    netbsd: bind 4.9.3

--- usr.sbin/named/Attic/storage.c
+++ usr.sbin/named/Attic/storage.c
@@ -23,7 +23,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: storage.c,v 1.2 1993/08/01 17:57:37 mycroft Exp $";
+static char rcsid[] = "$Id: storage.c,v 1.1 1995/10/18 08:47:51 deraadt Exp dm $";
 #endif
 
 #include <sys/param.h>

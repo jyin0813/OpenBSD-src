d890064f8b677f98c3ef59cb821143a839bba8ae
dm <dm@openbsd.org>
1996-02-20 03:55:12 +0000

    netbsd: bind 4.9.3

--- usr.sbin/named/tools/nslookup/Attic/subr.c
+++ usr.sbin/named/tools/nslookup/Attic/subr.c
@@ -33,7 +33,7 @@
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)subr.c	5.24 (Berkeley) 3/2/91";*/
-static char rcsid[] = "$Id: subr.c,v 1.2 1993/08/01 17:58:06 mycroft Exp $";
+static char rcsid[] = "$Id: subr.c,v 1.1 1995/10/18 08:47:56 deraadt Exp dm $";
 #endif /* not lint */
 
 /*

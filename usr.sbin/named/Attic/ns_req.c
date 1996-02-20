a596d9d2e5525fcd3ac4dc3d7ccfcda0fe81bf9c
dm <dm@openbsd.org>
1996-02-20 03:54:27 +0000

    netbsd: bind 4.9.3

--- usr.sbin/named/Attic/ns_req.c
+++ usr.sbin/named/Attic/ns_req.c
@@ -33,7 +33,7 @@
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)ns_req.c	4.47 (Berkeley) 7/1/91";*/
-static char rcsid[] = "$Id: ns_req.c,v 1.2 1993/08/01 17:57:43 mycroft Exp $";
+static char rcsid[] = "$Id: ns_req.c,v 1.1 1995/10/18 08:47:50 deraadt Exp dm $";
 #endif /* not lint */
 
 #include <sys/param.h>

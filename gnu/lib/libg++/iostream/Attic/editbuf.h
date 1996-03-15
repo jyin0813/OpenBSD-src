8298f82edc5694a5aec6ea5d82d394fcaac7e72d
etheisen <etheisen@openbsd.org>
1996-03-15 13:44:06 +0000

    Removed for 2.7.1 import.

--- gnu/lib/libg++/iostream/Attic/editbuf.h
+++ gnu/lib/libg++/iostream/Attic/editbuf.h
@@ -14,7 +14,7 @@
 //    License along with this library; if not, write to the Free
 //    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //
-//	$Id: editbuf.h,v 1.2 1993/08/02 17:22:30 mycroft Exp $
+//	$Id: editbuf.h,v 1.1 1995/10/18 08:38:12 deraadt Exp etheisen $
 
 #ifndef _EDITBUF_H
 #define _EDITBUF_H

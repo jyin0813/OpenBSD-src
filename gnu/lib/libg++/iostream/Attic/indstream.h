344b3c0f5bb4d75aa3bce72810d710995da168b8
etheisen <etheisen@openbsd.org>
1996-03-15 13:44:15 +0000

    Removed for 2.7.1 import.

--- gnu/lib/libg++/iostream/Attic/indstream.h
+++ gnu/lib/libg++/iostream/Attic/indstream.h
@@ -15,7 +15,7 @@
 //    License along with this library; if not, write to the Free
 //    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //
-//	$Id: indstream.h,v 1.2 1993/08/02 17:22:33 mycroft Exp $
+//	$Id: indstream.h,v 1.1 1995/10/18 08:38:13 deraadt Exp etheisen $
 
 #ifndef _INDSTREAM_H
 #define _INDSTREAM_H

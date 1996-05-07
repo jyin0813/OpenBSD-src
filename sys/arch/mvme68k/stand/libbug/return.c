d782c5ef40f91c01f39fcbb80b9f5ae7a9b18178
deraadt <deraadt@openbsd.org>
1996-05-07 17:25:11 +0000

    idea by chuck to make libbug MI

---
+++ sys/arch/mvme68k/stand/libbug/return.c
@@ -0,0 +1,16 @@
+/*	$OpenBSD$ */
+
+/*
+ * bug routines -- assumes that the necessary sections of memory
+ * are preserved.
+ */
+#include <sys/types.h>
+#include <machine/prom.h>
+
+/* BUG - return to bug routine */
+void
+mvmeprom_return()
+{
+	MVMEPROM_CALL(MVMEPROM_EXIT);
+	/*NOTREACHED*/
+}

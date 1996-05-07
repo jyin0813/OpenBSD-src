1643be2e2b5c7b9a6996396320c73e0ef11a98d4
deraadt <deraadt@openbsd.org>
1996-05-07 17:25:12 +0000

    idea by chuck to make libbug MI

---
+++ sys/arch/mvme68k/stand/libbug/rtc_rd.c
@@ -0,0 +1,16 @@
+/*	$OpenBSD$ */
+
+/*
+ * bug routines -- assumes that the necessary sections of memory
+ * are preserved.
+ */
+#include <sys/types.h>
+#include <machine/prom.h>
+
+void
+mvmeprom_rtc_rd(ptime)
+	struct mvmeprom_time *ptime;
+{
+	MVMEPROM_ARG1(ptime);
+	MVMEPROM_CALL(MVMEPROM_RTC_RD);
+}

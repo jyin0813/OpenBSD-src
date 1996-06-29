cccb239313f3bef6ee0d9c2f944d623c1e273ea3
tholo <tholo@openbsd.org>
1996-06-29 11:52:17 +0000

    Use tar(1) emulation as provided by pax(1)

--- gnu/usr.bin/tar/Attic/update.c
+++ gnu/usr.bin/tar/Attic/update.c
@@ -18,7 +18,7 @@ along with GNU Tar; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: update.c,v 1.4 1994/03/27 09:27:38 cgd Exp $";
+static char rcsid[] = "$Id: update.c,v 1.1 1995/10/18 08:41:10 deraadt Exp tholo $";
 #endif /* not lint */
 
 /* JF implement the 'r' 'u' and 'A' options for tar. */

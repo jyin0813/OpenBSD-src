ce6933292da5c3019d9aee322a976b984bd9daea
pefo <pefo@openbsd.org>
1996-08-31 03:32:14 +0000

    The pica tree is now obsolete and removed. Use arch/arc.

--- sys/arch/pica/include/Attic/trap.h
+++ sys/arch/pica/include/Attic/trap.h
@@ -1,4 +1,4 @@
-/*      $OpenBSD: trap.h,v 1.1.1.1 1995/10/18 10:39:15 deraadt Exp $	*/
+/*      $OpenBSD: trap.h,v 1.2 1996/06/06 23:07:18 deraadt Exp pefo $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.

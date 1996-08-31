1b17048429db0be734c0e1b54e947d5c0f1d9c46
pefo <pefo@openbsd.org>
1996-08-31 03:32:12 +0000

    The pica tree is now obsolete and removed. Use arch/arc.

--- sys/arch/pica/include/Attic/regdef.h
+++ sys/arch/pica/include/Attic/regdef.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: regdef.h,v 1.4 1994/10/26 21:09:58 cgd Exp $	*/
+/*	$OpenBSD: regdef.h,v 1.2 1996/06/06 23:07:09 deraadt Exp pefo $	*/
 /*	$NetBSD: regdef.h,v 1.4 1994/10/26 21:09:58 cgd Exp $	*/
 
 /*

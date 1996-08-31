a80004735e51e9497c35a9ed532d2dd33b1f8050
pefo <pefo@openbsd.org>
1996-08-31 03:31:57 +0000

    The pica tree is now obsolete and removed. Use arch/arc.

--- sys/arch/pica/dev/Attic/scsi.h
+++ sys/arch/pica/dev/Attic/scsi.h
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)scsi.h	8.1 (Berkeley) 6/10/93
- *      $Id: scsi.h,v 1.3 1994/05/27 08:59:28 glass Exp $
+ *      $Id: scsi.h,v 1.1 1995/10/18 10:39:09 deraadt Exp pefo $
  *
  * scsi.h --
  *
@@ -46,7 +46,7 @@
  *
  * from: Header: /sprite/src/kernel/dev/RCS/scsi.h,
  *	v 9.1 90/02/13 23:11:24 jhh Exp  SPRITE (Berkeley)
- * $Id: scsi.h,v 1.3 1994/05/27 08:59:28 glass Exp $
+ * $Id: scsi.h,v 1.1 1995/10/18 10:39:09 deraadt Exp pefo $
  */
 
 #ifndef _SCSI_H

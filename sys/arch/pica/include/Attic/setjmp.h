96d621efad1c508b54f6236a56c72a59924e6d3d
pefo <pefo@openbsd.org>
1996-08-31 03:32:13 +0000

    The pica tree is now obsolete and removed. Use arch/arc.

--- sys/arch/pica/include/Attic/setjmp.h
+++ sys/arch/pica/include/Attic/setjmp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: setjmp.h,v 1.1 1994/12/20 10:37:05 cgd Exp $	*/
+/*	$OpenBSD: setjmp.h,v 1.2 1996/06/06 23:07:13 deraadt Exp pefo $	*/
 /*	$NetBSD: setjmp.h,v 1.1 1994/12/20 10:37:05 cgd Exp $	*/
 
 /*

98ec7e8183d653e5ac9a67006baeb5183cb85b3c
niklas <niklas@openbsd.org>
1996-11-23 23:28:25 +0000

    Remove accidentally committed dirs from the trunk

--- gnu/usr.bin/binutils/sim/mips/Attic/gencode.c
+++ gnu/usr.bin/binutils/sim/mips/Attic/gencode.c
@@ -13,9 +13,9 @@
    THIS SOFTWARE INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
-   $Revision: 1.1.2.1 $
+   $Revision: 1.2 $
      $Author: niklas $
-       $Date: 1996/11/19 14:42:12 $
+       $Date: 1996/11/23 03:55:12 $
 */
 
 #if 0

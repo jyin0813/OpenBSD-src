c4cde8aa4176cec88df232749158f784277dd4b2
niklas <niklas@openbsd.org>
1996-11-23 23:34:51 +0000

    Remove accidentally committed dirs from the trunk

--- gnu/usr.bin/binutils/texinfo/makeinfo/Attic/multi.c
+++ gnu/usr.bin/binutils/texinfo/makeinfo/Attic/multi.c
@@ -1,5 +1,5 @@
 /* multi.c -- Multitable stuff for makeinfo.
-   $Id: multi.c,v 1.1.2.1 1996/11/19 14:53:02 niklas Exp $
+   $Id: multi.c,v 1.2 1996/11/23 04:03:25 niklas Exp $
 
    Copyright (C) 1996 Free Software Foundation, Inc.
 

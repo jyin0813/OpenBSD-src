97fc7e9342567bfcb383854063c507484132f52c
millert <millert@openbsd.org>
1997-01-13 06:43:05 +0000

    Fix core dump and use libc err(3) instead of private one (which had slightly different semantics).  Also clean up some gcc -Wall'isms.

--- usr.bin/tail/misc.c
+++ usr.bin/tail/misc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: misc.c,v 1.2 1996/06/26 05:40:16 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -40,7 +40,7 @@
 #if 0
 static char sccsid[] = "@(#)misc.c	8.1 (Berkeley) 6/6/93";
 #endif
-static char rcsid[] = "$OpenBSD: misc.c,v 1.3 1994/11/23 07:42:04 jtc Exp $";
+static char rcsid[] = "$OpenBSD: misc.c,v 1.2 1996/06/26 05:40:16 deraadt Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>

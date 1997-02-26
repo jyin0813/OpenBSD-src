4705bd09642de4d02d6f7803cd6c8a3419b71e5e
downsj <downsj@openbsd.org>
1997-02-26 11:40:08 +0000

    These don't belong here.

--- lib/libwrap/Attic/scaffold.c
+++ lib/libwrap/Attic/scaffold.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: scaffold.c,v 1.1 1997/02/26 03:06:56 downsj Exp $	*/
 
  /*
   * Routines for testing only. Not really industrial strength.
@@ -10,7 +10,7 @@
 #if 0
 static char sccs_id[] = "@(#) scaffold.c 1.5 95/01/03 09:13:48";
 #else
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: scaffold.c,v 1.1 1997/02/26 03:06:56 downsj Exp $";
 #endif
 #endif
 

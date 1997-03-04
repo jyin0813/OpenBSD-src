67bd0baba9507a5b14e8056ba47b20cd97b30655
rahnds <rahnds@openbsd.org>
1997-03-04 03:21:12 +0000

    Cleanup after import. This also seems to bring up the current version.

--- sys/arch/mvme88k/include/psl.h
+++ sys/arch/mvme88k/include/psl.h
@@ -1,30 +1,100 @@
+/*
+ * Copyright (c) 1996 Nivas Madhur
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Nivas Madhur.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*
+ * Mach Operating System
+ * Copyright (c) 1991 Carnegie Mellon University
+ * Copyright (c) 1991 OMRON Corporation
+ * All Rights Reserved.
+ *
+ * Permission to use, copy, modify and distribute this software and its
+ * documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ *
+ */
 #ifndef __M88K_M88100_PSL_H__
 #define __M88K_M88100_PSL_H__
 
 /* needs major cleanup - XXX nivas */
 
+#if 0
+spl0 is a function by itself. I really am serious about the clean up
+above...
 #define spl0()		spln(0)
-#define spl1()		spln(1)
-#define spl2()		spln(2)
-#define spl3()		spln(3)
-#define spl4()		spln(4)
-#define spl5()		spln(5)
-#define spl6()		spln(6)
-#define spl7()		spln(7)
+#endif /* 0 */
+#define spl1()		setipl(1)
+#define spl2()		setipl(2)
+#define spl3()		setipl(3)
+#define spl4()		setipl(4)
+#define spl5()		setipl(5)
+#define spl6()		setipl(6)
+#define spl7()		setipl(7)
 
-#define splnone()	spln(0)
-#define splsoftclock()	spln(1)
-#define splnet()	spln(1)
-#define splbio()	spln(3)
-#define splimp()	spln(3)
-#define spltty()	spln(4)
-#define splclock()	spln(6)
-#define splstatclock()	spln(6)
-#define splvm()		spln(6)
-#define splhigh()	spln(7)
-#define splsched()	spln(7)
+/*
+ * IPL levels.
+ * We use 6 as IPL_HIGH so that abort can be programmed at 7 so that
+ * it is always possible to break into the system unless interrupts
+ * are disabled.
+ */
 
-#define splx(x)		spln(x)
+#define IPL_NONE	0
+#define IPL_SOFTCLOCK	1
+#define IPL_SOFTNET	1
+#define IPL_BIO		2
+#define IPL_NET		3
+#define IPL_TTY		3
+#define IPL_CLOCK	5
+#define IPL_STATCLOCK	5
+#define IPL_IMP		6
+#define IPL_VM		6
+#define IPL_HIGH	6
+#define IPL_SCHED	6
+#define IPL_NMI		7
+#define IPL_ABORT	7
+
+#define splnone		spl0
+#define splsoftclock()	setipl(IPL_SOFTCLOCK)
+#define splsoftnet()	setipl(IPL_SOFTNET)
+#define splbio()	setipl(IPL_BIO)
+#define splnet()	setipl(IPL_NET)
+#define spltty()	setipl(IPL_TTY)
+#define splclock()	setipl(IPL_CLOCK)
+#define splstatclock()	setipl(IPL_STATCLOCK)
+#define splimp()	setipl(IPL_IMP)
+#define splvm()		setipl(IPL_VM)
+#define splhigh()	setipl(IPL_HIGH)
+#define splsched()	setipl(IPL_SCHED)
+
+#define splx(x)		((x) ? setipl((x)) : spl0())
 
 /* 
  * 88100 control registers
@@ -95,3 +165,4 @@ struct psr {
 #define XIP_ADDR	0xFFFFFFFCU	/* address mask */
 
 #endif /* __M88K_M88100_PSL_H__ */
+

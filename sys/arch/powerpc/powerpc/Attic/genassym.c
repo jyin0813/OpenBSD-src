32bdbb7cd5a85f84dc62b45a9ddfb9d6ffd6f330
rahnds <rahnds@openbsd.org>
1997-04-19 02:33:11 +0000

    Use the new genassym.cf method of generating the assym.h file.

--- sys/arch/powerpc/powerpc/Attic/genassym.c
+++ sys/arch/powerpc/powerpc/Attic/genassym.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: genassym.c,v 1.2 1996/12/28 06:22:00 rahnds Exp $	*/
+/*	$OpenBSD: genassym.c,v 1.3 1997/01/09 03:07:15 rahnds Exp $	*/
 /*	$NetBSD: genassym.c,v 1.1 1996/09/30 16:34:46 ws Exp $	*/
 
 /*

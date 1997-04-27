a723d5f8561b5e34e5d5fb9cb583e1a18e4318c2
downsj <downsj@openbsd.org>
1997-04-27 19:01:29 +0000

    Pull in more includes to properly prototype library functions, kill local decls of library functions, kill local implementation of setproctitle().
    This of course points out how utterly bad this code is and that someone needs
    to come in here with a chainsaw sometime.

--- usr.bin/rdist/Attic/setargs.c
+++ usr.bin/rdist/Attic/setargs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: setargs.c,v 1.3 1996/06/26 05:38:18 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1983 Regents of the University of California.
@@ -35,7 +35,7 @@
 
 #ifndef lint
 static char RCSid[] = 
-"$OpenBSD: setargs.c,v 1.2 1996/03/05 03:16:13 dm Exp $";
+"$OpenBSD: setargs.c,v 1.3 1996/06/26 05:38:18 deraadt Exp $";
 
 static char sccsid[] = "@(#)setargs.c";
 

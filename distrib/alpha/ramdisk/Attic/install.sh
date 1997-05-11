37b7045e89f8c0811acc2611da45b41154cefa11
millert <millert@openbsd.org>
1997-05-11 09:54:24 +0000

    Use the common distrib/miniroot{install.sh,install.sub}.

--- distrib/alpha/ramdisk/Attic/install.sh
+++ distrib/alpha/ramdisk/Attic/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$OpenBSD: install.sh,v 1.1 1997/05/07 12:46:56 niklas Exp $
+#	$OpenBSD: install.sh,v 1.2 1997/05/09 05:40:57 millert Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1996 The NetBSD Foundation, Inc.

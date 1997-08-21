14c3e8863d56024fa5b384415e3d25cef9afea5d
deraadt <deraadt@openbsd.org>
1997-08-21 05:44:13 +0000

    this has not been used for a while

--- bin/sh/Attic/myhistedit.h
+++ bin/sh/Attic/myhistedit.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: myhistedit.h,v 1.2 1996/06/23 14:21:25 deraadt Exp $	*/
+/*	$OpenBSD: myhistedit.h,v 1.3 1996/10/20 00:54:57 millert Exp $	*/
 /*	$NetBSD: myhistedit.h,v 1.4 1995/05/11 21:29:40 christos Exp $	*/
 
 /*-

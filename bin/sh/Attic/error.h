a990855823aee6960094ef39a528825fe9446454
deraadt <deraadt@openbsd.org>
1997-08-21 05:43:54 +0000

    this has not been used for a while

--- bin/sh/Attic/error.h
+++ bin/sh/Attic/error.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: error.h,v 1.2 1996/06/23 14:21:11 deraadt Exp $	*/
+/*	$OpenBSD: error.h,v 1.3 1996/10/20 00:54:46 millert Exp $	*/
 /*	$NetBSD: error.h,v 1.8 1995/05/11 21:28:53 christos Exp $	*/
 
 /*-

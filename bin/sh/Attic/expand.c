d3000fcf2e692f969043c55aec95e1d4e8554455
deraadt <deraadt@openbsd.org>
1997-08-21 05:43:58 +0000

    this has not been used for a while

--- bin/sh/Attic/expand.c
+++ bin/sh/Attic/expand.c
@@ -1,4 +1,4 @@
-/*    $OpenBSD: expand.c,v 1.6 1996/11/02 16:00:04 millert Exp $     */
+/*    $OpenBSD: expand.c,v 1.7 1997/06/18 19:15:50 kstailey Exp $     */
 /*    $NetBSD: expand.c,v 1.20 1996/02/12 15:11:41 christos Exp $     */
 
 /*-
@@ -41,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)expand.c	8.5 (Berkeley) 5/15/95";
 #else
-static char rcsid[] = "$OpenBSD: expand.c,v 1.6 1996/11/02 16:00:04 millert Exp $";
+static char rcsid[] = "$OpenBSD: expand.c,v 1.7 1997/06/18 19:15:50 kstailey Exp $";
 #endif
 #endif /* not lint */
 

e34193787d528110c6f53fb2de8a561e7917018a
provos <provos@openbsd.org>
1997-08-26 18:04:45 +0000

    made hopefully more user friendly + support for new esp.

--- sbin/ipsec/ipsecadm/Attic/xf_espdes.c
+++ sbin/ipsec/ipsecadm/Attic/xf_espdes.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: xf_espdes.c,v 1.3 1997/07/01 22:18:07 provos Exp $ */
+/* $OpenBSD: xf_espdes.c,v 1.4 1997/07/11 23:50:24 provos Exp $ */
 /*
  * The author of this code is John Ioannidis, ji@tla.org,
  * 	(except when noted otherwise).

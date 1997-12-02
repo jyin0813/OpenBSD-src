b204d7786b85f7856be0d92b8024133da4462ca6
deraadt <deraadt@openbsd.org>
1997-12-02 09:28:10 +0000

    repository move of src/bin/pdksh to src/bin/ksh

--- bin/pdksh/Attic/ksh_wait.h
+++ bin/pdksh/Attic/ksh_wait.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ksh_wait.h,v 1.1.1.1 1996/08/14 06:19:11 downsj Exp $	*/
+/*	$OpenBSD: ksh_wait.h,v 1.3 1997/06/19 13:58:43 kstailey Exp $	*/
 
 /* Wrapper around the ugly sys/wait includes/ifdefs */
 

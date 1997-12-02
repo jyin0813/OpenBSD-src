d700bc1dd59e3bc0ba4304c73ace972bde5b8abd
deraadt <deraadt@openbsd.org>
1997-12-02 09:27:51 +0000

    repository move of src/bin/pdksh to src/bin/ksh

--- bin/pdksh/Attic/emacs.c
+++ bin/pdksh/Attic/emacs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: emacs.c,v 1.2 1996/08/19 20:08:48 downsj Exp $	*/
+/*	$OpenBSD: emacs.c,v 1.4 1997/06/19 13:58:39 kstailey Exp $	*/
 
 /*
  *  Emacs-like command line editing and history

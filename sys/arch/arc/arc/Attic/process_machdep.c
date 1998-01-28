89bf1976363b7a965bf293562d1776f98e4780bf
pefo <pefo@openbsd.org>
1998-01-28 20:45:58 +0000

    Moving around files for new mips arch layout

--- sys/arch/arc/arc/Attic/process_machdep.c
+++ sys/arch/arc/arc/Attic/process_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: process_machdep.c,v 1.1.1.1 1996/06/24 09:07:21 pefo Exp $	*/
+/*	$OpenBSD: process_machdep.c,v 1.2 1997/04/19 17:19:47 pefo Exp $	*/
 /*
  * Copyright (c) 1994 Adam Glass   
  * Copyright (c) 1993 The Regents of the University of California.
@@ -39,7 +39,7 @@
  * From:
  *	Id: procfs_i386.c,v 4.1 1993/12/17 10:47:45 jsp Rel
  *
- *	$Id: process_machdep.c,v 1.1.1.1 1996/06/24 09:07:21 pefo Exp $
+ *	$Id: process_machdep.c,v 1.2 1997/04/19 17:19:47 pefo Exp $
  */
 
 /*

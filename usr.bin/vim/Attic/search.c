020932d855723d23c67d42ef9b49171a7a1cd31d
downsj <downsj@openbsd.org>
1998-02-23 19:06:57 +0000

    This belongs in ports.

--- usr.bin/vim/Attic/search.c
+++ usr.bin/vim/Attic/search.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: search.c,v 1.1.1.1 1996/09/07 21:40:24 downsj Exp $	*/
+/*	$OpenBSD: search.c,v 1.2 1996/09/21 06:23:19 downsj Exp $	*/
 /* vi:set ts=4 sw=4:
  *
  * VIM - Vi IMproved		by Bram Moolenaar

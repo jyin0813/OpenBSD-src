e7300f5908fedbaa8ab3e8a76a38d67250fe4930
downsj <downsj@openbsd.org>
1998-02-23 19:06:39 +0000

    This belongs in ports.

--- usr.bin/vim/Attic/gui_motif.c
+++ usr.bin/vim/Attic/gui_motif.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gui_motif.c,v 1.1.1.1 1996/09/07 21:40:28 downsj Exp $	*/
+/*	$OpenBSD: gui_motif.c,v 1.2 1996/09/21 06:23:03 downsj Exp $	*/
 /* vi:set ts=4 sw=4:
  *
  * VIM - Vi IMproved			by Bram Moolenaar

1ffef24102c52b7951a22147a321d01ead3f88d7
downsj <downsj@openbsd.org>
1998-02-23 19:06:29 +0000

    This belongs in ports.

--- usr.bin/vim/Attic/cmdline.c
+++ usr.bin/vim/Attic/cmdline.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cmdline.c,v 1.5 1996/10/14 03:55:08 downsj Exp $	*/
+/*	$OpenBSD: cmdline.c,v 1.6 1996/10/15 08:07:49 downsj Exp $	*/
 /* vi:set ts=4 sw=4:
  *
  * VIM - Vi IMproved		by Bram Moolenaar

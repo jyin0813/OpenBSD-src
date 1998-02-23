89da541d612987f48893e7d5771b3da6bb14dd6b
downsj <downsj@openbsd.org>
1998-02-23 19:07:01 +0000

    This belongs in ports.

--- usr.bin/vim/Attic/tag.c
+++ usr.bin/vim/Attic/tag.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tag.c,v 1.2 1996/09/21 06:23:21 downsj Exp $	*/
+/*	$OpenBSD: tag.c,v 1.3 1996/10/14 03:55:28 downsj Exp $	*/
 /* vi:set ts=4 sw=4:
  *
  * VIM - Vi IMproved		by Bram Moolenaar

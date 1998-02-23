02a26ca9e4d80d2f435973b2afa9b701724518c4
downsj <downsj@openbsd.org>
1998-02-23 19:06:35 +0000

    This belongs in ports.

--- usr.bin/vim/Attic/globals.h
+++ usr.bin/vim/Attic/globals.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: globals.h,v 1.3 1996/09/22 01:18:02 downsj Exp $	*/
+/*	$OpenBSD: globals.h,v 1.4 1996/10/14 03:55:11 downsj Exp $	*/
 /* vi:set ts=4 sw=4:
  *
  * VIM - Vi IMproved		by Bram Moolenaar

9737a76674b059090e450aef81a9f0efed26637a
downsj <downsj@openbsd.org>
1998-02-23 19:06:33 +0000

    This belongs in ports.

--- usr.bin/vim/Attic/edit.c
+++ usr.bin/vim/Attic/edit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: edit.c,v 1.4 1996/10/14 03:55:10 downsj Exp $	*/
+/*	$OpenBSD: edit.c,v 1.5 1996/10/15 08:07:54 downsj Exp $	*/
 /* vi:set ts=4 sw=4:
  *
  * VIM - Vi IMproved		by Bram Moolenaar

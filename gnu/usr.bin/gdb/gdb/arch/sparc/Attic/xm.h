3d5d9346b9d62fc4ca91ab2d6855c5d0e510dd11
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:58 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/sparc/Attic/xm.h
+++ gnu/usr.bin/gdb/gdb/arch/sparc/Attic/xm.h
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: xm.h,v 1.1 1994/01/28 12:42:16 pk Exp $
+	$Id: xm.h,v 1.1 1995/10/18 08:40:09 deraadt Exp $
 */
 
 #include <machine/limits.h>		/* for INT_MIN, to avoid "INT_MIN

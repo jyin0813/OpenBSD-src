b25e359ef4c7df8907c6e3b2abec67380521788d
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:50 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/sparc/Attic/sparc-nat.c
+++ gnu/usr.bin/gdb/gdb/arch/sparc/Attic/sparc-nat.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: sparc-nat.c,v 1.4 1994/05/19 15:50:27 pk Exp $
+	$Id: sparc-nat.c,v 1.1 1995/10/18 08:40:08 deraadt Exp $
 */
 
 #include "defs.h"

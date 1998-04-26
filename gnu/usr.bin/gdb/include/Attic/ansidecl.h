b853104126ce5c4108c50d7b7246b3211d4cc21f
deraadt <deraadt@openbsd.org>
1998-04-26 23:28:11 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/include/Attic/ansidecl.h
+++ gnu/usr.bin/gdb/include/Attic/ansidecl.h
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: ansidecl.h,v 1.1 1994/01/28 12:42:20 pk Exp $
+	$Id: ansidecl.h,v 1.1 1995/10/18 08:40:09 deraadt Exp $
 */
 
 /* ANSI and traditional C compatibility macros

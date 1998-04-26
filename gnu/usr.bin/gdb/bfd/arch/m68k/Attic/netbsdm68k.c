aa8f8cdbf5ab5c536bc453b078e336120972db45
deraadt <deraadt@openbsd.org>
1998-04-26 23:24:13 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/bfd/arch/m68k/Attic/netbsdm68k.c
+++ gnu/usr.bin/gdb/bfd/arch/m68k/Attic/netbsdm68k.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: netbsdm68k.c,v 1.2 1994/02/04 18:08:15 mycroft Exp $
+	$Id: netbsdm68k.c,v 1.1 1995/10/18 08:39:55 deraadt Exp $
 */
 
 #define	BYTES_IN_WORD	4

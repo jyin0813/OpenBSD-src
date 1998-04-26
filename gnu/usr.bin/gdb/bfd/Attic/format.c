a3cf68dc022740124ec82a9c6317d8837c325feb
deraadt <deraadt@openbsd.org>
1998-04-26 23:23:37 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/bfd/Attic/format.c
+++ gnu/usr.bin/gdb/bfd/Attic/format.c
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: format.c,v 1.1 1994/01/28 12:37:53 pk Exp $
+	$Id: format.c,v 1.1 1995/10/18 08:39:53 deraadt Exp $
 */
 
 /*

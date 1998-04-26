e5cedbaf8b34fec90eb5cf355ef6d85d6c1e04bd
deraadt <deraadt@openbsd.org>
1998-04-26 23:29:43 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/readline/Attic/vi_mode.c
+++ gnu/usr.bin/gdb/readline/Attic/vi_mode.c
@@ -21,7 +21,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: vi_mode.c,v 1.4 1994/01/28 12:43:39 pk Exp $
+	$Id: vi_mode.c,v 1.1 1995/10/18 08:40:16 deraadt Exp $
 */
 
 

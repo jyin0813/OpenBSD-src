9f4f90035b4ff9b0f855d47c15d655daad599270
deraadt <deraadt@openbsd.org>
1998-04-26 23:29:40 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/readline/Attic/tilde.c
+++ gnu/usr.bin/gdb/readline/Attic/tilde.c
@@ -19,7 +19,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: tilde.c,v 1.1 1994/01/28 12:43:35 pk Exp $
+	$Id: tilde.c,v 1.1 1995/10/18 08:40:16 deraadt Exp $
 */
 
 #include "sysdep.h"

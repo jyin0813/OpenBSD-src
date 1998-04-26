1e9315e2f4d7faa6bd183dd6d5b5e9795f2207d0
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:55 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/sparc/Attic/tm-sparc.h
+++ gnu/usr.bin/gdb/gdb/arch/sparc/Attic/tm-sparc.h
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: tm-sparc.h,v 1.1 1994/01/28 12:42:13 pk Exp $
+	$Id: tm-sparc.h,v 1.1 1995/10/18 08:40:09 deraadt Exp $
 */
 
 #define TARGET_BYTE_ORDER BIG_ENDIAN

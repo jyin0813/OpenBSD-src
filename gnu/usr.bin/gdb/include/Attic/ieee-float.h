b825648fb38d9c8365f239518c0dc124f04e9228
deraadt <deraadt@openbsd.org>
1998-04-26 23:28:18 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/include/Attic/ieee-float.h
+++ gnu/usr.bin/gdb/include/Attic/ieee-float.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: ieee-float.h,v 1.1 1994/01/28 12:42:26 pk Exp $
+	$Id: ieee-float.h,v 1.1 1995/10/18 08:40:10 deraadt Exp $
 */
 
 #if !defined (IEEE_FLOAT_H)

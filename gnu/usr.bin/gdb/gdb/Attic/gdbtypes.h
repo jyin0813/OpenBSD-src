5cd7b6b4d068f2adcedd6336c595b7e30102f125
deraadt <deraadt@openbsd.org>
1998-04-26 23:25:42 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/gdbtypes.h
+++ gnu/usr.bin/gdb/gdb/Attic/gdbtypes.h
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: gdbtypes.h,v 1.1 1994/01/28 12:39:51 pk Exp $
+	$Id: gdbtypes.h,v 1.1 1995/10/18 08:40:00 deraadt Exp $
 */
 
 #if !defined (GDBTYPES_H)

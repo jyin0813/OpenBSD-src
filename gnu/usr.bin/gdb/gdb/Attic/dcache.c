e2e14ffc9bf368f138d129d39aad47a884bdb10c
deraadt <deraadt@openbsd.org>
1998-04-26 23:25:13 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/dcache.c
+++ gnu/usr.bin/gdb/gdb/Attic/dcache.c
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: dcache.c,v 1.1 1994/01/28 12:39:21 pk Exp $
+	$Id: dcache.c,v 1.1 1995/10/18 08:39:58 deraadt Exp $
 */
 
 #include "defs.h"

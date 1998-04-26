8acc13a288d546570b15b1b22eeac26b88e13680
deraadt <deraadt@openbsd.org>
1998-04-26 23:24:12 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/bfd/arch/m68k/Attic/cpu-m68k.c
+++ gnu/usr.bin/gdb/bfd/arch/m68k/Attic/cpu-m68k.c
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: cpu-m68k.c,v 1.1 1994/01/28 12:38:24 pk Exp $
+	$Id: cpu-m68k.c,v 1.1 1995/10/18 08:39:54 deraadt Exp $
 */
 
 #include "bfd.h"

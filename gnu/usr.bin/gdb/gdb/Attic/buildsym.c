2052f7e8e272940a371bc2b5c6a886002d1a3a87
deraadt <deraadt@openbsd.org>
1998-04-26 23:24:41 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/buildsym.c
+++ gnu/usr.bin/gdb/gdb/Attic/buildsym.c
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: buildsym.c,v 1.1 1994/01/28 12:38:52 pk Exp $
+	$Id: buildsym.c,v 1.1 1995/10/18 08:39:56 deraadt Exp $
 */
 
 /* This module provides subroutines used for creating and adding to

583d11f829216d1352ad531df6eca2bb8bf79719
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:15 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/i386/Attic/tm-i386v.h
+++ gnu/usr.bin/gdb/gdb/arch/i386/Attic/tm-i386v.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: tm-i386v.h,v 1.1 1994/01/28 12:41:37 pk Exp $
+	$Id: tm-i386v.h,v 1.1 1995/10/18 08:40:06 deraadt Exp $
 */
 
 #if !defined (TM_I386V_H)

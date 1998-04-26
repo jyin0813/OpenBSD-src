3cbf1c94d23b43e928c1d2127d46accd2032119a
deraadt <deraadt@openbsd.org>
1998-04-26 23:25:30 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/expression.h
+++ gnu/usr.bin/gdb/gdb/Attic/expression.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: expression.h,v 1.1 1994/01/28 12:39:37 pk Exp $
+	$Id: expression.h,v 1.1 1995/10/18 08:39:59 deraadt Exp $
 */
 
 #if !defined (EXPRESSION_H)

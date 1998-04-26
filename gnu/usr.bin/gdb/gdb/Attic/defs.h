2c954640d71ad64cda01560de8b8ac6015158791
deraadt <deraadt@openbsd.org>
1998-04-26 23:25:16 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/defs.h
+++ gnu/usr.bin/gdb/gdb/Attic/defs.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: defs.h,v 1.3 1995/01/12 10:20:27 pk Exp $
+	$Id: defs.h,v 1.1 1995/10/18 08:39:59 deraadt Exp $
 */
 
 #if !defined (DEFS_H)

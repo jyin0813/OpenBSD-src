6d21d3b3c595d66cb62de637128dd894bcc9f7bd
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:53 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/sparc/Attic/sparc-pinsn.c
+++ gnu/usr.bin/gdb/gdb/arch/sparc/Attic/sparc-pinsn.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: sparc-pinsn.c,v 1.1 1994/01/28 12:42:09 pk Exp $
+	$Id: sparc-pinsn.c,v 1.1 1995/10/18 08:40:09 deraadt Exp $
 */
 
 #include "defs.h"

a33ad9df57af50e3f669be9ba7145739ee52bf8b
deraadt <deraadt@openbsd.org>
1998-04-26 23:25:58 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/m2-lang.c
+++ gnu/usr.bin/gdb/gdb/Attic/m2-lang.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: m2-lang.c,v 1.1 1994/01/28 12:40:06 pk Exp $
+	$Id: m2-lang.c,v 1.1 1995/10/18 08:40:01 deraadt Exp $
 */
 
 #include "defs.h"

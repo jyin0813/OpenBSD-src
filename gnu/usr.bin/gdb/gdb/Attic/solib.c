d07d4e4f1488fe2efe627da800396c9f5e53d1eb
deraadt <deraadt@openbsd.org>
1998-04-26 23:26:30 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/solib.c
+++ gnu/usr.bin/gdb/gdb/Attic/solib.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: solib.c,v 1.2 1994/05/25 11:24:26 pk Exp $
+	$Id: solib.c,v 1.1 1995/10/18 08:40:03 deraadt Exp $
 */
 
 

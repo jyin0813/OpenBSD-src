aed4109bca049268c4dff3c1007da08eb1cd8155
deraadt <deraadt@openbsd.org>
1998-04-26 23:26:21 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/remote-utils.c
+++ gnu/usr.bin/gdb/gdb/Attic/remote-utils.c
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: remote-utils.c,v 1.1 1994/01/28 12:40:30 pk Exp $
+	$Id: remote-utils.c,v 1.1 1995/10/18 08:40:03 deraadt Exp $
 */
 
 /*  This file actually contains two distinct logical "packages".  They

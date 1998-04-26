39f5883b65047a0889f4edd93840f4c7c37835b5
deraadt <deraadt@openbsd.org>
1998-04-26 23:23:19 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/bfd/Attic/aout32.c
+++ gnu/usr.bin/gdb/bfd/Attic/aout32.c
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: aout32.c,v 1.1 1994/01/28 12:37:32 pk Exp $
+	$Id: aout32.c,v 1.1 1995/10/18 08:39:51 deraadt Exp $
 */
 
 #define ARCH_SIZE 32

aadf4e74333bf3c1cf42646b21a8f06c4bc61d58
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:09 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/i386/Attic/i386-dis.c
+++ gnu/usr.bin/gdb/gdb/arch/i386/Attic/i386-dis.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: i386-dis.c,v 1.2 1995/02/05 13:27:12 mycroft Exp $
+	$Id: i386-dis.c,v 1.1 1995/10/18 08:40:06 deraadt Exp $
 */
 
 /*

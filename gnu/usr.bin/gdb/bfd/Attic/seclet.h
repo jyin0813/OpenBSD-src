029b2398071a7e10022c5ce5d1616a13cda97150
deraadt <deraadt@openbsd.org>
1998-04-26 23:23:55 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/bfd/Attic/seclet.h
+++ gnu/usr.bin/gdb/bfd/Attic/seclet.h
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: seclet.h,v 1.1 1994/01/28 12:38:07 pk Exp $
+	$Id: seclet.h,v 1.1 1995/10/18 08:39:53 deraadt Exp $
 */
 
 #ifndef _SECLET_H

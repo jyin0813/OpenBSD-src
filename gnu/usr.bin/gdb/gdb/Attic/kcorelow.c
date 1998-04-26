e2559ff83e14bb6a80897ecff27aa672de633d25
deraadt <deraadt@openbsd.org>
1998-04-26 23:25:53 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/kcorelow.c
+++ gnu/usr.bin/gdb/gdb/Attic/kcorelow.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: kcorelow.c,v 1.1.1.1 1995/10/18 08:40:01 deraadt Exp $
+	$Id: kcorelow.c,v 1.2 1996/05/06 11:27:24 deraadt Exp $
 */
 
 #ifdef KERNEL_DEBUG

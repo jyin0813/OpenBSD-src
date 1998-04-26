5cdf9cdbe58be811bba68537ac88d12b50436cca
deraadt <deraadt@openbsd.org>
1998-04-26 23:25:07 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/coredep.c
+++ gnu/usr.bin/gdb/gdb/Attic/coredep.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: coredep.c,v 1.1 1994/01/28 12:39:15 pk Exp $
+	$Id: coredep.c,v 1.1 1995/10/18 08:39:58 deraadt Exp $
 */
 
 /* core.c is supposed to be the more machine-independent aspects of this;

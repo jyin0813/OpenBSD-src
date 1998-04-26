ba5b64a459617f7a819bfa5d8da7e82d07496990
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:47 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/sparc/Attic/nm.h
+++ gnu/usr.bin/gdb/gdb/arch/sparc/Attic/nm.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: nm.h,v 1.2 1994/03/09 16:15:57 pk Exp $
+	$Id: nm.h,v 1.1 1995/10/18 08:40:08 deraadt Exp $
 */
 
 #ifndef NM_SPARC_H

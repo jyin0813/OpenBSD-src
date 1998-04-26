232da1869c9ac86237e75d45cccc64bd7d53b56e
deraadt <deraadt@openbsd.org>
1998-04-26 23:28:50 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/libiberty/Attic/argv.c
+++ gnu/usr.bin/gdb/libiberty/Attic/argv.c
@@ -18,7 +18,7 @@ License along with libiberty; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.
 
-	$Id: argv.c,v 1.1 1994/01/28 12:42:55 pk Exp $
+	$Id: argv.c,v 1.1 1995/10/18 08:40:12 deraadt Exp $
 */
 
 

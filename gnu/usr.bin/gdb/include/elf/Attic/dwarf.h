c33a63d808e83cec0f15a1b07166536b2acbdfec
deraadt <deraadt@openbsd.org>
1998-04-26 23:28:37 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/include/elf/Attic/dwarf.h
+++ gnu/usr.bin/gdb/include/elf/Attic/dwarf.h
@@ -21,7 +21,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: dwarf.h,v 1.1 1994/01/28 12:42:45 pk Exp $
+	$Id: dwarf.h,v 1.1 1995/10/18 08:40:11 deraadt Exp $
 */
 
 /* This file is derived from the DWARF specification (a public document)

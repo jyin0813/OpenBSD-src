344cf08f80f4fc278020f27eb636d3c9ea31ca41
deraadt <deraadt@openbsd.org>
1998-04-26 23:28:43 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/include/opcode/Attic/sparc.h
+++ gnu/usr.bin/gdb/include/opcode/Attic/sparc.h
@@ -20,7 +20,7 @@ You should have received a copy of the GNU General Public License
 along with GAS or GDB; see the file COPYING.	If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: sparc.h,v 1.1 1994/01/28 12:42:50 pk Exp $
+	$Id: sparc.h,v 1.1 1995/10/18 08:40:12 deraadt Exp $
 */
 
 /* The SPARC opcode table (and other related data) is defined in

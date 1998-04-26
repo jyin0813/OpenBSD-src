9320fc930a678440215fa675e5b39e07a8c284bb
deraadt <deraadt@openbsd.org>
1998-04-26 23:29:02 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/libiberty/Attic/obstack.c
+++ gnu/usr.bin/gdb/libiberty/Attic/obstack.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Library General Public License
 along with this program; if not, write to the Free Software
 Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: obstack.c,v 1.1 1994/01/28 12:43:06 pk Exp $
+	$Id: obstack.c,v 1.1 1995/10/18 08:40:13 deraadt Exp $
 */
 
 #include "obstack.h"

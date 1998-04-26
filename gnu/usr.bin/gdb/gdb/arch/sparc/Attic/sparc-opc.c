4c69c685f7b7ecb12e89a9c834f228ca5c23cf10
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:51 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/sparc/Attic/sparc-opc.c
+++ gnu/usr.bin/gdb/gdb/arch/sparc/Attic/sparc-opc.c
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this software; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: sparc-opc.c,v 1.1 1994/01/28 12:42:07 pk Exp $
+	$Id: sparc-opc.c,v 1.1 1995/10/18 08:40:08 deraadt Exp $
 */
 
 /* FIXME-someday: perhaps the ,a's and such should be embedded in the

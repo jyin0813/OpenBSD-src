dfa2a1cbb81364f229a4e2ed537e88b1d5896d0f
deraadt <deraadt@openbsd.org>
1998-04-26 23:24:51 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/call-cmds.h
+++ gnu/usr.bin/gdb/gdb/Attic/call-cmds.h
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: call-cmds.h,v 1.1 1994/01/28 12:39:01 pk Exp $
+	$Id: call-cmds.h,v 1.1 1995/10/18 08:39:57 deraadt Exp $
 */
 
 extern void

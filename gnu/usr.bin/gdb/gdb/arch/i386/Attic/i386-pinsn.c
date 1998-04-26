f440d9b332d8c422c2e5404e7301f04b13313485
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:10 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/i386/Attic/i386-pinsn.c
+++ gnu/usr.bin/gdb/gdb/arch/i386/Attic/i386-pinsn.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: i386-pinsn.c,v 1.1 1994/01/28 12:41:28 pk Exp $
+	$Id: i386-pinsn.c,v 1.1 1995/10/18 08:40:06 deraadt Exp $
 */
 
 #include "defs.h"

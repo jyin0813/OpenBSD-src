64a25bdceb9840b15840667d933c666c245bfe9b
deraadt <deraadt@openbsd.org>
1998-04-26 23:26:04 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/main.c
+++ gnu/usr.bin/gdb/gdb/Attic/main.c
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: main.c,v 1.5 1995/01/24 23:03:51 cgd Exp $
+	$Id: main.c,v 1.1 1995/10/18 08:40:01 deraadt Exp $
 */
 
 #include "defs.h"

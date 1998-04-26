481c4dfcbd2277cea36248d87ee41fe422bbd7fd
deraadt <deraadt@openbsd.org>
1998-04-26 23:28:09 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/vax/Attic/xm.h
+++ gnu/usr.bin/gdb/gdb/arch/vax/Attic/xm.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: xm.h,v 1.1 1995/06/05 15:23:14 ragge Exp $
+	$Id: xm.h,v 1.1 1995/10/18 08:40:09 deraadt Exp $
 */
 
 #define HOST_BYTE_ORDER LITTLE_ENDIAN

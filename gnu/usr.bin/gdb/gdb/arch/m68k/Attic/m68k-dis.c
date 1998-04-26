326b38b35db2dd1ad5b46934686faa87be03c90c
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:22 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/m68k/Attic/m68k-dis.c
+++ gnu/usr.bin/gdb/gdb/arch/m68k/Attic/m68k-dis.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: m68k-dis.c,v 1.1 1994/01/28 12:41:46 pk Exp $
+	$Id: m68k-dis.c,v 1.1 1995/10/18 08:40:07 deraadt Exp $
 */
 
 #include "dis-asm.h"

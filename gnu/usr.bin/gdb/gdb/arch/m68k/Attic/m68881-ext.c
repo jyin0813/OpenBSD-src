e968fd5f78e6cf0416787547d43c76598a0220c0
deraadt <deraadt@openbsd.org>
1998-04-26 23:27:21 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/arch/m68k/Attic/m68881-ext.c
+++ gnu/usr.bin/gdb/gdb/arch/m68k/Attic/m68881-ext.c
@@ -1,4 +1,4 @@
-/*	$Id: m68881-ext.c,v 1.1 1994/01/28 12:41:45 pk Exp $ */
+/*	$Id: m68881-ext.c,v 1.1 1995/10/18 08:40:07 deraadt Exp $ */
 #include "ieee-float.h"
 
 CONST struct ext_format ext_format_68881 = {

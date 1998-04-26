f7bc196ea2cf7c0beda7f0fff49aa4e4ce50daac
deraadt <deraadt@openbsd.org>
1998-04-26 23:24:48 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/c-typeprint.c
+++ gnu/usr.bin/gdb/gdb/Attic/c-typeprint.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: c-typeprint.c,v 1.1 1994/01/28 12:38:58 pk Exp $
+	$Id: c-typeprint.c,v 1.1 1995/10/18 08:39:57 deraadt Exp $
 */
 
 #include "defs.h"

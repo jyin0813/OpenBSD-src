34e53962660b799a55862b6a4fe0b628ecaaf301
deraadt <deraadt@openbsd.org>
1998-04-26 23:26:50 +0000

    we use binutils gdb everwhere now

--- gnu/usr.bin/gdb/gdb/Attic/thread.c
+++ gnu/usr.bin/gdb/gdb/Attic/thread.c
@@ -20,7 +20,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: thread.c,v 1.1 1994/01/28 12:41:01 pk Exp $
+	$Id: thread.c,v 1.1 1995/10/18 08:40:05 deraadt Exp $
 */
 
 #include "defs.h"

9d935af8cf7c54e292bc013ccf499595d9b3ea3b
peter <peter@openbsd.org>
1998-07-21 21:35:31 +0000

    remove irrelevant files
    retained some netbsd files to act as templates for openbsd

--- lib/libpthread/machdep/Attic/posix-sunos-4.1.3.h
+++ lib/libpthread/machdep/Attic/posix-sunos-4.1.3.h
@@ -1,7 +1,7 @@
 /* ==== posix.h ============================================================
  * Copyright (c) 1993 by Chris Provenzano, proven@athena.mit.edu	
  *
- * $Id: posix-sunos-4.1.3.h,v 1.51 1995/01/06 07:27:16 raeburn Exp $
+ * $Id: posix-sunos-4.1.3.h,v 1.1 1998/07/21 13:19:15 peter Exp $
  *
  * Description : Do the right thing for a sunos 4.1.3 system.
  *

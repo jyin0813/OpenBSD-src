8d34466bd2a75ca1ae82374f1d5c002f5841d5d8
peter <peter@openbsd.org>
1998-07-21 21:35:35 +0000

    remove irrelevant files
    retained some netbsd files to act as templates for openbsd

--- lib/libpthread/machdep/Attic/posix-ultrix-4.2.h
+++ lib/libpthread/machdep/Attic/posix-ultrix-4.2.h
@@ -1,7 +1,7 @@
 /* ==== posix.h ============================================================
  * Copyright (c) 1993 by Chris Provenzano, proven@athena.mit.edu	
  *
- * $Id: posix-ultrix-4.2.h,v 1.51 1995/01/06 07:28:38 raeburn Exp $
+ * $Id: posix-ultrix-4.2.h,v 1.1 1998/07/21 13:19:16 peter Exp $
  *
  * Description : Convert an Ultrix-4.2 system to a more or less POSIX system.
  *

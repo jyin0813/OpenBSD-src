eb8ca916d290d7ac4a5fbbf68b42d274ae5f7e5d
peter <peter@openbsd.org>
1998-07-22 16:46:41 +0000

    o removed more unused or old, left over files
    o general changes so that now the library compiles and many of the
      tests run correctly
    
    o pthreads/Makefile.inc rebuilt using libc/sys/Makefile.inc as a template.
      this should be kept in sync in case of new syscalls
    
    soon TODO:
    figure out why remaining tests fail and fix
    look at building .so version of library
    tidy up arch/i386 directory - no longer uses syscall-template.S
    port other arch's from libc syscall templates

--- lib/libpthread/include/Attic/kernel.h
+++ lib/libpthread/include/Attic/kernel.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: kernel.h,v 1.3 1994/02/07 22:02:30 proven Exp $ $provenid: kernel.h,v 1.17 1994/02/07 03:31:57 proven Exp $
+ * $Id: kernel.h,v 1.1 1995/10/18 08:43:04 deraadt Exp $ $provenid: kernel.h,v 1.17 1994/02/07 03:31:57 proven Exp $
  *
  * Description : mutex header.
  *

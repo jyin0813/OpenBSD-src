463d27697b20efa303a7a4513ec6a83540671c02
peter <peter@openbsd.org>
1998-07-22 14:21:26 +0000

    my cvs skills are rusty. I forgot to delete these.

--- lib/libpthread/machdep/sunos-5.3/Attic/timers.h
+++ lib/libpthread/machdep/sunos-5.3/Attic/timers.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: timers.h,v 1.1.4.1 1996/03/05 08:28:47 proven Exp $
+ * $Id: timers.h,v 1.1 1998/07/21 13:19:35 peter Exp $
  *
  * Description : Basic timers header.
  *

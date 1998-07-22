798c8a26235e3cbc51373f2c8cddcb457a2f3b4c
peter <peter@openbsd.org>
1998-07-22 16:46:44 +0000

    o removed more unused or old, left over files
    o general changes so that now the library compiles and many of the
      tests run correctly
    
    o pthreads/Makefile.inc rebuilt using libc/sys/Makefile.inc as a template.
      this should be kept in sync in case of new syscalls
    
    soon TODO:
    figure out why remaining tests fail and fix
    look at building .so version of library
    tidy up arch/i386 directory - no longer uses syscall-template.S
    port other arch's from libc syscall templates

--- lib/libpthread/include/Attic/posix.h
+++ lib/libpthread/include/Attic/posix.h
@@ -3,7 +3,7 @@
  *
  * Description : Convert an Ultrix-4.2 system to a more or less POSIX system.
  *
- * $Id: posix.h,v 1.3 1994/02/07 22:02:32 proven Exp $ $provenid: posix-netbsd-0.9.h,v 1.17 1994/02/07 03:32:24 proven Exp $
+ * $Id: posix.h,v 1.1 1995/10/18 08:43:04 deraadt Exp $ $provenid: posix-netbsd-0.9.h,v 1.17 1994/02/07 03:32:24 proven Exp $
  *
  *  1.00 93/07/20 proven
  *      -Started coding this file.

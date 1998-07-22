416ff1fd3bc64d41eba921e3f537a4af0f3e5006
peter <peter@openbsd.org>
1998-07-22 14:21:02 +0000

    my cvs skills are rusty. I forgot to delete these.

--- lib/libpthread/machdep/sunos-4.1.3/Attic/compat.h
+++ lib/libpthread/machdep/sunos-4.1.3/Attic/compat.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: compat.h,v 1.50 1994/08/08 03:44:14 proven Exp $
+ * $Id: compat.h,v 1.1 1998/07/21 13:19:33 peter Exp $
  *
  * Description : Basic timers header.
  *

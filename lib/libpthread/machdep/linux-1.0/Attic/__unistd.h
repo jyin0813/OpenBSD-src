05aeef3cde0ba251e4b62b14cfb96222a5b41b60
peter <peter@openbsd.org>
1998-07-22 14:19:43 +0000

    my cvs skills are rusty. I forgot to delete these.

--- lib/libpthread/machdep/linux-1.0/Attic/__unistd.h
+++ lib/libpthread/machdep/linux-1.0/Attic/__unistd.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)stdlib.h	5.13 (Berkeley) 6/4/91
- *	$Id: __unistd.h,v 1.1 1995/01/03 12:54:54 proven Exp $
+ *	$Id: __unistd.h,v 1.1 1998/07/21 13:19:28 peter Exp $
  */
 
 #ifndef _SYS___UNISTD_H_

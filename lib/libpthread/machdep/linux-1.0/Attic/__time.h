aea0da515f8d75c70e464888d199000e5e1641eb
peter <peter@openbsd.org>
1998-07-22 14:19:42 +0000

    my cvs skills are rusty. I forgot to delete these.

--- lib/libpthread/machdep/linux-1.0/Attic/__time.h
+++ lib/libpthread/machdep/linux-1.0/Attic/__time.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: __time.h,v 1.2 1995/06/14 07:22:28 proven Exp $
+ * $Id: __time.h,v 1.1 1998/07/21 13:19:28 peter Exp $
  *
  * Description : System specific time header.
  *

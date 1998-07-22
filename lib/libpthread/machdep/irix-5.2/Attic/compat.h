c8f5d4801bf7c5d0465f0aff0f4ec73bfa3cd97e
peter <peter@openbsd.org>
1998-07-22 14:19:28 +0000

    my cvs skills are rusty. I forgot to delete these.

--- lib/libpthread/machdep/irix-5.2/Attic/compat.h
+++ lib/libpthread/machdep/irix-5.2/Attic/compat.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: compat.h,v 1.1 1995/08/30 22:25:52 proven Exp $
+ * $Id: compat.h,v 1.1 1998/07/21 13:19:26 peter Exp $
  *
  * Description : 
  *

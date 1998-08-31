c2ed8eaca632cf6cd65d0ddce0ea8575a92f75bf
brian <brian@openbsd.org>
1998-08-31 06:22:09 +0000

    Upgrade ppp to version 2.0
    This adds a number of features including:
    o Multilink
    o Callback (including CBCP)
    o DNS negotiation (client & server)
    o Easy to configure masquerading/packet aliasing
    o Any number of diagnostic socket connections (limits DOS attacks)
    o Sticky routes (ppp updates the routing table when the IP number is changed)
    
    See the file README.changes for things that effect current
    configurations.

--- usr.sbin/ppp/libalias/Attic/alias_local.h
+++ usr.sbin/ppp/libalias/Attic/alias_local.h
@@ -78,7 +78,9 @@ int GetDeltaSeqOut(struct ip *, struct alias_link *);
 void AddSeq(struct ip *, struct alias_link *, int);
 void SetExpire(struct alias_link *, int);
 void ClearCheckNewLink(void);
+#ifndef NO_FW_PUNCH
 void PunchFWHole(struct alias_link *);
+#endif
 
 
 /* Housekeeping function */
@@ -88,6 +90,12 @@ void HouseKeeping(void);
 /*lint -save -library Suppress flexelint warnings */
 void AliasHandleFtpOut(struct ip *, struct alias_link *, int);
 void AliasHandleIrcOut(struct ip *pip, struct alias_link *link, int maxsize );
+void AliasHandleUdpNbt(struct ip *, struct alias_link *, struct in_addr *, u_short);
+void AliasHandleUdpNbtNS(struct ip *, struct alias_link *, struct in_addr *, u_short *, struct in_addr *, u_short *);
+void AliasHandleCUSeeMeOut(struct ip *, struct alias_link *);
+void AliasHandleCUSeeMeIn(struct ip *, struct in_addr);
+
+
 
 enum alias_tcp_state {
     ALIAS_TCP_STATE_NOT_CONNECTED,

ff46bca860bd1bfa835b8472f7f315adc296d066
brian <brian@openbsd.org>
1998-08-31 06:20:48 +0000

    Upgrade ppp to version 2.0
    This adds a number of features including:
    o Multilink
    o Callback (including CBCP)
    o DNS negotiation (client & server)
    o Easy to configure masquerading/packet aliasing
    o Any number of diagnostic socket connections (limits DOS attacks)
    o Sticky routes (ppp updates the routing table when the IP number is changed)
    
    See the file README.changes for things that effect current
    configurations.

--- usr.sbin/ppp/Attic/id.c
+++ usr.sbin/ppp/Attic/id.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: id.c,v 1.4 1998/01/21 02:13:33 brian Exp $
+ *	$Id: id.c,v 1.5 1998/02/19 02:02:46 brian Exp $
  */
 
 #include <sys/types.h>

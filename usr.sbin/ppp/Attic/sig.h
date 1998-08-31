3914dfe860f254ae41ebafeb362ff1642b696070
brian <brian@openbsd.org>
1998-08-31 06:21:38 +0000

    Upgrade ppp to version 2.0
    This adds a number of features including:
    o Multilink
    o Callback (including CBCP)
    o DNS negotiation (client & server)
    o Easy to configure masquerading/packet aliasing
    o Any number of diagnostic socket connections (limits DOS attacks)
    o Sticky routes (ppp updates the routing table when the IP number is changed)
    
    See the file README.changes for things that effect current
    configurations.

--- usr.sbin/ppp/Attic/sig.h
+++ usr.sbin/ppp/Attic/sig.h
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id$
+ *	$Id: sig.h,v 1.2 1997/12/21 14:27:15 brian Exp $
  */
 
 typedef void (*sig_type)(int);

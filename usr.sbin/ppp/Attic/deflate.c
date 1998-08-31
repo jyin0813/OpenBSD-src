c800725981fd39d5f484585fe73698b034029011
brian <brian@openbsd.org>
1998-08-31 06:20:35 +0000

    Upgrade ppp to version 2.0
    This adds a number of features including:
    o Multilink
    o Callback (including CBCP)
    o DNS negotiation (client & server)
    o Easy to configure masquerading/packet aliasing
    o Any number of diagnostic socket connections (limits DOS attacks)
    o Sticky routes (ppp updates the routing table when the IP number is changed)
    
    See the file README.changes for things that effect current
    configurations.

--- usr.sbin/ppp/Attic/deflate.c
+++ usr.sbin/ppp/Attic/deflate.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: deflate.c,v 1.5 1998/01/10 01:55:14 brian Exp $
+ *	$Id: deflate.c,v 1.6 1998/03/13 01:25:55 brian Exp $
  */
 
 #include <sys/param.h>

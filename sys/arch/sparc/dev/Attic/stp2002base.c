078586e642d4adc43d444dd812c8c2857b2e521f
jason <jason@openbsd.org>
1998-09-01 23:37:00 +0000

    o Removed dependence on stp2002 (not sharable with qec+be) o Added collision counters o Should work on sun4c

--- sys/arch/sparc/dev/Attic/stp2002base.c
+++ sys/arch/sparc/dev/Attic/stp2002base.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: stp2002base.c,v 1.1 1998/07/17 21:33:11 jason Exp $	*/
+/*	$OpenBSD: stp2002base.c,v 1.2 1998/07/22 17:27:15 jason Exp $	*/
 
 /*
  * Copyright (c) 1998 Jason L. Wright (jason@thought.net)

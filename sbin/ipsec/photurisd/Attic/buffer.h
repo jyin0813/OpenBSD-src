49e8d7c3d0261d021d4435676b63abaea69344b4
deraadt <deraadt@openbsd.org>
1998-11-15 06:37:05 +0000

    move ipsec tools into .

--- sbin/ipsec/photurisd/Attic/buffer.h
+++ sbin/ipsec/photurisd/Attic/buffer.h
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/* $Id: buffer.h,v 1.1.1.1 1997/07/18 22:48:51 provos Exp $ */
+/* $Id: buffer.h,v 1.2 1998/03/04 11:43:12 provos Exp $ */
 /*
  * buffer.h: 
  * variables with buffers.

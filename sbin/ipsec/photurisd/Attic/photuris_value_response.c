8bcb3e487c4dc3aaa69342481987587a5a431791
deraadt <deraadt@openbsd.org>
1998-11-15 06:37:16 +0000

    move ipsec tools into .

--- sbin/ipsec/photurisd/Attic/photuris_value_response.c
+++ sbin/ipsec/photurisd/Attic/photuris_value_response.c
@@ -34,7 +34,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: photuris_value_response.c,v 1.1.1.1 1997/07/18 22:48:49 provos Exp $";
+static char rcsid[] = "$Id: photuris_value_response.c,v 1.2 1998/03/04 11:43:44 provos Exp $";
 #endif
 
 #include <stdio.h>

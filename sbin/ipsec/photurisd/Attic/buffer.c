1995d0f4e9855f77769d7f4999dd8f7ce9fcfcb7
deraadt <deraadt@openbsd.org>
1998-11-15 06:37:05 +0000

    move ipsec tools into .

--- sbin/ipsec/photurisd/Attic/buffer.c
+++ sbin/ipsec/photurisd/Attic/buffer.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: buffer.c,v 1.1.1.1 1997/07/18 22:48:50 provos Exp $";
+static char rcsid[] = "$Id: buffer.c,v 1.2 1998/03/04 11:43:11 provos Exp $";
 #endif
 
 #include <sys/types.h>

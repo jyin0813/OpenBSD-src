34072807cd5ab48aacbb15387e822e711b880bfc
deraadt <deraadt@openbsd.org>
1998-11-15 06:37:17 +0000

    move ipsec tools into .

--- sbin/ipsec/photurisd/Attic/secrets.h
+++ sbin/ipsec/photurisd/Attic/secrets.h
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/* $Id: secrets.h,v 1.2 1997/09/02 17:26:48 provos Exp $ */
+/* $Id: secrets.h,v 1.3 1998/03/04 11:43:50 provos Exp $ */
 /*
  * secrets.h:
  * prototypes for compute_secrets.c

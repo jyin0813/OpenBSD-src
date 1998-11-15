887b1c270d483a2e60b07cb68d4dad977d9064c0
deraadt <deraadt@openbsd.org>
1998-11-15 06:37:13 +0000

    move ipsec tools into .

--- sbin/ipsec/photurisd/Attic/packets.h
+++ sbin/ipsec/photurisd/Attic/packets.h
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/* $Id: packets.h,v 1.1.1.1 1997/07/18 22:48:49 provos Exp $ */
+/* $Id: packets.h,v 1.2 1998/03/04 11:43:37 provos Exp $ */
 /*
  * packets.h:
  */

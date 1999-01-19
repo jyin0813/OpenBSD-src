ebf18f1d3d5fb5bfde45b4f05ac2547840234253
millert <millert@openbsd.org>
1999-01-19 02:08:59 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_wattron.c
+++ lib/libcurses/Attic/lib_wattron.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_wattron.c,v 1.2 1998/07/23 21:19:51 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

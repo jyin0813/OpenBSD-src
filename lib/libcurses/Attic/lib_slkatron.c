e9236dfb10f1d94bfb582d75069a58534df33dab
millert <millert@openbsd.org>
1999-01-19 02:08:31 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_slkatron.c
+++ lib/libcurses/Attic/lib_slkatron.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_slkatron.c,v 1.2 1998/07/23 21:19:26 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

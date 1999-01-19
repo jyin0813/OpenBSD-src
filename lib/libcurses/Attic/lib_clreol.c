818c305f8f66154bbd505a3d7cc73d24724d4a7a
millert <millert@openbsd.org>
1999-01-19 02:07:39 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_clreol.c
+++ lib/libcurses/Attic/lib_clreol.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_clreol.c,v 1.4 1998/07/23 21:18:35 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

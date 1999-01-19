32bde072890f71814d5f7ac13babf4dbe71d2d28
millert <millert@openbsd.org>
1999-01-19 02:08:25 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_scrollok.c
+++ lib/libcurses/Attic/lib_scrollok.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_scrollok.c,v 1.1 1998/07/23 21:19:21 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

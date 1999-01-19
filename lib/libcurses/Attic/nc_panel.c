89d4b4a67b8fdb754153ceaeed5ae49bb44c3c23
millert <millert@openbsd.org>
1999-01-19 02:09:03 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/nc_panel.c
+++ lib/libcurses/Attic/nc_panel.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: nc_panel.c,v 1.2 1998/07/23 21:19:55 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

318e7336e7d8807d703e42e8a865bbff6f5248c4
millert <millert@openbsd.org>
1999-01-19 02:07:59 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_insdel.c
+++ lib/libcurses/Attic/lib_insdel.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_insdel.c,v 1.4 1998/07/23 21:18:54 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

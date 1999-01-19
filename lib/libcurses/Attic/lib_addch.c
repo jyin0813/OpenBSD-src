82d9f343749104b99df379a8d224d80ba677fc3f
millert <millert@openbsd.org>
1999-01-19 02:07:29 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_addch.c
+++ lib/libcurses/Attic/lib_addch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_addch.c,v 1.5 1998/07/23 21:18:25 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

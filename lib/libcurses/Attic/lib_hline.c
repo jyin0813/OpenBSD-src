3e6f7d0f9d607e3edb59425f70068b87b16f0ac0
millert <millert@openbsd.org>
1999-01-19 02:07:55 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_hline.c
+++ lib/libcurses/Attic/lib_hline.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_hline.c,v 1.2 1998/07/23 21:18:49 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

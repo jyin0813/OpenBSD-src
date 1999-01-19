e485ff53225a2098c7df3d83b67286a1e8874f21
millert <millert@openbsd.org>
1999-01-19 02:07:50 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_flash.c
+++ lib/libcurses/Attic/lib_flash.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_flash.c,v 1.2 1998/07/23 21:18:45 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

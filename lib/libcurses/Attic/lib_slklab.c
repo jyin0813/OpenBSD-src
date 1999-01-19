ef5f70023c89fe8807880973958309daec25aadb
millert <millert@openbsd.org>
1999-01-19 02:08:37 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_slklab.c
+++ lib/libcurses/Attic/lib_slklab.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_slklab.c,v 1.2 1998/07/23 21:19:32 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

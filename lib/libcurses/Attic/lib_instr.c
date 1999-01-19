66f45cc7bd7d9218b97e80f2d411a2b9ee964e6a
millert <millert@openbsd.org>
1999-01-19 02:08:01 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_instr.c
+++ lib/libcurses/Attic/lib_instr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_instr.c,v 1.4 1998/07/23 21:18:56 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

4fdeaf19e85d91c53f50db58b0939accc030f7c1
millert <millert@openbsd.org>
1999-01-19 02:08:27 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_set_term.c
+++ lib/libcurses/Attic/lib_set_term.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lib_set_term.c,v 1.8 1998/10/31 06:30:30 millert Exp $	*/
+/*	$OpenBSD: lib_set_term.c,v 1.9 1998/11/17 03:16:21 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

cb994e56ccedcfaf26485980cc0218a6aa2f4a74
millert <millert@openbsd.org>
1999-01-19 02:08:34 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_slkclear.c
+++ lib/libcurses/Attic/lib_slkclear.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_slkclear.c,v 1.2 1998/07/23 21:19:29 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

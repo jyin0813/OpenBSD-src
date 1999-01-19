f77621c540cebd1997a553434a32eb1ca4d933e3
millert <millert@openbsd.org>
1999-01-19 02:08:17 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_printw.c
+++ lib/libcurses/Attic/lib_printw.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_printw.c,v 1.6 1998/07/23 21:19:13 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

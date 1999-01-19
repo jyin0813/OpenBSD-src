3130fd7f0c45fdd9ae0afe8660c9381304977a48
millert <millert@openbsd.org>
1999-01-19 02:08:10 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_mvwin.c
+++ lib/libcurses/Attic/lib_mvwin.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_mvwin.c,v 1.5 1998/07/23 21:19:04 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

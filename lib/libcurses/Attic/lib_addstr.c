f69f5336f4dff81ce066fb025184df75bacd6054
millert <millert@openbsd.org>
1999-01-19 02:07:30 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_addstr.c
+++ lib/libcurses/Attic/lib_addstr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_addstr.c,v 1.4 1998/07/23 21:18:26 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

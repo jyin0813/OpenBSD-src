1313721e149c97e82bda8eeac7d41eb638be3bc9
millert <millert@openbsd.org>
1999-01-19 02:07:28 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_adabind.c
+++ lib/libcurses/Attic/lib_adabind.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_adabind.c,v 1.1 1998/07/23 21:18:25 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

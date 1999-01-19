ad08fa09de36dee8ffc4816b8e468b07074a97da
millert <millert@openbsd.org>
1999-01-19 02:09:09 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/safe_sprintf.c
+++ lib/libcurses/Attic/safe_sprintf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: safe_sprintf.c,v 1.4 1998/08/15 19:06:38 millert Exp $	*/
+/*	$OpenBSD: safe_sprintf.c,v 1.5 1998/09/13 19:16:30 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

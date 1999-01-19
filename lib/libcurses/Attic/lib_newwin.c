0b3f86c20242b0b2cefc108d41ad4055e36ffb8b
millert <millert@openbsd.org>
1999-01-19 02:08:12 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_newwin.c
+++ lib/libcurses/Attic/lib_newwin.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_newwin.c,v 1.4 1998/07/23 21:19:07 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

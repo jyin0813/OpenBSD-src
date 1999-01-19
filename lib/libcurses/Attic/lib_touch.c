1f1a7e08599e9c8fe22b7d7f0082ac88f1eab445
millert <millert@openbsd.org>
1999-01-19 02:08:44 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_touch.c
+++ lib/libcurses/Attic/lib_touch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_touch.c,v 1.4 1998/07/23 21:19:37 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

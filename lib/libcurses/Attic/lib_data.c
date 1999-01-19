0697387c9b095caeb56363bc293a84827f760b79
millert <millert@openbsd.org>
1999-01-19 02:07:42 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_data.c
+++ lib/libcurses/Attic/lib_data.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_data.c,v 1.4 1998/07/23 21:18:37 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

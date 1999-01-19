225e6baac42fa0ef6c4003b9227bccb1b2dd6a1e
millert <millert@openbsd.org>
1999-01-19 02:08:43 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_ti.c
+++ lib/libcurses/Attic/lib_ti.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lib_ti.c,v 1.1 1998/07/23 21:19:37 millert Exp $	*/
+/*	$OpenBSD: lib_ti.c,v 1.2 1998/10/31 06:30:30 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

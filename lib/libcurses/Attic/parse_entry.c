e92e2f90acc012600a9a2e2faa0792e0ef6816e3
millert <millert@openbsd.org>
1999-01-19 02:09:05 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/parse_entry.c
+++ lib/libcurses/Attic/parse_entry.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: parse_entry.c,v 1.1 1998/07/23 21:19:58 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

9224f46121edce0f6cade8a47778df4443134023
millert <millert@openbsd.org>
1999-01-19 02:08:33 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_slkattr.c
+++ lib/libcurses/Attic/lib_slkattr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_slkattr.c,v 1.2 1998/07/23 21:19:28 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

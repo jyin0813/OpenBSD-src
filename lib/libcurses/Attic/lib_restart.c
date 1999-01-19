04febc4c69ae10d3785a5288702bf362e3ee2cb0
millert <millert@openbsd.org>
1999-01-19 02:08:20 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_restart.c
+++ lib/libcurses/Attic/lib_restart.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_restart.c,v 1.1 1998/07/23 21:19:18 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

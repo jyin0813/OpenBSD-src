23e7e1d337a1e9086b921cbafda141495a3fdee2
millert <millert@openbsd.org>
1999-01-19 02:08:50 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_tracedmp.c
+++ lib/libcurses/Attic/lib_tracedmp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_tracedmp.c,v 1.3 1998/07/23 21:19:42 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

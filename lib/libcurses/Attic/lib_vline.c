7b329648d946292c3c52a6ff2b48cb46d3668039
millert <millert@openbsd.org>
1999-01-19 02:08:57 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_vline.c
+++ lib/libcurses/Attic/lib_vline.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_vline.c,v 1.2 1998/07/23 21:19:49 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

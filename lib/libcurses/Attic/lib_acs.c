287e43105b85f0b65b7f60ca0e4d2129e71e2c0b
millert <millert@openbsd.org>
1999-01-19 02:07:27 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_acs.c
+++ lib/libcurses/Attic/lib_acs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_acs.c,v 1.5 1998/07/23 21:18:24 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

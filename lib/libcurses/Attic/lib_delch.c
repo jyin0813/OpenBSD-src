12f30f429e00913ef00a0706976e73d50760f6c9
millert <millert@openbsd.org>
1999-01-19 02:07:43 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_delch.c
+++ lib/libcurses/Attic/lib_delch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_delch.c,v 1.4 1998/07/23 21:18:38 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

3b532d22d61416f5e9ebefd3ae46d10ec3be1d64
millert <millert@openbsd.org>
1999-01-19 02:08:03 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_leaveok.c
+++ lib/libcurses/Attic/lib_leaveok.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_leaveok.c,v 1.1 1998/07/23 21:18:59 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

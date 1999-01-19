caf6e256907586314cf07c229e7826ab0be26706
millert <millert@openbsd.org>
1999-01-19 02:08:06 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_move.c
+++ lib/libcurses/Attic/lib_move.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_move.c,v 1.4 1998/07/23 21:19:02 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

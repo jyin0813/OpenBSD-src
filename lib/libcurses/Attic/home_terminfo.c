d8bf1ab34d8fcfe8a38689c67f0705a0ca508102
millert <millert@openbsd.org>
1999-01-19 02:07:25 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/home_terminfo.c
+++ lib/libcurses/Attic/home_terminfo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: home_terminfo.c,v 1.1 1998/10/31 06:30:29 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

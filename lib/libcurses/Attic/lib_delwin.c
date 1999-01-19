719df77c53474603b4c6adbcb90171cffc5f93a1
millert <millert@openbsd.org>
1999-01-19 02:07:44 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_delwin.c
+++ lib/libcurses/Attic/lib_delwin.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_delwin.c,v 1.4 1998/07/23 21:18:39 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

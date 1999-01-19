fa82fc87026e4f1affe8ad7277ed5939e650e224
millert <millert@openbsd.org>
1999-01-19 02:08:13 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_nl.c
+++ lib/libcurses/Attic/lib_nl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: lib_nl.c,v 1.1 1998/07/23 21:19:08 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

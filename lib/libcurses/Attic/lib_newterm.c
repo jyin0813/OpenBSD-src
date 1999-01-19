4e701ebeb916f8f0b73683a789d8476c53e4a567
millert <millert@openbsd.org>
1999-01-19 02:08:11 +0000

    ncurses-4.2-990116

--- lib/libcurses/Attic/lib_newterm.c
+++ lib/libcurses/Attic/lib_newterm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lib_newterm.c,v 1.5 1998/07/23 21:19:06 millert Exp $	*/
+/*	$OpenBSD: lib_newterm.c,v 1.6 1998/10/31 06:30:30 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *

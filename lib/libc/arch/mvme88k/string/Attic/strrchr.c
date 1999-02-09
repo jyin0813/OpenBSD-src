78ad314bea9057890713e2254799e4602628df2f
smurph <smurph@openbsd.org>
1999-02-09 12:23:43 +0000

    Fixed brk() and sbrk().  Removal of old mvme88k dir.

--- lib/libc/arch/mvme88k/string/Attic/strrchr.c
+++ lib/libc/arch/mvme88k/string/Attic/strrchr.c
@@ -1,6 +1,6 @@
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)strlen.c	5.5 (Berkeley) 1/26/91";*/
-static char *rcsid = "$Id: strlen.c,v 1.1 1995/10/18 21:31:11 drahn Exp $";
+static char *rcsid = "$Id: strrchr.c,v 1.1 1997/03/25 17:07:14 rahnds Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <string.h>

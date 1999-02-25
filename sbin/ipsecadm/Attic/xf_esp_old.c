d5ed0a3af245da84f8079b413683348d3066f472
angelos <angelos@openbsd.org>
1999-02-25 05:53:44 +0000

    Almost rewritten ipsecadm to use pfkey. Error reporting is a bit lacking still.

--- sbin/ipsecadm/Attic/xf_esp_old.c
+++ sbin/ipsecadm/Attic/xf_esp_old.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: xf_esp_old.c,v 1.3 1998/05/24 13:29:08 provos Exp $ */
+/* $OpenBSD: xf_esp_old.c,v 1.1 1998/11/14 23:37:21 deraadt Exp $ */
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and 

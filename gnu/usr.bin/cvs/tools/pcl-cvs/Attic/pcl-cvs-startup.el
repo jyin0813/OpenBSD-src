b2fc9f96f0223e2cb4868326acbab28311e36379
tholo <tholo@openbsd.org>
1999-03-01 04:58:16 +0000

    Integrate local changes

--- gnu/usr.bin/cvs/tools/pcl-cvs/Attic/pcl-cvs-startup.el
+++ gnu/usr.bin/cvs/tools/pcl-cvs/Attic/pcl-cvs-startup.el
@@ -1,6 +1,6 @@
 ;;;#ident "@(#)OrigId: pcl-cvs-startup.el,v 1.4 1993/05/31 18:40:33 ceder Exp "
 ;;;
-;;;#ident "@(#)cvs/contrib/pcl-cvs:$Name:  $:$Id: pcl-cvs-startup.el,v 1.1 1996/04/14 15:17:59 kfogel Exp $"
+;;;#ident "@(#)cvs/contrib/pcl-cvs:$Name:  $:$Id: pcl-cvs-startup.el,v 1.1 1996/05/06 22:20:48 tholo Exp $"
 ;;;
 (autoload 'cvs-update "pcl-cvs"
 	  "Run a 'cvs update' in the current working directory. Feed the

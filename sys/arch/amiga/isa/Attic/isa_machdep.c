199a63a3b10f9190d70d404e0d85777749a3dcb5
niklas <niklas@openbsd.org>
1999-03-06 02:17:44 +0000

    Add forgotten file, espie@ has checked it compiles

--- sys/arch/amiga/isa/Attic/isa_machdep.c
+++ sys/arch/amiga/isa/Attic/isa_machdep.c
@@ -1,8 +1,7 @@
 /*	$OpenBSD$	*/
-/*	$NetBSD: isa_machdep.c,v 1.1 1995/08/02 14:10:17 niklas Exp $	*/
 
 /*
- * Copyright (c) 1995 Niklas Hallqvist
+ * Copyright (c) 1999 Niklas Hallqvist
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,135 +29,45 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include <sys/param.h>
-#include <sys/device.h>
-#include <sys/malloc.h>
 
+#include <sys/types.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+
+#include <machine/intr.h>
+
+#include <dev/isa/isareg.h>
 #include <dev/isa/isavar.h>
 
-#include <machine/pio.h>
-
-#include <amiga/amiga/device.h>
-#include <amiga/isa/isa_intr.h>
-
-void	isaattach __P((struct device *, struct device *, void *));
-int	isamatch __P((struct device *, void *, void *));
-
 /*
- * After careful thought about this issue I decided that allowing only
- * one isabus configured into a system would be sufficient.  I'm not
- * lazy, I did the original design with possibilities of multiple ISA
- * busses, but that made porting of existing drivers a bit harder and
- * error-prone, as well as I had to write obfuscated code.  This
- * solution is more in the spirit of KISS.  --niklas@appli.se
+ * Just check to see if an IRQ is available/can be shared.
+ * 0 = interrupt not available
+ * 1 = interrupt shareable
+ * 2 = interrupt all to ourself
  */
-struct isa_link *isa;
-int isadebug = 0;
-
-struct cfdriver isacd = {
-	NULL, "isa", isamatch, isaattach,
-	DV_DULL, sizeof(struct device), 1
-};
-
 int
-isamatch(parent, cfdata, aux)
-	struct device *parent;
-	void *cfdata, *aux;
+__isa_intr_check(irq, type, intrtype)
+	int irq;
+	int type;
+	int *intrtype;
 {
-	struct cfdata *cf = cfdata;
-
-#ifdef DEBUG
-	if (isadebug)
-		printf(" isamatch");
-#endif
-
-	/* See if the unit number is valid. */
-	if (cf->cf_unit > 0)
+	if (type == IST_NONE)
 		return (0);
 
+	switch (intrtype[irq]) {
+	case IST_NONE:
+		return (2);
+		break;
+	case IST_LEVEL:
+		if (type != intrtype[irq])
+			return (0);
+		return (1);
+		break;
+	case IST_EDGE:
+	case IST_PULSE:
+		if (type != IST_NONE)
+			return (0);
+	}
 	return (1);
 }
 
-void
-isaattach(parent, self, aux)
-	struct device *parent, *self;
-	void *aux;
-{
-	struct isa_softc *sc = (struct isa_softc *)self;
-
-	isa = (struct isa_link *)aux;
-
-	printf("\n");
-
-	TAILQ_INIT (&sc->sc_subdevs);
-
-	config_scan(isascan, self);
-}
-
-void *
-isa_intr_establish(intr, type, level, ih_fun, ih_arg, ih_what)
-	int intr;
-	int type;
-	int level;
-	int (*ih_fun)(void *);
-	void *ih_arg;
-	char *ih_what;
-{
-	return (*isa_intr_fcns->isa_intr_establish)(intr, type, level,
-	    ih_fun, ih_arg, ih_what);
-}
-
-void
-isa_intr_disestablish(handler)
-	void *handler;
-{
-	(*isa_intr_fcns->isa_intr_disestablish)(handler);
-}
-
-void
-isa_outsb(port, addr, cnt)
-	int port;
-	void *addr;
-	int cnt;
-{
-	u_int8_t *p = addr;
-
-	while (cnt--)
-		outb(port, *p++);
-}
-
-void
-isa_insb(port, addr, cnt)
-	int port;
-	void *addr;
-	int cnt;
-{
-	u_int8_t *p = addr;
-
-	while (cnt--)
-		*p++ = inb(port);
-}
-
-void
-isa_outsw(port, addr, cnt)
-	int port;
-	void *addr;
-	int cnt;
-{
-	u_int16_t *p = addr;
-
-	while (cnt--)
-		outw(port, *p++);
-}
-
-void
-isa_insw(port, addr, cnt)
-	int port;
-	void *addr;
-	int cnt;
-{
-	u_int16_t *p = addr;
-
-	while (cnt--)
-		*p++ = inw(port);
-}

c9ece7fd8ba3c58727a4fc1f620d8350c18faf75
d <d@openbsd.org>
1999-03-10 15:53:48 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/stdio/Attic/fvwrite.c
+++ lib/libpthread/stdio/Attic/fvwrite.c
@@ -36,7 +36,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)fvwrite.c	5.3 (Berkeley) 5/4/91";*/
-static char *rcsid = "$Id: fvwrite.c,v 1.1 1994/02/07 22:06:03 proven Exp $";
+static char *rcsid = "$Id: fvwrite.c,v 1.1 1995/10/18 08:43:07 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <pthread.h>

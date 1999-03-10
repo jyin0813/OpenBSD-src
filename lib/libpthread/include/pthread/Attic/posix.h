6ce128389426e17d6f126c1f85857eca95172305
d <d@openbsd.org>
1999-03-10 15:53:18 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/include/pthread/Attic/posix.h
+++ lib/libpthread/include/pthread/Attic/posix.h
@@ -3,7 +3,7 @@
  *
  * Description : Convert an Ultrix-4.2 system to a more or less POSIX system.
  *
- * $Id: posix-openbsd-2.0.h,v 1.1.1.1 1998/07/21 13:19:19 peter Exp $
+ * $Id: posix.h,v 1.1 1998/07/21 19:50:19 peter Exp $
  *
  *  1.00 93/07/20 proven
  *      -Started coding this file.

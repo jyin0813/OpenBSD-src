422f1a7a90d35c7c068929fa1d2905821af22901
d <d@openbsd.org>
1999-03-10 15:53:17 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/include/pthread/Attic/fd_pipe.h
+++ lib/libpthread/include/pthread/Attic/fd_pipe.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: fd_pipe.h,v 1.50 1994/08/08 03:39:28 proven Exp $
+ * $Id: fd_pipe.h,v 1.1 1998/07/21 13:19:11 peter Exp $
  *
  * Description : The new fast ITC pipe header.
  *

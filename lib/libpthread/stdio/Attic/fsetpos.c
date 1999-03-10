4d7f1874dc0f735ae7dacca6ff2e79280146fa6a
d <d@openbsd.org>
1999-03-10 15:53:47 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/stdio/Attic/fsetpos.c
+++ lib/libpthread/stdio/Attic/fsetpos.c
@@ -36,7 +36,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)fsetpos.c	5.2 (Berkeley) 2/5/91";*/
-static char *rcsid = "$Id: fsetpos.c,v 1.1 1994/02/07 22:05:59 proven Exp $";
+static char *rcsid = "$Id: fsetpos.c,v 1.1 1995/10/18 08:43:07 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>

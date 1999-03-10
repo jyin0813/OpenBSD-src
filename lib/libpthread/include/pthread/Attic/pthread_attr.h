d1cdfe8f6f17b12a62b0aeae53f67e2a84298a1b
d <d@openbsd.org>
1999-03-10 15:53:18 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/include/pthread/Attic/pthread_attr.h
+++ lib/libpthread/include/pthread/Attic/pthread_attr.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: pthread_attr.h,v 1.58.4.1 1996/11/11 05:56:42 proven Exp $
+ * $Id: pthread_attr.h,v 1.1 1998/07/21 13:19:12 peter Exp $
  *
  * Description : Basic pthread attributes header.
  *

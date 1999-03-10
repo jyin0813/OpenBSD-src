b60b4a159e067b8541213c09465e7f650a337bdc
d <d@openbsd.org>
1999-03-10 15:53:17 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/include/pthread/Attic/kthread.h
+++ lib/libpthread/include/pthread/Attic/kthread.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: kthread.h,v 1.50 1994/08/08 03:39:36 proven Exp $
+ * $Id: kthread.h,v 1.1 1998/07/21 13:19:11 peter Exp $
  *
  * Description : Basic pthread header.
  *

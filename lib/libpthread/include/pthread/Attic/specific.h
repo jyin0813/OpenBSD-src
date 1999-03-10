208c9d65f2df673c91e200e4c3d71b82500345ef
d <d@openbsd.org>
1999-03-10 15:53:19 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/include/pthread/Attic/specific.h
+++ lib/libpthread/include/pthread/Attic/specific.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: specific.h,v 1.52 1994/09/29 06:19:25 proven Exp $
+ * $Id: specific.h,v 1.1 1998/07/21 13:19:12 peter Exp $
  *
  * Description : Thread specific data management header.
  *

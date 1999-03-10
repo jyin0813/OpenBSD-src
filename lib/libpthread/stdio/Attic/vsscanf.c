9026424bdb09c27f920e75ae75d7b9d0e4a2787d
d <d@openbsd.org>
1999-03-10 15:53:58 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/stdio/Attic/vsscanf.c
+++ lib/libpthread/stdio/Attic/vsscanf.c
@@ -36,7 +36,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)vsscanf.c	5.1 (Berkeley) 4/15/91";*/
-static char *rcsid = "$Id: vsscanf.c,v 1.1 1994/02/07 22:07:00 proven Exp $";
+static char *rcsid = "$Id: vsscanf.c,v 1.1 1995/10/18 08:43:09 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>

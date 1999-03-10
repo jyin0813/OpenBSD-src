596a1f81dac4601a82772d4898562eed9cb9cfd9
d <d@openbsd.org>
1999-03-10 15:53:43 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/stdio/Attic/feof.c
+++ lib/libpthread/stdio/Attic/feof.c
@@ -36,7 +36,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)feof.c	5.1 (Berkeley) 1/20/91";*/
-static char *rcsid = "$Id: feof.c,v 1.1 1994/02/07 22:05:30 proven Exp $";
+static char *rcsid = "$Id: feof.c,v 1.1 1995/10/18 08:43:06 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <pthread.h>

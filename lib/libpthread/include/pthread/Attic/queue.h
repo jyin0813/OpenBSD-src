40ba2be84f3b01b804f6644783b1f871c1d9a379
d <d@openbsd.org>
1999-03-10 15:53:19 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/include/pthread/Attic/queue.h
+++ lib/libpthread/include/pthread/Attic/queue.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: queue.h,v 1.50 1994/08/08 03:39:44 proven Exp $
+ * $Id: queue.h,v 1.1 1998/07/21 13:19:12 peter Exp $
  *
  * Description : mutex header.
  *

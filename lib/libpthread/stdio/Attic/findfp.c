33ef3ab716729c062e4a153ae7eb6b751be1d6e1
d <d@openbsd.org>
1999-03-10 15:53:45 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/stdio/Attic/findfp.c
+++ lib/libpthread/stdio/Attic/findfp.c
@@ -36,7 +36,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)findfp.c	5.10 (Berkeley) 2/24/91";*/
-static char *rcsid = "$Id: findfp.c,v 1.1 1994/02/07 22:05:46 proven Exp $";
+static char *rcsid = "$Id: findfp.c,v 1.1 1995/10/18 08:43:06 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <pthread.h>

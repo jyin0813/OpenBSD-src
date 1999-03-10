fefcb165c983c610e918d755b098de44413e5e4a
d <d@openbsd.org>
1999-03-10 15:53:27 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/net/Attic/getservbyname.c
+++ lib/libpthread/net/Attic/getservbyname.c
@@ -33,7 +33,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)getservbyname.c	5.7 (Berkeley) 2/24/91";*/
-static char *rcsid = "$Id: getservbyname.c,v 1.3.4.1 1996/02/09 05:39:43 ghudson Exp $";
+static char *rcsid = "$Id: getservbyname.c,v 1.1 1998/07/21 13:19:45 peter Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <netdb.h>

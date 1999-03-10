f0124d53be74bb9a9fafe18ea92645065cf25679
d <d@openbsd.org>
1999-03-10 15:53:43 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/stdio/Attic/fclose.c
+++ lib/libpthread/stdio/Attic/fclose.c
@@ -36,7 +36,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)fclose.c	5.2 (Berkeley) 2/1/91";*/
-static char *rcsid = "$Id: fclose.c,v 1.1 1994/02/07 22:05:26 proven Exp $";
+static char *rcsid = "$Id: fclose.c,v 1.1 1995/10/18 08:43:06 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <pthread.h>

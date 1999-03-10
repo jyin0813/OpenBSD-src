141def72443da1dab65244177246b202a175725e
d <d@openbsd.org>
1999-03-10 15:53:09 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/gen/Attic/getcwd.c
+++ lib/libpthread/gen/Attic/getcwd.c
@@ -33,7 +33,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)getcwd.c	5.11 (Berkeley) 2/24/91";*/
-static char *rcsid = "$Id: getcwd.c,v 1.4 1995/09/02 23:39:30 snl Exp $";
+static char *rcsid = "$Id: getcwd.c,v 1.1 1998/07/21 13:19:02 peter Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <pthread.h>

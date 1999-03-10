93c901ff87e6d14a1d7287672d6e5e79cf830cfa
d <d@openbsd.org>
1999-03-10 15:53:38 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/pthreads/Attic/mutex.c
+++ lib/libpthread/pthreads/Attic/mutex.c
@@ -36,7 +36,7 @@
  */
 
 #ifndef lint
-static const char rcsid[] = "$Id: mutex.c,v 1.3 1994/02/07 22:04:21 proven Exp $ $provenid: mutex.c,v 1.16 1994/02/07 02:19:03 proven Exp $";
+static const char rcsid[] = "$Id: mutex.c,v 1.1 1995/10/18 08:43:05 deraadt Exp $ $provenid: mutex.c,v 1.16 1994/02/07 02:19:03 proven Exp $";
 #endif
 
 #include <pthread.h>

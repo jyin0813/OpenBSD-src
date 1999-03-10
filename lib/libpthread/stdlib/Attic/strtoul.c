df767faf5ad9f8742469b84a4b9235c9e5018ff8
d <d@openbsd.org>
1999-03-10 15:54:01 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/stdlib/Attic/strtoul.c
+++ lib/libpthread/stdlib/Attic/strtoul.c
@@ -33,7 +33,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)strtoul.c	5.3 (Berkeley) 2/23/91";*/
-static char *rcsid = "$Id: strtoul.c,v 1.2 1995/08/22 22:15:04 proven Exp $";
+static char *rcsid = "$Id: strtoul.c,v 1.1 1998/07/21 13:22:19 peter Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <pthread.h>

c9f62025d107f70706b6574450ea07a283562589
d <d@openbsd.org>
1999-03-10 15:53:58 +0000

    Goodbye, MIT pthreads... you were a handy reference implementation

--- lib/libpthread/stdio/Attic/wsetup.c
+++ lib/libpthread/stdio/Attic/wsetup.c
@@ -36,7 +36,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)wsetup.c	5.2 (Berkeley) 2/24/91";*/
-static char *rcsid = "$Id: wsetup.c,v 1.1 1994/02/07 22:07:02 proven Exp $";
+static char *rcsid = "$Id: wsetup.c,v 1.1 1995/10/18 08:43:09 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <pthread.h>

fc5ec09df9e405869088e0893c1c117236175613
pjanzen <pjanzen@openbsd.org>
1999-03-12 10:02:39 +0000

    Correct documentation, remove useless code, remove duplicate documentation, don't compile with xTRACE defined, and trim things.

--- games/trek/Attic/cgetc.c
+++ games/trek/Attic/cgetc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: cgetc.c,v 1.2 1998/08/19 07:41:15 pjanzen Exp $	*/
 /*	$NetBSD: cgetc.c,v 1.3 1995/04/22 10:58:34 cgd Exp $	*/
 
 /*
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)cgetc.c	8.1 (Berkeley) 5/31/93";
 #else
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: cgetc.c,v 1.2 1998/08/19 07:41:15 pjanzen Exp $";
 #endif
 #endif /* not lint */
 

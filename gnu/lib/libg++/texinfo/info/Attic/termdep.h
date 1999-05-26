f06f7b3edb5ea41fcdea6c5c9fd50bb11f48b704
espie <espie@openbsd.org>
1999-05-26 22:39:28 +0000

    old libg++, replaced by part of egcs.

--- gnu/lib/libg++/texinfo/info/Attic/termdep.h
+++ gnu/lib/libg++/texinfo/info/Attic/termdep.h
@@ -1,5 +1,5 @@
 /* termdep.h -- System things that terminal.c depends on.
-   $Id: termdep.h,v 1.2 1997/10/30 05:55:14 law Exp $
+   $Id: termdep.h,v 1.3 1998/03/03 21:48:52 millert Exp $
 
    This file is part of GNU Info, a program for reading online documentation
    stored in Info format.

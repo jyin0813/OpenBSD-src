f5aa6a3915faec2f33b8c16a00bdd4a3ab0717e9
jakob <jakob@openbsd.org>
1999-07-29 02:41:37 +0000

    -  Merge some changes from tcpdump 3.4    -a flag; attempt to convert network and broadcast addresses to names    Improved signal handling    Miscellaneous fixes and typos    OSPF MD5 authentication support
    -  -X flag; emacs-hexl print (including ascii)
    
    -  Add ECN bits to TCP and IP headers
    
    -  IKE & IPsec (ESP & AH) support
    
    OK deraadt@

--- usr.sbin/tcpdump/lbl/Attic/os-sunos4.h
+++ usr.sbin/tcpdump/lbl/Attic/os-sunos4.h
@@ -18,7 +18,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @(#) $Header: os-sunos4.h,v 1.32 96/11/29 15:18:18 leres Exp $ (LBL)
+ * @(#) $Header: /cvs/src/usr.sbin/tcpdump/lbl/os-sunos4.h,v 1.1 1996/12/12 16:08:23 bitblt Exp $ (LBL)
  */
 
 /* Prototypes missing in SunOS 4 */

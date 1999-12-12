19a108bdb9dc9174ba2835ff7cef3cf7d227e6b9
csapuntz <csapuntz@openbsd.org>
1999-12-12 04:25:07 +0000

    Remove old IDE driver

--- sys/dev/isa/Attic/wd.c
+++ sys/dev/isa/Attic/wd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wd.c,v 1.40 1999/05/09 20:40:43 weingart Exp $	*/
+/*	$OpenBSD: wd.c,v 1.41 1999/08/10 23:09:49 deraadt Exp $	*/
 /*	$NetBSD: wd.c,v 1.150 1996/05/12 23:54:03 mycroft Exp $ */
 
 /*

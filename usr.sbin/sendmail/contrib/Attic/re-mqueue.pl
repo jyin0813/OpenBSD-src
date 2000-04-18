489de4046ff29aae0539407d0b23fc8a644283d4
millert <millert@openbsd.org>
2000-04-18 10:57:09 +0000

    remove sendmail 8.9.3

--- usr.sbin/sendmail/contrib/Attic/re-mqueue.pl
+++ usr.sbin/sendmail/contrib/Attic/re-mqueue.pl
@@ -84,7 +84,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#)$Id: re-mqueue,v 1.3 1995/05/25 18:14:53 p-pomes Exp $
+# @(#)$Id: re-mqueue.pl,v 1.2 1998/07/12 19:43:49 millert Exp $
 
 require "syslog.pl";
 

3914be848a540db7bebb30cb66949333a2344e0e
rahnds <rahnds@openbsd.org>
2000-09-06 07:46:45 +0000

    vgafb is removed, was only used on powerpc, and is not in a state to be an MI device. This old version was based on old wscons and will not build.

--- sys/dev/pci/Attic/vgafb_pci.c
+++ sys/dev/pci/Attic/vgafb_pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vgafb_pci.c,v 1.2 1998/09/27 05:29:59 rahnds Exp $	*/
+/*	$OpenBSD: vgafb_pci.c,v 1.3 1998/10/09 02:00:52 rahnds Exp $	*/
 /*	$NetBSD: vga_pci.c,v 1.4 1996/12/05 01:39:38 cgd Exp $	*/
 
 /*

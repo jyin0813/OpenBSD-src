946740b5d32742207f197464a8ad05b27a2e4cfc
rahnds <rahnds@openbsd.org>
2000-09-06 08:23:25 +0000

    The ISA based systems are not currently supported. When or If they are supported in the future, some of this code can be revived, or rewritten as appropriate. But since it not maintained and will likely not compile, there is little reason to keep it around for now.

--- sys/arch/powerpc/isa/Attic/spkrreg.h
+++ sys/arch/powerpc/isa/Attic/spkrreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: spkrreg.h,v 1.3 1996/10/30 22:39:43 niklas Exp $	*/
+/*	$OpenBSD: spkrreg.h,v 1.1 1998/09/27 03:55:58 rahnds Exp $	*/
 /*	$NetBSD: spkrreg.h,v 1.1 1996/04/12 01:54:46 cgd Exp $	*/
 
 /*

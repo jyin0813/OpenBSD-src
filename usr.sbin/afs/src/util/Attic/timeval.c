9ba11a49f45cf81a3ced28780eb19c4f3dd9c4da
art <art@openbsd.org>
2000-09-11 20:41:40 +0000

    New Arla userland from Arla between 0.34.2 and current in arla cvs. Too many new features and fixes to mention here.

--- usr.sbin/afs/src/util/Attic/timeval.c
+++ usr.sbin/afs/src/util/Attic/timeval.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: timeval.c,v 1.1 1998/09/14 21:53:25 art Exp $	*/
 /*
  * Copyright (c) 1995, 1996, 1997 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).

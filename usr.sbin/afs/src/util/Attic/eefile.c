4cf620b6a93e520a1b3c6230bbaacad61390d7a3
art <art@openbsd.org>
2000-09-11 20:41:37 +0000

    New Arla userland from Arla between 0.34.2 and current in arla cvs. Too many new features and fixes to mention here.

--- usr.sbin/afs/src/util/Attic/eefile.c
+++ usr.sbin/afs/src/util/Attic/eefile.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: eefile.c,v 1.1 1999/04/30 01:59:16 art Exp $	*/
 /*
  * Copyright (c) 1995, 1996, 1997, 1999 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).

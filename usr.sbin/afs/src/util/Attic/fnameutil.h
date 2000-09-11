57cc49e518deef31d89a2ff1a804e8adbc3d9051
art <art@openbsd.org>
2000-09-11 20:41:37 +0000

    New Arla userland from Arla between 0.34.2 and current in arla cvs. Too many new features and fixes to mention here.

--- usr.sbin/afs/src/util/Attic/fnameutil.h
+++ usr.sbin/afs/src/util/Attic/fnameutil.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: fnameutil.h,v 1.1 1998/09/14 21:53:22 art Exp $	*/
 /*
  * Copyright (c) 1995, 1996, 1997, 1998 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).

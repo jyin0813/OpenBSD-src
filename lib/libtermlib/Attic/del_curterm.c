0ce48314f7bd9bebe56fa1a96b4c7d05533ca0f1
millert <millert@openbsd.org>
2000-12-06 00:57:50 +0000

    We haven't used this in years, remove it.

--- lib/libtermlib/Attic/del_curterm.c
+++ lib/libtermlib/Attic/del_curterm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: del_curterm.c,v 1.1.1.1 1996/05/31 05:40:02 tholo Exp $	*/
+/*	$OpenBSD: del_curterm.c,v 1.2 1996/08/31 02:40:30 tholo Exp $	*/
 
 /*
  * Copyright (c) 1996 SigmaSoft, Th. Lockert <tholo@sigmasoft.com>
@@ -31,7 +31,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: del_curterm.c,v 1.1.1.1 1996/05/31 05:40:02 tholo Exp $";
+static char rcsid[] = "$OpenBSD: del_curterm.c,v 1.2 1996/08/31 02:40:30 tholo Exp $";
 #endif
 
 #include <stdlib.h>

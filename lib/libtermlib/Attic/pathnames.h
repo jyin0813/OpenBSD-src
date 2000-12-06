171b19661234d81cb1a9872c1faac5057174b462
millert <millert@openbsd.org>
2000-12-06 00:57:51 +0000

    We haven't used this in years, remove it.

--- lib/libtermlib/Attic/pathnames.h
+++ lib/libtermlib/Attic/pathnames.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pathnames.h,v 1.1.1.1 1996/05/31 05:40:02 tholo Exp $	*/
+/*	$OpenBSD: pathnames.h,v 1.2 1996/07/22 03:13:54 tholo Exp $	*/
 
 /*
  * Copyright (c) 1996 SigmaSoft, Th. Lockert <tholo@sigmasoft.com>

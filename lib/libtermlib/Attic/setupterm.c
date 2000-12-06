51d39bf018982302d6c93e104ae9d9d08837edb3
millert <millert@openbsd.org>
2000-12-06 00:57:52 +0000

    We haven't used this in years, remove it.

--- lib/libtermlib/Attic/setupterm.c
+++ lib/libtermlib/Attic/setupterm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: setupterm.c,v 1.1.1.1 1996/05/31 05:40:02 tholo Exp $	*/
+/*	$OpenBSD: setupterm.c,v 1.2 1996/06/02 20:19:29 tholo Exp $	*/
 
 /*
  * Copyright (c) 1996 SigmaSoft, Th. Lockert <tholo@sigmasoft.com>
@@ -31,7 +31,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: setupterm.c,v 1.1.1.1 1996/05/31 05:40:02 tholo Exp $";
+static char rcsid[] = "$OpenBSD: setupterm.c,v 1.2 1996/06/02 20:19:29 tholo Exp $";
 #endif
 
 #include <stdlib.h>

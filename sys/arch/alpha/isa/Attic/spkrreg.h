91fbf72c9e9fbb68c1e09710a91939b7aaaca4f8
art <art@openbsd.org>
2001-02-03 19:19:47 +0000

    dead file

--- sys/arch/alpha/isa/Attic/spkrreg.h
+++ sys/arch/alpha/isa/Attic/spkrreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: spkrreg.h,v 1.1 1996/04/12 01:54:46 cgd Exp $	*/
+/*	$OpenBSD: spkrreg.h,v 1.3 1996/10/30 22:39:43 niklas Exp $	*/
 /*	$NetBSD: spkrreg.h,v 1.1 1996/04/12 01:54:46 cgd Exp $	*/
 
 /*

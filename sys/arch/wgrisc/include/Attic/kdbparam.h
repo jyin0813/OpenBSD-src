6c49cf91f7e0deac4f531ad7424093114fa6020d
art <art@openbsd.org>
2001-02-07 14:46:55 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/wgrisc/include/Attic/kdbparam.h
+++ sys/arch/wgrisc/include/Attic/kdbparam.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kdbparam.h,v 1.1.1.1 1996/06/24 09:07:17 pefo Exp $	*/
+/*	$OpenBSD: kdbparam.h,v 1.1 1997/02/06 16:02:43 pefo Exp $	*/
 /*	$NetBSD: kdbparam.h,v 1.4 1994/10/26 21:09:42 cgd Exp $	*/
 
 /*-

d30d45f1b914f9f2dbad1c1032d0696ec839b359
art <art@openbsd.org>
2001-02-07 14:47:00 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/wgrisc/include/Attic/vmparam.h
+++ sys/arch/wgrisc/include/Attic/vmparam.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmparam.h,v 1.2 1996/07/30 20:24:30 pefo Exp $	*/
+/*	$OpenBSD: vmparam.h,v 1.1 1997/02/06 16:02:43 pefo Exp $	*/
 /*	$NetBSD: vmparam.h,v 1.5 1994/10/26 21:10:10 cgd Exp $	*/
 
 /*

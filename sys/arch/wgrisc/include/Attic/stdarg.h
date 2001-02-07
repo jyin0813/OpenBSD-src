b4d701c9bd2a38ce159d231ab9c3971d68f80cb1
art <art@openbsd.org>
2001-02-07 14:46:59 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/wgrisc/include/Attic/stdarg.h
+++ sys/arch/wgrisc/include/Attic/stdarg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: stdarg.h,v 1.2 1996/07/30 20:24:29 pefo Exp $	*/
+/*	$OpenBSD: stdarg.h,v 1.1 1997/02/06 16:02:43 pefo Exp $	*/
 /*	$NetBSD: stdarg.h,v 1.7 1995/03/28 18:19:28 jtc Exp $	*/
 
 /*-

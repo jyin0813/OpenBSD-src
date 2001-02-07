02e9e0d7b5be29420c86ee09463f66f47d48c498
art <art@openbsd.org>
2001-02-07 14:46:07 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/kbus/dev/Attic/dmavar.h
+++ sys/arch/kbus/dev/Attic/dmavar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dmavar.h,v 1.3 1996/04/28 11:24:43 deraadt Exp $ */
+/*	$OpenBSD: dmavar.h,v 1.1 1997/10/14 07:25:30 gingold Exp $ */
 
 /*
  * Copyright (c) 1982, 1990 The Regents of the University of California.

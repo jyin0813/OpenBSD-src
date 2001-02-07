e37345ca19c67c6aed3a17f25b73fbcc0d4e0957
art <art@openbsd.org>
2001-02-07 14:47:06 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/wgrisc/wgrisc/Attic/disksubr.c
+++ sys/arch/wgrisc/wgrisc/Attic/disksubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disksubr.c,v 1.11 1999/01/08 04:29:11 millert Exp $	*/
+/*	$OpenBSD: disksubr.c,v 1.12 2000/10/18 21:00:41 mickey Exp $	*/
 /*	$NetBSD: disksubr.c,v 1.21 1996/05/03 19:42:03 christos Exp $	*/
 
 /*

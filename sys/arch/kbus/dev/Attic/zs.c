48d31785ddbf704694d24bb8d7931b329b65cd99
art <art@openbsd.org>
2001-02-07 14:46:10 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/kbus/dev/Attic/zs.c
+++ sys/arch/kbus/dev/Attic/zs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: zs.c,v 1.1.1.1 1997/10/14 07:25:29 gingold Exp $	*/
+/*	$OpenBSD: zs.c,v 1.2 1999/01/11 05:11:28 millert Exp $	*/
 /*	$NetBSD: zs.c,v 1.42 1996/11/20 18:57:03 gwr Exp $	*/
 
 /*-

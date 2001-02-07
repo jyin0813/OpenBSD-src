61f40f0779ce2ba732b77e6a35151846e49f386d
art <art@openbsd.org>
2001-02-07 14:46:58 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/wgrisc/include/Attic/reg.h
+++ sys/arch/wgrisc/include/Attic/reg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: reg.h,v 1.1.1.1 1996/06/24 09:07:18 pefo Exp $	*/
+/*	$OpenBSD: reg.h,v 1.1 1997/02/06 16:02:43 pefo Exp $	*/
 /*	$NetBSD: reg.h,v 1.6 1995/12/20 02:00:27 jonathan Exp $	*/
 
 /*

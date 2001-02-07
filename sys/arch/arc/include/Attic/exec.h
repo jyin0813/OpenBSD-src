96c26a6465cd135f93ac69c6ff19882c6683f924
art <art@openbsd.org>
2001-02-07 14:45:07 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/arc/include/Attic/exec.h
+++ sys/arch/arc/include/Attic/exec.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec.h,v 1.2 1996/12/23 02:42:26 deraadt Exp $	*/
+/*	$OpenBSD: exec.h,v 1.3 2001/01/22 14:51:03 art Exp $	*/
 /*	$NetBSD: exec.h,v 1.5 1994/10/26 21:09:39 cgd Exp $	*/
 
 /*-

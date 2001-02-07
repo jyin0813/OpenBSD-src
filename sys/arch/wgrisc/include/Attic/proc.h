2be0a8e610608c627167141273b08e6958a0b3d8
art <art@openbsd.org>
2001-02-07 14:46:57 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/wgrisc/include/Attic/proc.h
+++ sys/arch/wgrisc/include/Attic/proc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: proc.h,v 1.2 1996/07/30 20:24:27 pefo Exp $	*/
+/*	$OpenBSD: proc.h,v 1.1 1997/02/06 16:02:43 pefo Exp $	*/
 /*	$NetBSD: proc.h,v 1.4 1994/10/26 21:09:52 cgd Exp $	*/
 
 /*

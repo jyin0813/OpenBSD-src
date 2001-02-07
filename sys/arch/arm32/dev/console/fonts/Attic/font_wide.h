ab5c68b0a2d40e07035909dacc45c2921abc2cf7
art <art@openbsd.org>
2001-02-07 14:45:26 +0000

    We need to loose some weight. If someone feels very opposed to this, the code is left in the Attic. Just revive and _make_it_work_.

--- sys/arch/arm32/dev/console/fonts/Attic/font_wide.h
+++ sys/arch/arm32/dev/console/fonts/Attic/font_wide.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: font_wide.h,v 1.1 1996/01/31 23:20:22 mark Exp $ */
+/* $OpenBSD: font_wide.h,v 1.2 2000/03/03 00:54:48 todd Exp $ */
 /* $NetBSD: font_wide.h,v 1.1 1996/01/31 23:20:22 mark Exp $ */
 
 /*

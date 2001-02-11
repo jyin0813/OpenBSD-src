d58efaf87428a6e743dfd24ccfac68a9545e3656
markus <markus@openbsd.org>
2001-02-11 19:59:24 +0000

    1) clean up the MAC support for SSH-2 2) allow you to specify the MAC with 'ssh -m' 3) or the 'MACs' keyword in ssh(d)_config 4) add hmac-{md5,sha1}-96 	ok stevesk@, provos@

--- usr.bin/ssh/Attic/hmac.c
+++ usr.bin/ssh/Attic/hmac.c
@@ -23,7 +23,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: hmac.c,v 1.6 2001/01/21 19:05:49 markus Exp $");
+RCSID("$OpenBSD: hmac.c,v 1.7 2001/02/08 19:30:51 itojun Exp $");
 
 #include "xmalloc.h"
 #include "getput.h"

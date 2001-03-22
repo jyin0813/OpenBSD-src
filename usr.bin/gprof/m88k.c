7c846597cc9e60d42e251fb09d7bea5ceedd9d07
mickey <mickey@openbsd.org>
2001-03-22 12:18:30 +0000

    _wall, err/warn; dummy hppa part, like others

--- usr.bin/gprof/m88k.c
+++ usr.bin/gprof/m88k.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD: m88k.c,v 1.2 1996/06/26 05:33:54 deraadt Exp $	*/
+/*	$OpenBSD: m88k.c,v 1.1 2000/12/28 23:44:37 smurph Exp $	*/
 /*	$NetBSD: m88k.c,v 1.4 1995/04/19 07:16:07 cgd Exp $	*/
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: m88k.c,v 1.2 1996/06/26 05:33:54 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: m88k.c,v 1.1 2000/12/28 23:44:37 smurph Exp $";
 #endif /* not lint */
 
 #include "gprof.h"
@@ -10,6 +10,7 @@ static char rcsid[] = "$OpenBSD: m88k.c,v 1.2 1996/06/26 05:33:54 deraadt Exp $"
 /*
  * gprof -c isn't currently supported...
  */
+void
 findcall( parentp , p_lowpc , p_highpc )
     nltype		*parentp;
     unsigned long	p_lowpc;

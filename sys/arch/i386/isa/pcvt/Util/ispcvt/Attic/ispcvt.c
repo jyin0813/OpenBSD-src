eba5e2e003086514b44eb20b05758a68948dacd2
mickey <mickey@openbsd.org>
2001-05-30 04:09:35 +0000

    remove this used to be great console driver; x11 builds w/o it all fine; aaron@, todd@, matthieu@ ok

--- sys/arch/i386/isa/pcvt/Util/ispcvt/Attic/ispcvt.c
+++ sys/arch/i386/isa/pcvt/Util/ispcvt/Attic/ispcvt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ispcvt.c,v 1.9 2000/01/19 15:51:54 aaron Exp $	*/
+/*	$OpenBSD: ispcvt.c,v 1.10 2000/12/07 18:19:11 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1992, 1995 Hellmuth Michaelis

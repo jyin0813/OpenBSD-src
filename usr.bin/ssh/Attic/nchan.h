970a9d98fbbd7645643d818213cd4274752b3e94
markus <markus@openbsd.org>
2001-05-30 18:55:11 +0000

    channel layer cleanup: merge header files and split .c files

--- usr.bin/ssh/Attic/nchan.h
+++ usr.bin/ssh/Attic/nchan.h
@@ -22,7 +22,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* RCSID("$OpenBSD: nchan.h,v 1.11 2001/05/04 23:47:34 markus Exp $"); */
+/* RCSID("$OpenBSD: nchan.h,v 1.12 2001/05/28 23:14:50 markus Exp $"); */
 
 #ifndef NCHAN_H
 #define NCHAN_H

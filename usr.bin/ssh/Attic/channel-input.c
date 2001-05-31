4fd2d1a899b72d693e0a3df21c5667af1c3a468a
markus <markus@openbsd.org>
2001-05-31 16:30:13 +0000

    undo the .c file split, just merge the header and keep the cvs history

--- usr.bin/ssh/Attic/channel-input.c
+++ usr.bin/ssh/Attic/channel-input.c
@@ -34,7 +34,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: channels.c,v 1.119 2001/05/28 23:25:24 markus Exp $");
+RCSID("$OpenBSD: channel-input.c,v 1.1 2001/05/30 12:55:07 markus Exp $");
 
 #include "ssh1.h"
 #include "ssh2.h"

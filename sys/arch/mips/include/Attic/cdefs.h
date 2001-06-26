b9b18c3b6955de420b736442fdf1fc0aa2f69ff9
brad <brad@openbsd.org>
2001-06-26 09:34:05 +0000

    bye bye -- Ok'd by deraadt@ & ericj@

--- sys/arch/mips/include/Attic/cdefs.h
+++ sys/arch/mips/include/Attic/cdefs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cdefs.h,v 1.1 1998/01/28 11:14:37 pefo Exp $	*/
+/*	$OpenBSD: cdefs.h,v 1.2 1999/01/27 04:46:04 imp Exp $	*/
 /*	$NetBSD: cdefs.h,v 1.3 1995/05/03 06:04:54 mellon Exp $	*/
 
 /*

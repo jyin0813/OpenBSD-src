6cde172a0deab9296fed56964bf607c38f54eb2b
brad <brad@openbsd.org>
2001-08-20 02:44:32 +0000

    remove unused bits and pieces. -- Ok'd by art@

--- lib/libc/arch/ns32k/gen/Attic/fpsetsticky.c
+++ lib/libc/arch/ns32k/gen/Attic/fpsetsticky.c
@@ -4,7 +4,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: fpsetsticky.c,v 1.2 1996/08/19 08:16:45 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <ieeefp.h>

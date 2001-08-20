e64fa064d95d7449b8b488f1f9001ef39c561658
brad <brad@openbsd.org>
2001-08-20 02:44:32 +0000

    remove unused bits and pieces. -- Ok'd by art@

--- lib/libc/arch/ns32k/gen/Attic/infinity.c
+++ lib/libc/arch/ns32k/gen/Attic/infinity.c
@@ -1,7 +1,7 @@
 /* infinity.c */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: infinity.c,v 1.3 1996/08/19 08:16:47 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <math.h>

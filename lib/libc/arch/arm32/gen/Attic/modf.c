7b665289cb98ad12d50a8718efee1d667bff3c80
brad <brad@openbsd.org>
2001-08-20 02:44:31 +0000

    remove unused bits and pieces. -- Ok'd by art@

--- lib/libc/arch/arm32/gen/Attic/modf.c
+++ lib/libc/arch/arm32/gen/Attic/modf.c
@@ -26,7 +26,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD$";
+static char *rcsid = "$OpenBSD: modf.c,v 1.2 1996/08/19 08:11:44 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>

616962cd75286f370301bca2dcf6df33929f6fea
brad <brad@openbsd.org>
2001-08-20 02:44:32 +0000

    remove unused bits and pieces. -- Ok'd by art@

--- lib/libc/arch/ns32k/string/Attic/bzero.c
+++ lib/libc/arch/ns32k/string/Attic/bzero.c
@@ -25,7 +25,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: bzero.c,v 1.2 1996/08/19 08:17:02 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 void

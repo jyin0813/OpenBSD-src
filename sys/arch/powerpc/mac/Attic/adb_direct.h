3a5b65d4d3550be2eadf3a00ec83702b2c321dce
drahn <drahn@openbsd.org>
2001-09-01 21:49:59 +0000

    The "powerpc" port which has supported the newer Apple Macintosh powerpc based is being renamed to macppc. This is to allow sharing of common code between different powerpc base platforms.
    Most of the work involved in the renaming process was performed by miod@
    
    Files moved from powerpc/mac to macppc/dev

--- sys/arch/powerpc/mac/Attic/adb_direct.h
+++ sys/arch/powerpc/mac/Attic/adb_direct.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: adb_direct.h,v 1.2 2001/07/04 08:38:49 niklas Exp $	*/
+/*	$OpenBSD: adb_direct.h,v 1.3 2001/07/09 03:30:19 mickey Exp $	*/
 /*	$NetBSD: adb_direct.h,v 1.1 1998/05/15 10:15:47 tsubai Exp $	*/
 
 /*

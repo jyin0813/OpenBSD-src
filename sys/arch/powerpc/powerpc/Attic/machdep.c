63ade137099f5c5c4fc86229c0103ea77d48e593
drahn <drahn@openbsd.org>
2001-09-01 21:44:20 +0000

    The "powerpc" port which has supported the newer Apple Macintosh powerpc based is being renamed to macppc. This is to allow sharing of common code between different powerpc base platforms.
    Most of the work involved in the renaming process was performed by miod@
    
    Files moved from powerpc/powerpc to macppc/macppc
    
    This moves hardware specific files from the common directory to the
    platform specific directory. This leaves common files.
    With this change all of the debugger (db_) files have been moved to
    the platform specific directory. The debugger should be reconsidered
    and commonized.

--- sys/arch/powerpc/powerpc/Attic/machdep.c
+++ sys/arch/powerpc/powerpc/Attic/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.79 2001/08/23 12:02:04 art Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.80 2001/08/23 14:01:03 art Exp $	*/
 /*	$NetBSD: machdep.c,v 1.4 1996/10/16 19:33:11 ws Exp $	*/
 
 /*

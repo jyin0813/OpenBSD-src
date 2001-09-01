605b6afbe6e120b29bce4b37037451b2274e53dd
drahn <drahn@openbsd.org>
2001-09-01 21:50:00 +0000

    The "powerpc" port which has supported the newer Apple Macintosh powerpc based is being renamed to macppc. This is to allow sharing of common code between different powerpc base platforms.
    Most of the work involved in the renaming process was performed by miod@
    
    Files moved from powerpc/mac to macppc/dev

--- sys/arch/powerpc/mac/Attic/gpio.c
+++ sys/arch/powerpc/mac/Attic/gpio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gpio.c,v 1.3 2001/07/04 08:38:51 niklas Exp $	*/
+/*	$OpenBSD: gpio.c,v 1.4 2001/07/09 03:30:20 mickey Exp $	*/
 /*	$NetBSD: gpio.c,v 1.2 2001/02/27 05:16:33 matt Exp $	*/
 
 /*-

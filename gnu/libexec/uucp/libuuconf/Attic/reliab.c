ebb1c4dde9e99bb5d22d615d52e62720554157ab
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/reliab.c
+++ gnu/libexec/uucp/libuuconf/Attic/reliab.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_reliab_rcsid[] = "$Id: reliab.c,v 1.3 1995/08/24 05:21:49 jtc Exp $";
+const char _uuconf_reliab_rcsid[] = "$Id: reliab.c,v 1.1 1995/10/18 08:38:35 deraadt Exp $";
 #endif
 
 /* Handle the "seven-bit" command for a port or a dialer.  The pvar

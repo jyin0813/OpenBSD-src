d8a8f050385505f96301f468a2c875251fc52489
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:33 +0000

    uucp goes away

--- gnu/libexec/uucp/uulog/Attic/uulog.c
+++ gnu/libexec/uucp/uulog/Attic/uulog.c
@@ -26,7 +26,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char uulog_rcsid[] = "$Id: uulog.c,v 1.3 1995/08/24 05:23:22 jtc Exp $";
+const char uulog_rcsid[] = "$Id: uulog.c,v 1.1 1995/10/18 08:38:43 deraadt Exp $";
 #endif
 
 #include <ctype.h>

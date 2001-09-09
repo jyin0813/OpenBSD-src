21806a9b0a2533566d044153dc1d3ee16eeff9b2
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/tcalou.c
+++ gnu/libexec/uucp/libuuconf/Attic/tcalou.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_tcalou_rcsid[] = "$Id: tcalou.c,v 1.3 1995/08/24 05:22:02 jtc Exp $";
+const char _uuconf_tcalou_rcsid[] = "$Id: tcalou.c,v 1.1 1995/10/18 08:38:36 deraadt Exp $";
 #endif
 
 #include <errno.h>

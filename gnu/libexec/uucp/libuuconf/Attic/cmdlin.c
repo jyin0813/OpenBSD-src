5d3e2d9a545d1526508108dc7832750fcbeb48b3
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:31 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/cmdlin.c
+++ gnu/libexec/uucp/libuuconf/Attic/cmdlin.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_cmdlin_rcsid[] = "$Id: cmdlin.c,v 1.3 1995/08/24 05:20:57 jtc Exp $";
+const char _uuconf_cmdlin_rcsid[] = "$Id: cmdlin.c,v 1.1 1995/10/18 08:38:33 deraadt Exp $";
 #endif
 
 #include <errno.h>

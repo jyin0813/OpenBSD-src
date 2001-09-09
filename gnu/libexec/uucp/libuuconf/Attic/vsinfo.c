703f9c67898ffebce43a3dd5fc330134d8154544
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/vsinfo.c
+++ gnu/libexec/uucp/libuuconf/Attic/vsinfo.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_vsinfo_rcsid[] = "$Id: vsinfo.c,v 1.3 1995/08/24 05:22:28 jtc Exp $";
+const char _uuconf_vsinfo_rcsid[] = "$Id: vsinfo.c,v 1.1 1995/10/18 08:38:37 deraadt Exp $";
 #endif
 
 #include <errno.h>

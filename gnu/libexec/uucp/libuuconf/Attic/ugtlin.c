6791ee38de11c4b3f623233005509ffddbb305f8
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/ugtlin.c
+++ gnu/libexec/uucp/libuuconf/Attic/ugtlin.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_ugtlin_rcsid[] = "$Id: ugtlin.c,v 1.3 1995/08/24 05:22:21 jtc Exp $";
+const char _uuconf_ugtlin_rcsid[] = "$Id: ugtlin.c,v 1.1 1995/10/18 08:38:37 deraadt Exp $";
 #endif
 
 /* Read a line from a file with backslash continuations.  This updates

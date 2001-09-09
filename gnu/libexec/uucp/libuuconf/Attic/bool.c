7bc2d6b4969935d2fe8fae62212d40ff04495afa
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:31 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/bool.c
+++ gnu/libexec/uucp/libuuconf/Attic/bool.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_bool_rcsid[] = "$Id: bool.c,v 1.3 1995/08/24 05:20:50 jtc Exp $";
+const char _uuconf_bool_rcsid[] = "$Id: bool.c,v 1.1 1995/10/18 08:38:33 deraadt Exp $";
 #endif
 
 /* Parse a boolean string into a variable.  This is called by

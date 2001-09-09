6610037e528960858dc23cea8c2787ccf24b13fd
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/snams.c
+++ gnu/libexec/uucp/libuuconf/Attic/snams.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_snams_rcsid[] = "$Id: snams.c,v 1.3 1995/08/24 05:21:54 jtc Exp $";
+const char _uuconf_snams_rcsid[] = "$Id: snams.c,v 1.1 1995/10/18 08:38:35 deraadt Exp $";
 #endif
 
 /* Get all known system names.  */

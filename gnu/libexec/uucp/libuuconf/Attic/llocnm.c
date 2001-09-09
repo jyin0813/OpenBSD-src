a2a81fe22620c02b0919591840940999f724757b
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/llocnm.c
+++ gnu/libexec/uucp/libuuconf/Attic/llocnm.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_llocnm_rcsid[] = "$Id: llocnm.c,v 1.3 1995/08/24 05:21:34 jtc Exp $";
+const char _uuconf_llocnm_rcsid[] = "$Id: llocnm.c,v 1.1 1995/10/18 08:38:34 deraadt Exp $";
 #endif
 
 #include <errno.h>

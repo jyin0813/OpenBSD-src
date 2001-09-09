db4d6d9d1301341a65c53957a629ad09aeb6798b
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:30 +0000

    uucp goes away

--- gnu/libexec/uucp/common_sources/Attic/util.c
+++ gnu/libexec/uucp/common_sources/Attic/util.c
@@ -26,7 +26,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char util_rcsid[] = "$Id: util.c,v 1.3 1995/08/24 05:19:08 jtc Exp $";
+const char util_rcsid[] = "$Id: util.c,v 1.1 1995/10/18 08:38:26 deraadt Exp $";
 #endif
 
 #include <ctype.h>

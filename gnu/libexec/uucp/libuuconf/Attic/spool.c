2c8a460bea6ea29024625feb6f311d88cc3d0475
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/spool.c
+++ gnu/libexec/uucp/libuuconf/Attic/spool.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_spool_rcsid[] = "$Id: spool.c,v 1.3 1995/08/24 05:21:56 jtc Exp $";
+const char _uuconf_spool_rcsid[] = "$Id: spool.c,v 1.1 1995/10/18 08:38:35 deraadt Exp $";
 #endif
 
 /* Get the name of the UUCP spool directory.  */

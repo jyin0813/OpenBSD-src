743ab09bb5d1833dfaea81bcbde64412c2e7b043
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/uucico/Attic/xcmd.c
+++ gnu/libexec/uucp/uucico/Attic/xcmd.c
@@ -26,7 +26,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char xcmd_rcsid[] = "$Id: xcmd.c,v 1.3 1995/08/24 05:23:13 jtc Exp $";
+const char xcmd_rcsid[] = "$Id: xcmd.c,v 1.1 1995/10/18 08:38:42 deraadt Exp $";
 #endif
 
 #include <errno.h>

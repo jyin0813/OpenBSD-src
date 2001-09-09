1b9966f677f8493e6549f46a41a7bf63a9b4e6b0
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:31 +0000

    uucp goes away

--- gnu/libexec/uucp/libunix/Attic/pipe.c
+++ gnu/libexec/uucp/libunix/Attic/pipe.c
@@ -27,7 +27,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char pipe_rcsid[] = "$Id: pipe.c,v 1.2 1995/08/24 05:20:05 jtc Exp $";
+const char pipe_rcsid[] = "$Id: pipe.c,v 1.1 1995/10/18 08:38:29 deraadt Exp $";
 #endif
 
 #include "uudefs.h"

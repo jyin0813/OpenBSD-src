3f3d14d30ca680082d32918e269c55d3abcb5546
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/iniglb.c
+++ gnu/libexec/uucp/libuuconf/Attic/iniglb.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_iniglb_rcsid[] = "$Id: iniglb.c,v 1.3 1995/08/24 05:21:28 jtc Exp $";
+const char _uuconf_iniglb_rcsid[] = "$Id: iniglb.c,v 1.1 1995/10/18 08:38:34 deraadt Exp $";
 #endif
 
 #include <errno.h>

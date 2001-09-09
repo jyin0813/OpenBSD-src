4ec669e97bd0d6982857b6716b4ec6323150494f
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/uucico/Attic/send.c
+++ gnu/libexec/uucp/uucico/Attic/send.c
@@ -26,7 +26,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char send_rcsid[] = "$Id: send.c,v 1.3 1995/08/24 05:23:05 jtc Exp $";
+const char send_rcsid[] = "$Id: send.c,v 1.1 1995/10/18 08:38:41 deraadt Exp $";
 #endif
 
 #include <errno.h>

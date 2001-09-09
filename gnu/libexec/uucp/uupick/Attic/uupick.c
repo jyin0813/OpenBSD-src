727f5f2c218fbbe50ab706f6e60991aa8bbaf4de
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:33 +0000

    uucp goes away

--- gnu/libexec/uucp/uupick/Attic/uupick.c
+++ gnu/libexec/uucp/uupick/Attic/uupick.c
@@ -26,7 +26,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char uupick_rcsid[] = "$Id: uupick.c,v 1.3 1995/08/24 05:23:26 jtc Exp $";
+const char uupick_rcsid[] = "$Id: uupick.c,v 1.1 1995/10/18 08:38:43 deraadt Exp $";
 #endif
 
 #include <errno.h>

c14a42bc4483960fda157e1e8ff1882c0891af68
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:31 +0000

    uucp goes away

--- gnu/libexec/uucp/libunix/Attic/xqtfil.c
+++ gnu/libexec/uucp/libunix/Attic/xqtfil.c
@@ -26,7 +26,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char xqtfil_rcsid[] = "$Id: xqtfil.c,v 1.3 1995/08/24 05:20:34 jtc Exp $";
+const char xqtfil_rcsid[] = "$Id: xqtfil.c,v 1.1 1995/10/18 08:38:32 deraadt Exp $";
 #endif
 
 #include "uudefs.h"

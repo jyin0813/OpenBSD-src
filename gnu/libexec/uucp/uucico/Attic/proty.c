49e9cf05a7523852a903ff1a89569f6f59a7678c
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/uucico/Attic/proty.c
+++ gnu/libexec/uucp/uucico/Attic/proty.c
@@ -23,7 +23,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char proty_id[] = "$Id: proty.c,v 1.1 1995/08/24 05:23:00 jtc Exp $";
+const char proty_id[] = "$Id: proty.c,v 1.1 1995/10/18 08:38:40 deraadt Exp $";
 #endif
 
 #include "uudefs.h"

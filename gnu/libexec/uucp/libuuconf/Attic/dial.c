93754657499094d3a5a7e8a786a69a382826a644
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/dial.c
+++ gnu/libexec/uucp/libuuconf/Attic/dial.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_dial_rcsid[] = "$Id: dial.c,v 1.3 1995/08/24 05:21:02 jtc Exp $";
+const char _uuconf_dial_rcsid[] = "$Id: dial.c,v 1.1 1995/10/18 08:38:33 deraadt Exp $";
 #endif
 
 /* Find a dialer by name.  */

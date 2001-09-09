a37364385c36df0ca5e795d2ca8f59c0d3432d75
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/vport.c
+++ gnu/libexec/uucp/libuuconf/Attic/vport.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_vport_rcsid[] = "$Id: vport.c,v 1.3 1995/08/24 05:22:26 jtc Exp $";
+const char _uuconf_vport_rcsid[] = "$Id: vport.c,v 1.1 1995/10/18 08:38:37 deraadt Exp $";
 #endif
 
 #include <errno.h>

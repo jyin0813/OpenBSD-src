b1bb7e47ca680cab275ee718768c94007d4883af
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/grdcmp.c
+++ gnu/libexec/uucp/libuuconf/Attic/grdcmp.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_grdcmp_rcsid[] = "$Id: grdcmp.c,v 1.3 1995/08/24 05:21:14 jtc Exp $";
+const char _uuconf_grdcmp_rcsid[] = "$Id: grdcmp.c,v 1.1 1995/10/18 08:38:34 deraadt Exp $";
 #endif
 
 #include <ctype.h>

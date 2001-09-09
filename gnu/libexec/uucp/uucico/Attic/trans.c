29baef7f4886c357b144caaf30eb39e122cd1bbc
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/uucico/Attic/trans.c
+++ gnu/libexec/uucp/uucico/Attic/trans.c
@@ -26,7 +26,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char trans_rcsid[] = "$Id: trans.c,v 1.3 1995/08/24 05:23:08 jtc Exp $";
+const char trans_rcsid[] = "$Id: trans.c,v 1.1 1995/10/18 08:38:41 deraadt Exp $";
 #endif
 
 #include <errno.h>

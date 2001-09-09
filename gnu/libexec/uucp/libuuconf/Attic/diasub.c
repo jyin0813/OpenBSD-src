ac10772bde2b921528884a6d60b21ecbf3c65b7b
deraadt <deraadt@openbsd.org>
2001-09-09 22:41:32 +0000

    uucp goes away

--- gnu/libexec/uucp/libuuconf/Attic/diasub.c
+++ gnu/libexec/uucp/libuuconf/Attic/diasub.c
@@ -26,7 +26,7 @@
 #include "uucnfi.h"
 
 #if USE_RCS_ID
-const char _uuconf_diasub_rcsid[] = "$Id: diasub.c,v 1.3 1995/08/24 05:21:03 jtc Exp $";
+const char _uuconf_diasub_rcsid[] = "$Id: diasub.c,v 1.1 1995/10/18 08:38:33 deraadt Exp $";
 #endif
 
 /* Clear the information in a dialer.  */

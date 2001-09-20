c5dcb1e9d37a5c19d0b50dd7c0d1ada1a612b195
mickey <mickey@openbsd.org>
2001-09-20 02:50:58 +0000

    merge vm/vm_kern.h into uvm/uvm_extern.h; art@ ok

--- sys/vm/Attic/vm_kern.h
+++ sys/vm/Attic/vm_kern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vm_kern.h,v 1.4 2001/06/27 04:52:39 art Exp $	*/
+/*	$OpenBSD: vm_kern.h,v 1.5 2001/08/12 23:12:06 miod Exp $	*/
 /*	$NetBSD: vm_kern.h,v 1.11 1998/02/10 14:08:58 mrg Exp $	*/
 
 /* 

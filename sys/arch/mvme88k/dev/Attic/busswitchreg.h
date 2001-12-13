cb8ca084daa782599a01a5fbfc5fd8a62feb664a
smurph <smurph@openbsd.org>
2001-12-13 15:55:51 +0000

    Support for MVME197 completed.  Fix SPL defs.

--- sys/arch/mvme88k/dev/Attic/busswitchreg.h
+++ sys/arch/mvme88k/dev/Attic/busswitchreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: busswitchreg.h,v 1.0 1999/09/27 05:52:30 smurph Exp $ */
+/*	$OpenBSD: busswitchreg.h,v 1.1 1999/09/27 18:43:22 smurph Exp $ */
 
 /*
  * Memory map for BusSwitch chip found in mvme197 boards.

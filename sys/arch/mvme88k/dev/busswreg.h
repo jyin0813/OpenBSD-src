00042d06a1a5c442b8af772245bb13c42c3af414
miod <miod@openbsd.org>
2001-12-17 06:49:46 +0000

    Revert the mvme88k to 20011212. Recent changes had not been merged correctly, and I am fed up with dissecting diffs to put back code that disappeared. This will likely be fixed shortly.

--- sys/arch/mvme88k/dev/busswreg.h
+++ sys/arch/mvme88k/dev/busswreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: busswitchreg.h,v 1.1 1999/09/27 18:43:22 smurph Exp $ */
+/*	$OpenBSD: busswreg.h,v 1.1 2001/12/13 08:59:38 smurph Exp $ */
 
 /*
  * Memory map for BusSwitch chip found in mvme197 boards.

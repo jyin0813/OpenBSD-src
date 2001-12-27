0e8a3f5822251fe293e346e68e0e6636dae989b3
miod <miod@openbsd.org>
2001-12-27 07:23:27 +0000

    Unbreak confusion between duplicated, but slightly different, m88k assembler sources and repair as in the process (we had ended mixing tc-mvme88k.c with tc-m88k.h).

--- gnu/usr.bin/gas/config/Attic/tc-mvme88k.h
+++ gnu/usr.bin/gas/config/Attic/tc-mvme88k.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tc-mvme88k.h,v 1.2 1998/02/15 18:49:41 niklas Exp $	*/
+/*	$OpenBSD: tc-mvme88k.h,v 1.3 2001/11/24 19:14:08 miod Exp $	*/
 
 /* m88k.h -- Assembler for the Motorola 88000
    Contributed by Devon Bowen of Buffalo University

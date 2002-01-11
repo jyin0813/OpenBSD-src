443a2c58010a1bec17962bebe343d40174a5379b
miod <miod@openbsd.org>
2002-01-11 05:25:40 +0000

    Bring back pmap_motorola for mac68k.

--- sys/arch/mac68k/mac68k/Attic/pmap.c
+++ sys/arch/mac68k/mac68k/Attic/pmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.c,v 1.35 2001/11/28 16:13:28 art Exp $	*/
+/*	$OpenBSD: pmap.c,v 1.37 2001/12/20 19:02:28 miod Exp $	*/
 /*	$NetBSD: pmap.c,v 1.55 1999/04/22 04:24:53 chs Exp $	*/
 
 /*

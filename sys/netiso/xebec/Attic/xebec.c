03b03fa1bee46320028ca316fc5ba86bdf2f1961
deraadt <deraadt@openbsd.org>
2002-02-19 04:46:03 +0000

    bye bye.  i delete this not because it is disgusting, but because it is disgusting code in the wrong place

--- sys/netiso/xebec/Attic/xebec.c
+++ sys/netiso/xebec/Attic/xebec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: xebec.c,v 1.2 1996/03/04 10:37:30 mickey Exp $	*/
+/*	$OpenBSD: xebec.c,v 1.3 2001/08/12 12:03:03 heko Exp $	*/
 /*	$NetBSD: xebec.c,v 1.4 1994/06/29 06:41:23 cgd Exp $	*/
 
 #include "xebec.h"

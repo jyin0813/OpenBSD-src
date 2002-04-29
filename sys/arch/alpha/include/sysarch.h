ef59fdf99a33ab161507d3a83376aefc9c1cda60
pvalchev <pvalchev@openbsd.org>
2002-04-29 02:55:14 +0000

    IEEE 754 floating point completion code, and implementation of the FP_C (Floating Point Control Quadword).
    From ross@NetBSD.  Added a way to disable it with option NO_IEEE,
    which appears on the ramdisks to save space.  This affects only
    programs compiled with -mieee, and what it essentially does is
    enabling infinities and NaNs, instead of generating SIGFPE on
    division by zero, overflow, etc.
    ok art, deraadt

--- sys/arch/alpha/include/sysarch.h
+++ sys/arch/alpha/include/sysarch.h
@@ -1,42 +1,69 @@
-/*	$NetBSD: sysarch.h,v 1.1 1996/04/12 01:42:45 cgd Exp $	*/
+/*	$OpenBSD$	*/
+/*	$NetBSD: sysarch.h,v 1.8 2001/04/26 03:10:46 ross Exp $	*/
 
-/*
- * Copyright (c) 1995, 1996 Carnegie-Mellon University.
+/*-
+ * Copyright (c) 2000 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
- * Author: Chris G. Demetriou
- * 
- * Permission to use, copy, modify and distribute this software and
- * its documentation is hereby granted, provided that both the copyright
- * notice and this permission notice appear in all copies of the
- * software, derivative works or modified versions, and any portions
- * thereof, and that both notices appear in supporting documentation.
- * 
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" 
- * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND 
- * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
- * Carnegie Mellon requests users of this software to return to
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe.
  *
- *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
- *  School of Computer Science
- *  Carnegie Mellon University
- *  Pittsburgh PA 15213-3890
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- * any improvements or extensions that they make and grant Carnegie the
- * rights to redistribute these changes.
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef _ALPHA_SYSARCH_H_
-#define	_ALPHA_SYSARCH_H_
+#define _ALPHA_SYSARCH_H_
 
-static inline u_int64_t
-alpha_rpcc()
-{
-	u_int64_t rv;
+#include <machine/bus.h>
+#include <machine/ieeefp.h>
 
-	__asm ("rpcc %0" : "=r" (rv));
-	return (rv);
-}
+/*
+ * Architecture specific syscalls (ALPHA)
+ */
 
-#endif /* _ALPHA_SYSARCH_H_ */
+#define	ALPHA_FPGETMASK			0
+#define	ALPHA_FPSETMASK			1
+#define	ALPHA_FPSETSTICKY		2
+#define	ALPHA_FPGETSTICKY		6
+#define	ALPHA_GET_FP_C			7
+#define	ALPHA_SET_FP_C			8
+
+struct alpha_fp_except_args {
+	fp_except mask;
+};
+
+struct alpha_fp_c_args {
+	uint64_t fp_c;
+};
+
+#ifdef _KERNEL
+int	sysarch(int, void *);
+#endif /* _KERNEL */
+
+#endif /* !_ALPHA_SYSARCH_H_ */

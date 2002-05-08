5b52b5a7ad64cad9b1da62ce49e2925ec2c9d0c0
art <art@openbsd.org>
2002-05-08 22:46:35 +0000

    Split out symbol handling into a separate file. Implement symbol handling for ELF (based on FreeBSD). Implement dummy support for alpha, hppa, powerpc and sparc64.
    Tested during the last 2 months on various archs (never comitted because
    other stuff got in the way).
    Some fixups to i386 support from mickey@
    
    This lets us switch all elf archs to this gprof.

--- usr.bin/gprof/hppa.c
+++ usr.bin/gprof/hppa.c
@@ -1,18 +1,7 @@
-/*	$OpenBSD: hppa.c,v 1.1 2001/03/22 05:18:30 mickey Exp $	*/
+#include        "gprof.h"
 
-#ifndef lint
-static char rcsid[] = "$OpenBSD: hppa.c,v 1.1 2001/03/22 05:18:30 mickey Exp $";
-#endif /* not lint */
-
-#include "gprof.h"
-
-/*
- * gprof -c isn't currently supported...
- */
 void
-findcall( parentp , p_lowpc , p_highpc )
-    nltype		*parentp;
-    unsigned long	p_lowpc;
-    unsigned long	p_highpc;
+findcall(nltype *parentp, unsigned long p_lowpc, unsigned long p_highpc)
 {
+	/* not supported. */
 }

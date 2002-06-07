34ae866b1fe5cd350086e1c0d044d681be8ffd2a
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/rcmd.c
+++ usr.sbin/afs/src/lib/roken/Attic/rcmd.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: rcmd.c,v 1.2 2000/08/16 01:23:44 lha Exp $");
+RCSID("$Id: rcmd.c,v 1.2 2000/09/11 14:41:03 art Exp $");
 #endif
 
 #include "roken.h"

36c418a62e3a287e590b414d7c0cf99492f85fb6
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/efile.c
+++ usr.sbin/afs/src/lib/roken/Attic/efile.c
@@ -42,7 +42,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: efile.c,v 1.1 1999/06/09 06:20:44 assar Exp $");
+RCSID("$Id: efile.c,v 1.1 2000/09/11 14:40:59 art Exp $");
 #endif
 
 #include <stdio.h>

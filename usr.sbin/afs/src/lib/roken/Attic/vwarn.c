d2a1554f245f680d2af23ffcd722911518996ebd
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/vwarn.c
+++ usr.sbin/afs/src/lib/roken/Attic/vwarn.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: vwarn.c,v 1.2 2000/08/16 01:23:51 lha Exp $");
+RCSID("$Id: vwarn.c,v 1.2 2000/09/11 14:41:06 art Exp $");
 #endif
 
 #include "err.h"

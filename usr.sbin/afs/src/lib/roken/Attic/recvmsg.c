e5d175ce5d7818ca27f018709d7cd094b1d549ed
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/recvmsg.c
+++ usr.sbin/afs/src/lib/roken/Attic/recvmsg.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: recvmsg.c,v 1.3 2000/08/16 01:23:44 lha Exp $");
+RCSID("$Id: recvmsg.c,v 1.3 2000/09/11 14:41:03 art Exp $");
 #endif
 
 #include "roken.h"

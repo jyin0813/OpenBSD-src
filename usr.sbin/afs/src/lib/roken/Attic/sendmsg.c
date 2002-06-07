1c89e4e1900155f0e827d9e31c39e9d0b659adb4
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/sendmsg.c
+++ usr.sbin/afs/src/lib/roken/Attic/sendmsg.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: sendmsg.c,v 1.2 2000/08/16 01:23:45 lha Exp $");
+RCSID("$Id: sendmsg.c,v 1.2 2000/09/11 14:41:03 art Exp $");
 #endif
 
 #include "roken.h"

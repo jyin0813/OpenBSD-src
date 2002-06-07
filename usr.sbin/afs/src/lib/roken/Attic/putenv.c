21848e636361d6032b0d3e39d002872459a5b53d
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/putenv.c
+++ usr.sbin/afs/src/lib/roken/Attic/putenv.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: putenv.c,v 1.2 2000/08/16 01:23:43 lha Exp $");
+RCSID("$Id: putenv.c,v 1.2 2000/09/11 14:41:03 art Exp $");
 #endif
 
 #include <stdlib.h>

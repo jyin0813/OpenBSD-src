ce0806c446ddc0207ee687a508ab1358338c63e3
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/strcollect.c
+++ usr.sbin/afs/src/lib/roken/Attic/strcollect.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: strcollect.c,v 1.1 2000/08/16 01:23:46 lha Exp $");
+RCSID("$Id: strcollect.c,v 1.1 2000/09/11 14:41:04 art Exp $");
 #endif
 
 #include <stdarg.h>

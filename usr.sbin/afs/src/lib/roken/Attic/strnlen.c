6efab532f0e6aff9b8663b8873ac6db48981f11d
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/strnlen.c
+++ usr.sbin/afs/src/lib/roken/Attic/strnlen.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: strnlen.c,v 1.2 2000/08/16 01:23:48 lha Exp $");
+RCSID("$Id: strnlen.c,v 1.2 2000/09/11 14:41:05 art Exp $");
 #endif
 
 #include "roken.h"

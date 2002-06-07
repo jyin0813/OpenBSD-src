da7b6a1ddadf5a1b287f22e1735ea208d9598f09
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/str2inaddr.c
+++ usr.sbin/afs/src/lib/roken/Attic/str2inaddr.c
@@ -38,7 +38,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: str2inaddr.c,v 1.1 1999/06/11 01:16:57 assar Exp $");
+RCSID("$Id: str2inaddr.c,v 1.1 2000/09/11 14:41:04 art Exp $");
 #endif
 
 #include <stdlib.h>

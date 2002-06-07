b1fa600a582423c89fb94c5310b26074c12ea1ae
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/parse_time.c
+++ usr.sbin/afs/src/lib/roken/Attic/parse_time.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: parse_time.c,v 1.2 2000/08/16 01:23:43 lha Exp $");
+RCSID("$Id: parse_time.c,v 1.2 2000/09/11 14:41:02 art Exp $");
 #endif
 
 #include <parse_units.h>

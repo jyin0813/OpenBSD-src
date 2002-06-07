62f16c29d9faadeab026678bc292153672ffb4b7
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/k_getpwuid.c
+++ usr.sbin/afs/src/lib/roken/Attic/k_getpwuid.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: k_getpwuid.c,v 1.3 2000/08/16 01:23:41 lha Exp $");
+RCSID("$Id: k_getpwuid.c,v 1.2 2000/09/11 14:41:01 art Exp $");
 #endif /* HAVE_CONFIG_H */
 
 #include "roken.h"

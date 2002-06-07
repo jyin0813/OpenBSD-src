836faad08dc5b12d2d1a7b8a19cc03660852c5a0
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/hstrerror.c
+++ usr.sbin/afs/src/lib/roken/Attic/hstrerror.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: hstrerror.c,v 1.3 2000/08/16 01:23:39 lha Exp $");
+RCSID("$Id: hstrerror.c,v 1.2 2000/09/11 14:41:01 art Exp $");
 #endif
 
 #ifndef HAVE_HSTRERROR

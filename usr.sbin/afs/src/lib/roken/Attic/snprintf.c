3b48b1c8e11ae864b033e9caac93c0632ee8decc
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/snprintf.c
+++ usr.sbin/afs/src/lib/roken/Attic/snprintf.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: snprintf.c,v 1.9 2000/08/16 01:23:46 lha Exp $");
+RCSID("$Id: snprintf.c,v 1.3 2000/09/11 14:41:04 art Exp $");
 #endif
 #include <stdio.h>
 #include <stdarg.h>

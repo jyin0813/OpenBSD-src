5acc8d1d191d2a29c1eac6a4996d3c7421b2f3c1
hin <hin@openbsd.org>
2002-06-07 10:30:23 +0000

    Bye bye

--- usr.sbin/afs/src/lib/roken/Attic/estrdup.c
+++ usr.sbin/afs/src/lib/roken/Attic/estrdup.c
@@ -33,7 +33,7 @@
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-RCSID("$Id: estrdup.c,v 1.2 2000/08/16 01:23:36 lha Exp $");
+RCSID("$Id: estrdup.c,v 1.2 2000/09/11 14:41:00 art Exp $");
 #endif
 
 #include <stdlib.h>

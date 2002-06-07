2df99a4109d01e85613083d554f85baccffdded4
hin <hin@openbsd.org>
2002-06-07 10:53:20 +0000

    New buildsystem reordered a bit, so these go away; bye bye

--- usr.sbin/afs/libroken/Attic/atypes.h
+++ usr.sbin/afs/libroken/Attic/atypes.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: atypes.h,v 1.1 1998/09/14 21:53:31 art Exp $	*/
 /* atypes.h -- this file was generated for i386-unknown-openbsd2.3 by
                $KTH: bits.c,v 1.4 1998/03/28 09:24:30 lha Exp $ */
 

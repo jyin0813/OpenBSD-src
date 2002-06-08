40b9e6daef9e12e9697204ff72e31a9d0131196b
miod <miod@openbsd.org>
2002-06-08 22:02:13 +0000

    One ddb to rule them all.
    Move the ddb files form macppc/macppc to powerpc/ddb, so that mvmeppc
    can benefit from the better ddb that was in macppc.
    db_interface.c is left as an md part.

--- sys/arch/mvmeppc/include/Attic/db_machdep.h
+++ sys/arch/mvmeppc/include/Attic/db_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_machdep.h,v 1.5 2001/11/06 00:30:38 art Exp $	*/
+/*	$OpenBSD: db_machdep.h,v 1.6 2002/03/14 01:26:41 millert Exp $	*/
 /*	$NetBSD: db_machdep.h,v 1.13 1996/04/29 20:50:08 leo Exp $	*/
 
 /* 

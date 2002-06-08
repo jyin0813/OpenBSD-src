cca7e4c20112eae337a5d6eb2a364fdbc6fef007
fgsch <fgsch@openbsd.org>
2002-06-08 10:52:55 +0000

    use weak aliases whenever is possible; millert@ ok.

--- lib/libc/gen/Attic/_err.c
+++ lib/libc/gen/Attic/_err.c
@@ -4,7 +4,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: _err.c,v 1.4 1996/08/19 08:21:19 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/cdefs.h>

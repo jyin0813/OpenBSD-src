ae78a75767cd0cdda33c6b4e0488cad9e6cc2c48
hugh <hugh@openbsd.org>
2002-06-11 15:36:23 +0000

    New boot code, mostly from ragge's work in NetBSD. Some header syncing and a couple network drivers came along for the ride. Assembly files have been renamed from .s to .S to facilitate diffs. Kernel is backwards compat - with manual interaction. OpenBSD features have been preserved.

--- sys/arch/vax/boot/common/Attic/romread.s
+++ sys/arch/vax/boot/common/Attic/romread.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: romread.s,v 1.3 1997/05/29 00:04:24 niklas Exp $ */
+/*	$OpenBSD: romread.s,v 1.1 2000/04/27 02:26:26 bjc Exp $ */
 /*	$NetBSD: romread.s,v 1.4 1996/08/02 11:22:24 ragge Exp $ */
 /*
  * Copyright (c) 1995 Ludd, University of Lule}, Sweden.

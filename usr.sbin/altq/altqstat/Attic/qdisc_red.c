1e5af67f05e1f5f95a9081bf48c2120f7ed64a9b
deraadt <deraadt@openbsd.org>
2002-11-24 01:47:28 +0000

    bye bye

--- usr.sbin/altq/altqstat/Attic/qdisc_red.c
+++ usr.sbin/altq/altqstat/Attic/qdisc_red.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: qdisc_red.c,v 1.1.1.1 2001/06/27 18:23:21 kjc Exp $	*/
+/*	$OpenBSD: qdisc_red.c,v 1.2 2001/08/16 12:59:43 kjc Exp $	*/
 /*	$KAME: qdisc_red.c,v 1.3 2001/08/15 12:51:59 kjc Exp $	*/
 /*
  * Copyright (C) 1999-2000

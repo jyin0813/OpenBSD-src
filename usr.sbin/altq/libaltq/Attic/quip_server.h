82285b025d5196120410a400228d799891acc3bf
deraadt <deraadt@openbsd.org>
2002-11-24 01:47:28 +0000

    bye bye

--- usr.sbin/altq/libaltq/Attic/quip_server.h
+++ usr.sbin/altq/libaltq/Attic/quip_server.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: quip_server.h,v 1.1.1.1 2001/06/27 18:23:36 kjc Exp $	*/
+/*	$OpenBSD: quip_server.h,v 1.2 2001/08/16 12:59:43 kjc Exp $	*/
 /*	$KAME: quip_server.h,v 1.3 2001/08/15 12:51:58 kjc Exp $	*/
 /*
  * Copyright (C) 1999-2000

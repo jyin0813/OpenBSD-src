f92ce8137941729689042bb1c6b42542d55264b2
deraadt <deraadt@openbsd.org>
2002-11-24 01:47:28 +0000

    bye bye

--- usr.sbin/altq/libaltq/Attic/qop_blue.c
+++ usr.sbin/altq/libaltq/Attic/qop_blue.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: qop_blue.c,v 1.2 2001/08/16 12:59:43 kjc Exp $	*/
+/*	$OpenBSD: qop_blue.c,v 1.3 2001/12/03 08:38:48 kjc Exp $	*/
 /*	$KAME: qop_blue.c,v 1.5 2001/08/16 10:39:13 kjc Exp $	*/
 /*
  * Copyright (C) 1999-2000

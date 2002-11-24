a0da17e637a262ea28609e4f70d65877f9f77643
deraadt <deraadt@openbsd.org>
2002-11-24 01:47:28 +0000

    bye bye

--- usr.sbin/altq/libaltq/Attic/qop_wfq.c
+++ usr.sbin/altq/libaltq/Attic/qop_wfq.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: qop_wfq.c,v 1.3 2001/12/03 08:38:48 kjc Exp $	*/
+/*	$OpenBSD: qop_wfq.c,v 1.4 2002/03/01 11:01:42 kjc Exp $	*/
 /*	$KAME: qop_wfq.c,v 1.5 2001/08/16 10:39:15 kjc Exp $	*/
 /*
  * Copyright (C) 1999-2000

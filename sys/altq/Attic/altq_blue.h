0fe69d95dd5987a8fb69ef8aabe72739a0d2e7d3
kjc <kjc@openbsd.org>
2002-12-16 16:18:05 +0000

    switchover to pf-based altq.  - remove files which are no longer used, or we don't have plans to support    in pf in the near future.  - remove altq ioctl related stuff.  - convert the PRIQ, HFSC and RIO modules to pf-based altq.    (these are not enabled in GENERIC, CDNR is not converted yet.)

--- sys/altq/Attic/altq_blue.h
+++ sys/altq/Attic/altq_blue.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: altq_blue.h,v 1.3 2002/03/14 03:15:50 millert Exp $	*/
+/*	$OpenBSD: altq_blue.h,v 1.4 2002/11/26 01:03:34 henning Exp $	*/
 /*	$KAME: altq_blue.h,v 1.5 2000/12/14 08:12:45 thorpej Exp $	*/
 
 /*

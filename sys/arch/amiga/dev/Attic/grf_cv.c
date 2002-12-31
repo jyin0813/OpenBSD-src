0061f50e9b664836f3bcef81d1d8af5c05dfbaa9
miod <miod@openbsd.org>
2002-12-31 23:35:37 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/amiga/dev/Attic/grf_cv.c
+++ sys/arch/amiga/dev/Attic/grf_cv.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: grf_cv.c,v 1.17 2002/03/14 01:26:28 millert Exp $	*/
+/*	$OpenBSD: grf_cv.c,v 1.18 2002/08/02 16:13:07 millert Exp $	*/
 /*	$NetBSD: grf_cv.c,v 1.24 1997/07/30 11:05:55 veego Exp $	*/
 
 /*

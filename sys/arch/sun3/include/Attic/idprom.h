e74468c96fbbf9da629b8d4a7547024dbf1fd64d
miod <miod@openbsd.org>
2002-12-31 23:35:40 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/sun3/include/Attic/idprom.h
+++ sys/arch/sun3/include/Attic/idprom.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: idprom.h,v 1.5 1997/01/16 04:04:07 kstailey Exp $	*/
+/*	$OpenBSD: idprom.h,v 1.6 2002/03/14 01:26:46 millert Exp $	*/
 /*	$NetBSD: idprom.h,v 1.12 1996/11/20 18:57:10 gwr Exp $	*/
 
 /*-

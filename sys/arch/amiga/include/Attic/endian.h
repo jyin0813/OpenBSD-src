5343f0c0e1511696cb4e8b751bf7dfc68697e074
miod <miod@openbsd.org>
2002-12-31 23:35:38 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/amiga/include/Attic/endian.h
+++ sys/arch/amiga/include/Attic/endian.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: endian.h,v 1.2 1996/11/25 13:11:14 niklas Exp $	*/
+/*	$OpenBSD: endian.h,v 1.3 1997/11/09 23:05:01 niklas Exp $	*/
 /*	$NetBSD: endian.h,v 1.7 1994/10/26 02:06:09 cgd Exp $	*/
 
 #ifndef _AMIGA_ENDIAN_H_

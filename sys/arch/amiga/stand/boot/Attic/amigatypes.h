5735291ea9ab53092bca4afec4eb44fc550ce776
miod <miod@openbsd.org>
2002-12-31 23:35:39 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/amiga/stand/boot/Attic/amigatypes.h
+++ sys/arch/amiga/stand/boot/Attic/amigatypes.h
@@ -1,5 +1,5 @@
 /*
- * $OpenBSD: amigatypes.h,v 1.1.1.1 1996/11/29 23:36:29 is Exp $
+ * $OpenBSD: amigatypes.h,v 1.1 1997/01/16 09:26:26 niklas Exp $
  * $NetBSD: amigatypes.h,v 1.1.1.1 1996/11/29 23:36:29 is Exp $
  *
  * Copyright (c) 1996 Ignatios Souvatzis

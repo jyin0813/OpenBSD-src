b3dc62970f732f9c112ca2141efbf49d15570b73
miod <miod@openbsd.org>
2002-12-31 23:35:40 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/sun3/include/Attic/cpufunc.h
+++ sys/arch/sun3/include/Attic/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpufunc.h,v 1.3 1997/09/21 04:21:07 niklas Exp $	*/
+/*	$OpenBSD: cpufunc.h,v 1.4 2002/03/14 01:26:46 millert Exp $	*/
 /*	$NetBSD: cpufunc.h,v 1.6 1996/11/20 18:57:07 gwr Exp $	*/
 
 /*-

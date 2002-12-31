f8b04d67ba19727da9976d7d66cb0d9378d307fa
miod <miod@openbsd.org>
2002-12-31 23:35:38 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/amiga/dev/Attic/sbicvar.h
+++ sys/arch/amiga/dev/Attic/sbicvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sbicvar.h,v 1.5 2002/03/14 01:26:29 millert Exp $	*/
+/*	$OpenBSD: sbicvar.h,v 1.6 2002/03/14 03:15:51 millert Exp $	*/
 /*	$NetBSD: sbicvar.h,v 1.11 1996/04/21 21:12:23 veego Exp $	*/
 
 /*

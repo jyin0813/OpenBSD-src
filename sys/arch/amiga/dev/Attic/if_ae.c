dda175e746d9ec748d70d963d4d15605de21bc2f
miod <miod@openbsd.org>
2002-12-31 23:35:37 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/amiga/dev/Attic/if_ae.c
+++ sys/arch/amiga/dev/Attic/if_ae.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ae.c,v 1.13 2001/02/20 19:39:30 mickey Exp $	*/
+/*	$OpenBSD: if_ae.c,v 1.14 2002/03/14 01:26:29 millert Exp $	*/
 /*	$NetBSD: if_ae.c,v 1.14 1997/03/18 18:44:53 veego Exp $	*/
 
 /*

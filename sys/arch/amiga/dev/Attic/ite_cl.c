f77d1c2b5a12ce86109eb78f7e595b8d70fedfb0
miod <miod@openbsd.org>
2002-12-31 23:35:37 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/amiga/dev/Attic/ite_cl.c
+++ sys/arch/amiga/dev/Attic/ite_cl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ite_cl.c,v 1.5 2002/03/14 01:26:29 millert Exp $	*/
+/*	$OpenBSD: ite_cl.c,v 1.6 2002/06/11 03:25:42 miod Exp $	*/
 /*	$NetBSD: ite_cl.c,v 1.3.2.1 1999/06/28 23:22:17 perry Exp $	*/
 
 /*

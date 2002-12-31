9a134a3b3f7fc6fb78e9ab030a8077ae28a0a334
miod <miod@openbsd.org>
2002-12-31 23:35:36 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/amiga/amiga/Attic/color.h
+++ sys/arch/amiga/amiga/Attic/color.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: color.h,v 1.2 1996/06/04 12:49:13 niklas Exp $	*/
+/*	$OpenBSD: color.h,v 1.3 2002/03/14 01:26:28 millert Exp $	*/
 /*	$NetBSD: color.h,v 1.8 1995/02/12 19:34:17 chopps Exp $	*/
 
 /*

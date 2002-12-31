42398687bdea90a678304ad4ff4c8284602dce8d
miod <miod@openbsd.org>
2002-12-31 23:35:40 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/sun3/include/Attic/autoconf.h
+++ sys/arch/sun3/include/Attic/autoconf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.h,v 1.7 1997/01/16 04:04:04 kstailey Exp $	*/
+/*	$OpenBSD: autoconf.h,v 1.8 2002/03/14 01:26:46 millert Exp $	*/
 /*	$NetBSD: autoconf.h,v 1.12 1996/11/20 18:57:05 gwr Exp $	*/
 
 /*-

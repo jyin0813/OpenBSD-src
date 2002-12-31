83b72cebf28eb3c2b96498355cbb98e940f159c0
miod <miod@openbsd.org>
2002-12-31 23:35:38 +0000

    amiga and sun3 turned out to not be y2k+3 compliant here. Remove them, as well as the few userland tools which were only used on these platforms.

--- sys/arch/amiga/dev/Attic/viewioctl.h
+++ sys/arch/amiga/dev/Attic/viewioctl.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: viewioctl.h,v 1.2 1996/05/02 06:44:37 niklas Exp $	*/
+/*	$OpenBSD: viewioctl.h,v 1.3 2002/03/14 01:26:29 millert Exp $	*/
 /*	$NetBSD: viewioctl.h,v 1.6 1996/04/21 21:12:40 veego Exp $	*/
 
 /*

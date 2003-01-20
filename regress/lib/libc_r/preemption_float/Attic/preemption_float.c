5a419c6eeb38e00ce2c0ca3119fac57d28e8ec74
deraadt <deraadt@openbsd.org>
2003-01-20 06:08:32 +0000

    repository move to libpthread

--- regress/lib/libc_r/preemption_float/Attic/preemption_float.c
+++ regress/lib/libc_r/preemption_float/Attic/preemption_float.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: preemption_float.c,v 1.1.1.1 2001/08/15 14:37:12 fgsch Exp $	*/
+/*	$OpenBSD: preemption_float.c,v 1.2 2002/06/23 20:21:22 marc Exp $	*/
 /*
  * Copyright (c) 1993, 1994, 1995, 1996 by Chris Provenzano and contributors, 
  * proven@mit.edu All rights reserved.

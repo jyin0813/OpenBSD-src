7a572cb7dc1a69a538c37fe679e647abf69dc12f
deraadt <deraadt@openbsd.org>
2003-01-20 06:08:31 +0000

    repository move to libpthread

--- regress/lib/libc_r/execve/Attic/execve.c
+++ regress/lib/libc_r/execve/Attic/execve.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: execve.c,v 1.1.1.1 2001/08/15 14:37:11 fgsch Exp $	*/
+/*	$OpenBSD: execve.c,v 1.2 2002/12/13 20:21:04 marc Exp $	*/
 /*
  * Copyright (c) 1993, 1994, 1995, 1996 by Chris Provenzano and contributors, 
  * proven@mit.edu All rights reserved.

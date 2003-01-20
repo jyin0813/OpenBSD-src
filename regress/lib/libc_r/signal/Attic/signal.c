ae9a2c31e8213ae5b0817607e2ef077bed89adc2
deraadt <deraadt@openbsd.org>
2003-01-20 06:08:33 +0000

    repository move to libpthread

--- regress/lib/libc_r/signal/Attic/signal.c
+++ regress/lib/libc_r/signal/Attic/signal.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: signal.c,v 1.3 2001/11/11 23:26:35 deraadt Exp $	*/
+/*	$OpenBSD: signal.c,v 1.4 2002/06/16 23:06:15 marc Exp $	*/
 /* David Leonard <d@openbsd.org>, 2001. Public Domain. */
 
 /*

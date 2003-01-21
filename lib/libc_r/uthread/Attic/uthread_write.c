2c992e061c79e3df160ccae2f89fabe2eed5aab9
marc <marc@openbsd.org>
2003-01-21 01:14:07 +0000

    bye-bye libc_r sources. the sources have been moved (with history) to /usr/src/lib/libpthread

--- lib/libc_r/uthread/Attic/uthread_write.c
+++ lib/libc_r/uthread/Attic/uthread_write.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uthread_write.c,v 1.7 2001/08/11 15:01:57 fgsch Exp $	*/
+/*	$OpenBSD: uthread_write.c,v 1.8 2001/08/21 19:24:53 fgsch Exp $	*/
 /*
  * Copyright (c) 1995-1998 John Birrell <jb@cimlogic.com.au>
  * All rights reserved.

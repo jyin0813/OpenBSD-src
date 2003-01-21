492fe75054aa89eee342c4f4532e4191b1fdf39a
marc <marc@openbsd.org>
2003-01-21 01:14:07 +0000

    bye-bye libc_r sources. the sources have been moved (with history) to /usr/src/lib/libpthread

--- lib/libc_r/uthread/Attic/uthread_sendto.c
+++ lib/libc_r/uthread/Attic/uthread_sendto.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uthread_sendto.c,v 1.4 1999/11/25 07:01:43 d Exp $	*/
+/*	$OpenBSD: uthread_sendto.c,v 1.5 2001/08/21 19:24:53 fgsch Exp $	*/
 /*
  * Copyright (c) 1995-1998 John Birrell <jb@cimlogic.com.au>
  * All rights reserved.

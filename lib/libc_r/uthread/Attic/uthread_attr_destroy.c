b86fc7f1e4190b60f2e9a19b77e6e74931e4d091
marc <marc@openbsd.org>
2003-01-21 01:14:06 +0000

    bye-bye libc_r sources. the sources have been moved (with history) to /usr/src/lib/libpthread

--- lib/libc_r/uthread/Attic/uthread_attr_destroy.c
+++ lib/libc_r/uthread/Attic/uthread_attr_destroy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uthread_attr_destroy.c,v 1.3 1999/11/25 07:01:30 d Exp $	*/
+/*	$OpenBSD: uthread_attr_destroy.c,v 1.4 2002/11/07 02:56:20 marc Exp $	*/
 /*
  * Copyright (c) 1996 John Birrell <jb@cimlogic.com.au>.
  * All rights reserved.

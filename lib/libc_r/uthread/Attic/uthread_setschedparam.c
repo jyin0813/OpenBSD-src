74d99b11a4ecbc2bffedfabb8b4348fc2712d332
marc <marc@openbsd.org>
2003-01-21 01:14:07 +0000

    bye-bye libc_r sources. the sources have been moved (with history) to /usr/src/lib/libpthread

--- lib/libc_r/uthread/Attic/uthread_setschedparam.c
+++ lib/libc_r/uthread/Attic/uthread_setschedparam.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uthread_setschedparam.c,v 1.4 2002/01/19 23:42:40 fgsch Exp $	*/
+/*	$OpenBSD: uthread_setschedparam.c,v 1.5 2002/01/19 23:49:32 fgsch Exp $	*/
 /*
  * Copyright (c) 1998 Daniel Eischen <eischen@vigrid.com>.
  * All rights reserved.

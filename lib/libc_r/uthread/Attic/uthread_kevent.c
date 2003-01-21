bbb6e67a16bb8d4cb529b66f65c944fd663335b3
marc <marc@openbsd.org>
2003-01-21 01:14:07 +0000

    bye-bye libc_r sources. the sources have been moved (with history) to /usr/src/lib/libpthread

--- lib/libc_r/uthread/Attic/uthread_kevent.c
+++ lib/libc_r/uthread/Attic/uthread_kevent.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uthread_kevent.c,v 1.2 2001/08/21 19:24:53 fgsch Exp $	*/
+/*	$OpenBSD: uthread_kevent.c,v 1.3 2001/12/08 17:08:07 fgsch Exp $	*/
 
 /*-
  * Copyright (c) 2000 Jonathan Lemon <jlemon@flugsvamp.com>

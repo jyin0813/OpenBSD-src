a1d1f0fe4b80fccb8a39d7d451fd62b2e168b5ee
jakob <jakob@openbsd.org>
2003-02-21 15:59:45 +0000

    remove bind4; ok deraadt@

--- usr.sbin/named/named/Attic/db_load.c
+++ usr.sbin/named/named/Attic/db_load.c
@@ -1,11 +1,11 @@
-/*	$OpenBSD: db_load.c,v 1.7 2002/02/17 19:42:37 millert Exp $	*/
+/*	$OpenBSD: db_load.c,v 1.8 2002/06/09 01:58:54 kjell Exp $	*/
 
 #if !defined(lint) && !defined(SABER)
 #if 0
 static char sccsid[] = "@(#)db_load.c	4.38 (Berkeley) 3/2/91";
 static char rcsid[] = "$From: db_load.c,v 8.33 1998/05/11 04:19:45 vixie Exp $";
 #else
-static char rcsid[] = "$OpenBSD: db_load.c,v 1.7 2002/02/17 19:42:37 millert Exp $";
+static char rcsid[] = "$OpenBSD: db_load.c,v 1.8 2002/06/09 01:58:54 kjell Exp $";
 #endif
 #endif /* not lint */
 

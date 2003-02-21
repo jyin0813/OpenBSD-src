6cc48eda17751350b0fcc17b362ef0449fc2df4b
millert <millert@openbsd.org>
2003-02-21 03:38:08 +0000

    Sync with ISC cron-current + my at(1) integration. The at(1) code is now more tightly integrated into the cron codebase.

--- usr.bin/at/Attic/panic.h
+++ usr.bin/at/Attic/panic.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: panic.h,v 1.5 2002/05/11 23:02:33 millert Exp $	*/
+/*	$OpenBSD: panic.h,v 1.6 2002/05/11 23:16:44 millert Exp $	*/
 /*	$NetBSD: panic.h,v 1.2 1995/03/25 18:13:35 glass Exp $	*/
 
 /*

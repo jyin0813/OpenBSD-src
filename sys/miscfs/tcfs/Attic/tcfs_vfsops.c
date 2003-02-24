4f1fd9a43ca37dd1781c56bd53cd467df3a3b8f5
deraadt <deraadt@openbsd.org>
2003-02-24 08:17:39 +0000

    Remove tcfs until this entire licensing mess is cleaned up.  This is ANOTHER case of a developer having ignored licenses when importing code.

--- sys/miscfs/tcfs/Attic/tcfs_vfsops.c
+++ sys/miscfs/tcfs/Attic/tcfs_vfsops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcfs_vfsops.c,v 1.4 2001/02/20 01:50:10 assar Exp $	*/
+/*	$OpenBSD: tcfs_vfsops.c,v 1.5 2002/03/14 01:27:08 millert Exp $	*/
 /*
  * Copyright 2000 The TCFS Project at http://tcfs.dia.unisa.it/
  * All rights reserved.

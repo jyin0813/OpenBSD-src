59fbfc13f928c123da68fcc277deca53445d393f
deraadt <deraadt@openbsd.org>
2003-02-24 09:13:14 +0000

    bye bye

--- usr.bin/tcfs/Attic/tcfs_flags.c
+++ usr.bin/tcfs/Attic/tcfs_flags.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcfs_flags.c,v 1.6 2000/06/20 07:58:57 fgsch Exp $	*/
+/*	$OpenBSD: tcfs_flags.c,v 1.7 2000/06/20 08:28:02 fgsch Exp $	*/
 
 /*
  *	Transparent Cryptographic File System (TCFS) for NetBSD 

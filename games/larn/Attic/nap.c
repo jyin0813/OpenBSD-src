bece2c7e6f7136810e89d2e51507b63c4145b95c
deraadt <deraadt@openbsd.org>
2003-03-11 16:29:28 +0000

    OK, this is a strange case.  At least some parts of this software is copyrighted, and then marked "Copying for Profit is Prohibited", but the author has passed away.  A number of people have tried to make some sort of effort at trying to contact his estate, and have failed. Therefore, lacking the ability to correct this copyright, we must delete it.  I would bet strongly that this is not what the author would have intended, but until we can get this resolved, this is the action we must take.

--- games/larn/Attic/nap.c
+++ games/larn/Attic/nap.c
@@ -1,9 +1,9 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: nap.c,v 1.2 1998/09/15 05:12:32 pjanzen Exp $	*/
 /*	$NetBSD: nap.c,v 1.5 1997/10/18 20:03:36 christos Exp $	*/
 
 /* nap.c		 Larn is copyrighted 1986 by Noah Morgan. */
 #ifndef lint
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: nap.c,v 1.2 1998/09/15 05:12:32 pjanzen Exp $";
 #endif				/* not lint */
 
 #include <unistd.h>

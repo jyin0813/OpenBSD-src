0576f4403aec35256454a39e58afeeb2ba0b2373
deraadt <deraadt@openbsd.org>
2003-03-11 16:29:28 +0000

    OK, this is a strange case.  At least some parts of this software is copyrighted, and then marked "Copying for Profit is Prohibited", but the author has passed away.  A number of people have tried to make some sort of effort at trying to contact his estate, and have failed. Therefore, lacking the ability to correct this copyright, we must delete it.  I would bet strongly that this is not what the author would have intended, but until we can get this resolved, this is the action we must take.

--- games/larn/Attic/pathnames.h
+++ games/larn/Attic/pathnames.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pathnames.h,v 1.2 1998/09/15 05:12:33 pjanzen Exp $	*/
+/*	$OpenBSD: pathnames.h,v 1.3 1998/10/01 05:31:38 pjanzen Exp $	*/
 /* $NetBSD: pathnames.h,v 1.7 1997/10/18 20:03:41 christos Exp $	 */
 
 /*-

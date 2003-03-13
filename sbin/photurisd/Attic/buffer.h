35c2e7a330100ca095376fe7dadc8736d1338dfa
deraadt <deraadt@openbsd.org>
2003-03-13 07:33:46 +0000

    thought we had already deleted this

--- sbin/photurisd/Attic/buffer.h
+++ sbin/photurisd/Attic/buffer.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: buffer.h,v 1.3 2001/01/28 22:45:06 niklas Exp $ */
+/* $OpenBSD: buffer.h,v 1.4 2002/06/09 08:13:08 todd Exp $ */
 /*
  * Copyright 1997-2000 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.

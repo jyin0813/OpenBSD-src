3c69407597e7cb4db0a6a518a7b0d6bef2195907
deraadt <deraadt@openbsd.org>
2003-03-13 07:33:47 +0000

    thought we had already deleted this

--- sbin/photurisd/Attic/schedule.h
+++ sbin/photurisd/Attic/schedule.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: schedule.h,v 1.3 2001/01/28 22:45:16 niklas Exp $ */
+/* $OpenBSD: schedule.h,v 1.4 2002/06/09 08:13:08 todd Exp $ */
 /*
  * Copyright 1997-2000 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.

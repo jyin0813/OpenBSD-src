89f8443d5a31e43c5d2f46aee3ed321a6ab5aae6
deraadt <deraadt@openbsd.org>
2003-03-13 07:33:47 +0000

    thought we had already deleted this

--- sbin/photurisd/Attic/handle_spi_update.c
+++ sbin/photurisd/Attic/handle_spi_update.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: handle_spi_update.c,v 1.7 2001/01/28 22:45:10 niklas Exp $	*/
+/*	$OpenBSD: handle_spi_update.c,v 1.8 2002/06/09 08:13:08 todd Exp $	*/
 
 /*
  * Copyright 1997-2000 Niels Provos <provos@citi.umich.edu>
@@ -36,7 +36,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: handle_spi_update.c,v 1.7 2001/01/28 22:45:10 niklas Exp $";
+static char rcsid[] = "$OpenBSD: handle_spi_update.c,v 1.8 2002/06/09 08:13:08 todd Exp $";
 #endif
 
 #include <stdio.h>

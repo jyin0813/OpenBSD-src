bc19709472b33fa6c19f5a541c495426f0b91432
nate <nate@openbsd.org>
2003-05-17 12:07:57 +0000

    sync with NetBSD and add various local hacks to make things work correctly with our scsi layer

--- sys/dev/usb/umassvar.h
+++ sys/dev/usb/umassvar.h
@@ -1,4 +1,5 @@
-/*	$NetBSD: umassvar.h,v 1.15 2002/02/07 13:52:55 augustss Exp $	*/
+/*	$OpenBSD$ */
+/*	$NetBSD: umassvar.h,v 1.19 2003/02/22 05:18:50 tsutsui Exp $	*/
 /*-
  * Copyright (c) 1999 MAEKAWA Masahide <bishop@rr.iij4u.or.jp>,
  *		      Nick Hibma <n_hibma@freebsd.org>
@@ -58,8 +59,6 @@ extern int umassdebug;
 #define DIR_NONE	0
 #define DIR_IN		1
 #define DIR_OUT		2
-
-#define MS_TO_TICKS(ms) ((ms) * hz / 1000)			      
 
 /* Endpoints for umass */
 #define	UMASS_BULKIN	0
@@ -178,11 +177,12 @@ struct umass_softc {
 #define UMASS_CPROTO_ISD_ATA	5
 
 	u_int32_t		sc_quirks;
-#define	UMASS_QUIRK_RS_NO_CLEAR_UA	0x00000002
-#define	UMASS_QUIRK_NO_START_STOP	0x00000004
-#define	UMASS_QUIRK_FORCE_SHORT_INQUIRY	0x00000008
-#define	UMASS_QUIRK_WRONG_CSWSIG	0x00000010
+#define UMASS_QUIRK_RS_NO_CLEAR_UA	0x00000002
+#define UMASS_QUIRK_NO_START_STOP	0x00000004
+#define UMASS_QUIRK_FORCE_SHORT_INQUIRY	0x00000008
+#define UMASS_QUIRK_WRONG_CSWSIG	0x00000010
 #define UMASS_QUIRK_NO_MAX_LUN		0x00000020
+#define UMASS_QUIRK_WRONG_CSWTAG	0x00000040
 
 	u_int32_t		sc_busquirks;
 
@@ -190,7 +190,7 @@ struct umass_softc {
 	umass_bbb_cbw_t		cbw;	/* command block wrapper */
 	umass_bbb_csw_t		csw;	/* command status wrapper*/
 	/* CBI specific variables for transfers in progress */
-	umass_cbi_cbl_t		cbl;	/* command block */ 
+	umass_cbi_cbl_t		cbl;	/* command block */
 	umass_cbi_sbl_t		sbl;	/* status block */
 
 	/* xfer handles
@@ -208,7 +208,7 @@ struct umass_softc {
 #define XFER_BBB_RESET1		6
 #define XFER_BBB_RESET2		7
 #define XFER_BBB_RESET3		8
-	
+
 #define XFER_CBI_CB		0	/* CBI */
 #define XFER_CBI_DATA		1
 #define XFER_CBI_STATUS		2
@@ -227,7 +227,7 @@ struct umass_softc {
 	int			transfer_dir;		/* data direction */
 	void			*transfer_data;		/* data buffer */
 	int			transfer_datalen;	/* (maximum) length */
-	int			transfer_actlen;	/* actual length */ 
+	int			transfer_actlen;	/* actual length */
 	umass_callback		transfer_cb;		/* callback */
 	void			*transfer_priv;		/* for callback */
 	int			transfer_status;
@@ -264,6 +264,7 @@ struct umass_softc {
 
 	int			sc_xfer_flags;
 	char			sc_dying;
+	int			sc_refcnt;
 
 	struct umassbus_softc	*bus;		 /* bus dependent data */
 };

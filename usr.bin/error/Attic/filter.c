1feea688251f3c3ccb8da0669c4ec9b718e3fcc5
deraadt <deraadt@openbsd.org>
2003-07-14 08:21:37 +0000

    toast this piece of junk

--- usr.bin/error/Attic/filter.c
+++ usr.bin/error/Attic/filter.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: filter.c,v 1.6 2001/07/12 05:17:02 deraadt Exp $	*/
+/*	$OpenBSD: filter.c,v 1.7 2003/06/03 02:56:07 millert Exp $	*/
 /*	$NetBSD: filter.c,v 1.3 1995/09/02 06:15:28 jtc Exp $	*/
 
 /*
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)filter.c	8.1 (Berkeley) 6/6/93";
 #endif
-static char rcsid[] = "$OpenBSD: filter.c,v 1.6 2001/07/12 05:17:02 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: filter.c,v 1.7 2003/06/03 02:56:07 millert Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>

79c5ae70ecfeec5b7955b5de7abecfa6eb8f0b72
deraadt <deraadt@openbsd.org>
2003-07-24 01:44:28 +0000

    bye bye

--- usr.sbin/ssio/Attic/hp_scanjet_IIc_grabscan.sh
+++ usr.sbin/ssio/Attic/hp_scanjet_IIc_grabscan.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-# $Id: hp_scanjet_IIc_grabscan,v 1.1 1996/01/14 01:50:11 ken Exp $
+# $Id: hp_scanjet_IIc_grabscan.sh,v 1.1 1997/03/11 03:23:14 kstailey Exp $
 #
 # Copyright (c) 1996 Kenneth Stailey
 # All rights reserved.

a7bc89c3c0b6a08fc4a8e6291d0c6b6f7e5c4d69
markus <markus@openbsd.org>
2003-08-22 19:22:27 +0000

    nuke "kerberos-2@ssh.com"

--- usr.bin/ssh/Attic/auth2-krb5.c
+++ usr.bin/ssh/Attic/auth2-krb5.c
@@ -23,7 +23,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: auth2-krb5.c,v 1.1 2003/05/14 02:15:47 markus Exp $");
+RCSID("$OpenBSD: auth2-krb5.c,v 1.2 2003/05/15 14:09:21 markus Exp $");
 
 #include <krb5.h>
 

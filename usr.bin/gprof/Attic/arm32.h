f24dd564e5dad10e02bd69edff45ccc4c0f5a0a9
drahn <drahn@openbsd.org>
2004-01-29 11:22:47 +0000

    s/arm32/arm/ ok deraadt@

--- usr.bin/gprof/Attic/arm32.h
+++ usr.bin/gprof/Attic/arm32.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: arm32.h,v 1.2 1996/06/26 05:33:49 deraadt Exp $	*/
+/*	$OpenBSD: arm32.h,v 1.3 2003/06/03 02:56:08 millert Exp $	*/
 /*	$NetBSD: arm32.h,v 1.1 1996/04/01 21:51:26 mark Exp $	*/
 
 /*-

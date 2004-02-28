6f74aec8cef93a2954b0907914d11dc1124ca60f
deraadt <deraadt@openbsd.org>
2004-02-28 00:36:14 +0000

    change amd64's MACHINE_ARCH from x86_64 to amd64.  There are many many reasons for this, quite a few of them technical, and not all of them in response to Intel's broken ia32e crud.  The gcc toolchain stays at x86_64 for now.

--- lib/libc/arch/x86_64/gen/Attic/infinity.c
+++ lib/libc/arch/x86_64/gen/Attic/infinity.c
@@ -1,5 +1,5 @@
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: infinity.c,v 1.1 2004/01/28 01:44:45 mickey Exp $";
+static char rcsid[] = "$OpenBSD: infinity.c,v 1.2 2004/02/08 17:31:44 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 /* infinity.c */

48c5d3698b9bb6c63079b6bf232a572f3ad9e3ea
miod <miod@openbsd.org>
2004-04-25 01:51:48 +0000

    Preliminary switch to an mvme88k bus_space world. Not all drivers have been converted yet, and they rely upon some linear mappings provided by bus_space.
    In order to not impact performance, almost all the bus_space accesses go
    through macros and inline functions. This currently restricts us to D16 and
    D32 access modes, which are selected at compilation time. Since there are no
    plans to support D8 vme devices in the future, this is acceptable for now.
    
    This makes the "len" locator for vme devices go away, and allows to simplify
    some code which was using the pcctwo device, and will now directly access
    hardware which is known to exist, rather than wait until the device is
    attached.
    
    While there, try to enforce more interrupt vector number checks, since these
    changes pointed out that dart(4) had been working correctly so far by sheer
    luck only.

--- sys/arch/mvme88k/dev/Attic/sysconfunc.h
+++ sys/arch/mvme88k/dev/Attic/sysconfunc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sysconfunc.h,v 1.1 2001/03/08 00:03:14 miod Exp $	*/
+/*	$OpenBSD: sysconfunc.h,v 1.2 2002/03/14 01:26:39 millert Exp $	*/
 
 #ifndef _MVME88K_SYSCON_H_
 #define _MVME88K_SYSCON_H_

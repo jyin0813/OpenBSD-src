9d69d7bb6f2d4ab5d6d502a787df54f0da858bd1
millert <millert@openbsd.org>
2004-04-27 01:38:12 +0000

    Use a common source file for all the hash helper functions that previously lived in foohl.c.  The foohl.c files are now generated via sed, though perhaps cpp could be used in the future.
    Use u_int8_t instead of unsigned char for the buffers struct fooContext.
    Add constants for buffer lengths and use them in function prototypes
    and the man pages.
    
    This is basically cosmetic surgery; there should be no functional changes.
    
    OK deraadt@

---
+++ lib/libc/hash/helper.c
@@ -0,0 +1,77 @@
+/*	$OpenBSD$	*/
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static const char rcsid[] = "$OpenBSD$";
+#endif /* LIBC_SCCS and not lint */
+
+#include <sys/types.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <hashinc>
+
+/* ARGSUSED */
+char *
+HASHEnd(HASH_CTX *ctx, char buf[HASH_DIGEST_STRING_LENGTH])
+{
+    int i;
+    u_char digest[HASH_DIGEST_LENGTH];
+    static const char hex[] = "0123456789abcdef";
+
+    if (buf == NULL && (buf = malloc(HASH_DIGEST_STRING_LENGTH)) == NULL)
+	return(NULL);
+
+    HASHFinal(digest,ctx);
+    for (i = 0; i < HASH_DIGEST_LENGTH; i++) {
+	buf[i + i] = hex[digest[i] >> 4];
+	buf[i + i + 1] = hex[digest[i] & 0x0f];
+    }
+    buf[i + i] = '\0';
+    memset(digest, 0, sizeof(digest));
+    return(buf);
+}
+
+char *
+HASHFile(char *filename, char buf[HASH_DIGEST_STRING_LENGTH])
+{
+    u_char buffer[BUFSIZ];
+    HASH_CTX ctx;
+    int fd, num, oerrno;
+
+    HASHInit(&ctx);
+
+    if ((fd = open(filename, O_RDONLY)) < 0)
+	return(0);
+
+    while ((num = read(fd, buffer, sizeof(buffer))) > 0)
+	HASHUpdate(&ctx, buffer, num);
+
+    oerrno = errno;
+    close(fd);
+    errno = oerrno;
+    return(num < 0 ? 0 : HASHEnd(&ctx, buf));
+}
+
+char *
+HASHData(const u_char *data, size_t len, char buf[HASH_DIGEST_STRING_LENGTH])
+{
+    HASH_CTX ctx;
+
+    HASHInit(&ctx);
+    HASHUpdate(&ctx, data, len);
+    return(HASHEnd(&ctx, buf));
+}

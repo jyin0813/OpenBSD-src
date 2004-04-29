db3483a4a1341c1deebf8c5ca4e8d4f5cdc09c1e
miod <miod@openbsd.org>
2004-04-29 20:33:27 +0000

    Continue factorizing m88k common code, this time files in <arch>/<arch> which were copied verbatim from mvme88k to luna88k.
    This requires backing out syntactic sugar in mvme88k kernel configuration
    files which would deduct the required processor types from the board models,
    as the common code only depends upon the M88100 and M88110 defines.

--- sys/arch/luna88k/luna88k/Attic/cmmu.c
+++ sys/arch/luna88k/luna88k/Attic/cmmu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cmmu.c,v 1.27 2004/01/20 14:35:54 miod Exp $	*/
+/*	$OpenBSD: cmmu.c,v 1.1 2004/04/21 15:23:58 aoyama Exp $	*/
 /*
  * Copyright (c) 1998 Steve Murphree, Jr.
  * Copyright (c) 1996 Nivas Madhur

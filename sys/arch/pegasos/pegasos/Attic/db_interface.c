675ff2e86ca1b6eef70f99618706fdc48b242663
drahn <drahn@openbsd.org>
2004-05-03 21:18:20 +0000

    Remove OpenBSD/pegaos, this has been announced before but delayed until the tree was stable. No point in code supporting a company which screws its employees being in the OpenBSD tree.

--- sys/arch/pegasos/pegasos/Attic/db_interface.c
+++ sys/arch/pegasos/pegasos/Attic/db_interface.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_interface.c,v 1.7 2003/10/15 17:50:16 drahn Exp $	*/
+/*	$OpenBSD: db_interface.c,v 1.1 2003/10/31 03:54:33 drahn Exp $	*/
 /*      $NetBSD: db_interface.c,v 1.12 2001/07/22 11:29:46 wiz Exp $ */
 
 #include <sys/param.h>

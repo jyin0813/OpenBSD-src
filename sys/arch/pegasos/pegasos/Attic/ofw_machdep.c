81194a042e3e39a73a5a4dc09f27a665c141bd33
drahn <drahn@openbsd.org>
2004-05-03 21:18:20 +0000

    Remove OpenBSD/pegaos, this has been announced before but delayed until the tree was stable. No point in code supporting a company which screws its employees being in the OpenBSD tree.

--- sys/arch/pegasos/pegasos/Attic/ofw_machdep.c
+++ sys/arch/pegasos/pegasos/Attic/ofw_machdep.c
@@ -1,4 +1,4 @@
- /*	$OpenBSD: ofw_machdep.c,v 1.1 2003/10/31 03:54:33 drahn Exp $	*/
+ /*	$OpenBSD: ofw_machdep.c,v 1.2 2004/02/04 20:07:18 drahn Exp $	*/
 /*	$NetBSD: ofw_machdep.c,v 1.1 1996/09/30 16:34:50 ws Exp $	*/
 
 /*

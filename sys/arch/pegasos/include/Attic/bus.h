fe4e2b61a4726d05cd75498c643f472006728ad8
drahn <drahn@openbsd.org>
2004-05-03 21:18:20 +0000

    Remove OpenBSD/pegaos, this has been announced before but delayed until the tree was stable. No point in code supporting a company which screws its employees being in the OpenBSD tree.

--- sys/arch/pegasos/include/Attic/bus.h
+++ sys/arch/pegasos/include/Attic/bus.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bus.h,v 1.1 2003/10/31 03:54:33 drahn Exp $	*/
+/*	$OpenBSD: bus.h,v 1.2 2004/02/04 20:07:18 drahn Exp $	*/
 
 /*
  * Copyright (c) 1997 Per Fogelstrom.  All rights reserved.

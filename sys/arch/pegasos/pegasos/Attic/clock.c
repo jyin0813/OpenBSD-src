81fbcaa659cd81b2f8c6c393dd00ea8670f215e2
drahn <drahn@openbsd.org>
2004-05-03 21:18:20 +0000

    Remove OpenBSD/pegaos, this has been announced before but delayed until the tree was stable. No point in code supporting a company which screws its employees being in the OpenBSD tree.

--- sys/arch/pegasos/pegasos/Attic/clock.c
+++ sys/arch/pegasos/pegasos/Attic/clock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: clock.c,v 1.1 2003/10/31 03:54:33 drahn Exp $	*/
+/*	$OpenBSD: clock.c,v 1.2 2003/12/18 20:06:15 drahn Exp $	*/
 /*	$NetBSD: clock.c,v 1.1 1996/09/30 16:34:40 ws Exp $	*/
 
 /*

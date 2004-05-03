1efb8e499a8ecb9c34035990a86fba49beca4286
drahn <drahn@openbsd.org>
2004-05-03 21:18:20 +0000

    Remove OpenBSD/pegaos, this has been announced before but delayed until the tree was stable. No point in code supporting a company which screws its employees being in the OpenBSD tree.

--- sys/arch/pegasos/pci/Attic/pchb.c
+++ sys/arch/pegasos/pci/Attic/pchb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pchb.c,v 1.1.2.1 2003/10/10 17:04:14 drahn Exp $	*/
+/*	$OpenBSD: pchb.c,v 1.1 2003/10/31 03:54:33 drahn Exp $	*/
 /*	$NetBSD: pchb.c,v 1.4 2000/01/25 07:19:11 tsubai Exp $	*/
 
 /*-

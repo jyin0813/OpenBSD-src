261bae01e3340dcb3edb7251cd5b1b8c52c36c69
drahn <drahn@openbsd.org>
2004-05-03 21:18:20 +0000

    Remove OpenBSD/pegaos, this has been announced before but delayed until the tree was stable. No point in code supporting a company which screws its employees being in the OpenBSD tree.

--- sys/arch/pegasos/pegasos/Attic/openfirm.c
+++ sys/arch/pegasos/pegasos/Attic/openfirm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: openfirm.c,v 1.1 2003/10/31 03:54:33 drahn Exp $	*/
+/*	$OpenBSD: openfirm.c,v 1.2 2004/01/14 23:27:53 brad Exp $	*/
 /*	$NetBSD: openfirm.c,v 1.1 1996/09/30 16:34:52 ws Exp $	*/
 
 /*

1203e22a6b46d830a488b0d44d66e96a87aa782b
drahn <drahn@openbsd.org>
2004-05-03 21:18:20 +0000

    Remove OpenBSD/pegaos, this has been announced before but delayed until the tree was stable. No point in code supporting a company which screws its employees being in the OpenBSD tree.

--- sys/arch/pegasos/pci/Attic/pci_addr_fixup.c
+++ sys/arch/pegasos/pci/Attic/pci_addr_fixup.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pci_addr_fixup.c,v 1.3 2003/11/14 20:14:32 drahn Exp $	*/
+/*	$OpenBSD: pci_addr_fixup.c,v 1.4 2003/12/20 22:40:27 miod Exp $	*/
 /*	$NetBSD: pci_addr_fixup.c,v 1.7 2000/08/03 20:10:45 nathanw Exp $	*/
 
 /*-

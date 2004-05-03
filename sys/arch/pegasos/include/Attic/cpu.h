269e9cf0ace332db5466e46e565df531cbcf69ee
drahn <drahn@openbsd.org>
2004-05-03 21:18:20 +0000

    Remove OpenBSD/pegaos, this has been announced before but delayed until the tree was stable. No point in code supporting a company which screws its employees being in the OpenBSD tree.

--- sys/arch/pegasos/include/Attic/cpu.h
+++ sys/arch/pegasos/include/Attic/cpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.h,v 1.1.2.1 2003/10/10 17:04:14 drahn Exp $	*/
+/*	$OpenBSD: cpu.h,v 1.1 2003/10/31 03:54:33 drahn Exp $	*/
 /*	$NetBSD: cpu.h,v 1.1 1996/09/30 16:34:21 ws Exp $	*/
 
 /*

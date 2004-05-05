bbc5f74264ce1ef9adb997211072d9b38e70561d
deraadt <deraadt@openbsd.org>
2004-05-05 05:37:06 +0000

    bye bye bootpd.  You will not be missed.

--- usr.sbin/bootpd/Attic/bootpef.c
+++ usr.sbin/bootpd/Attic/bootpef.c
@@ -21,7 +21,7 @@ SOFTWARE.
 ************************************************************************/
 
 #ifndef lint
-static char rcsid[] = "$Id: bootpef.c,v 1.2 1996/08/22 09:24:28 deraadt Exp $";
+static char rcsid[] = "$Id: bootpef.c,v 1.3 2002/06/10 19:53:36 hin Exp $";
 #endif
 
 

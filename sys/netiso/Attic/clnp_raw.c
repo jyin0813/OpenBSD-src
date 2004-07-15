bb2a8cb91190c9b55c59b25b1d3e70005a3d89ed
henning <henning@openbsd.org>
2004-07-15 18:10:45 +0000

    bye bye netiso, you won't be missed ok millert deraadt

--- sys/netiso/Attic/clnp_raw.c
+++ sys/netiso/Attic/clnp_raw.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: clnp_raw.c,v 1.6 2003/12/10 07:22:43 itojun Exp $	*/
+/*	$OpenBSD: clnp_raw.c,v 1.7 2004/01/03 14:08:54 espie Exp $	*/
 /*	$NetBSD: clnp_raw.c,v 1.9 1996/02/13 22:08:42 christos Exp $	*/
 
 /*-

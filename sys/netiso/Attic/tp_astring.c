d53852b5e8c30af10cbed699d4b2318d2074d432
henning <henning@openbsd.org>
2004-07-15 18:10:45 +0000

    bye bye netiso, you won't be missed ok millert deraadt

--- sys/netiso/Attic/tp_astring.c
+++ sys/netiso/Attic/tp_astring.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tp_astring.c,v 1.2 1996/03/04 10:35:48 mickey Exp $	*/
+/*	$OpenBSD: tp_astring.c,v 1.3 2003/06/02 23:28:18 millert Exp $	*/
 /*	$NetBSD: tp_astring.c,v 1.3 1996/02/13 22:10:37 christos Exp $	*/
 
 /*-

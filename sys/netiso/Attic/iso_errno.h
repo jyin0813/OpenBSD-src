e02b3d146658a6995f3f2ec9ee78d72759776f75
henning <henning@openbsd.org>
2004-07-15 18:10:45 +0000

    bye bye netiso, you won't be missed ok millert deraadt

--- sys/netiso/Attic/iso_errno.h
+++ sys/netiso/Attic/iso_errno.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: iso_errno.h,v 1.2 1996/03/04 10:35:32 mickey Exp $	*/
+/*	$OpenBSD: iso_errno.h,v 1.3 2003/06/02 23:28:17 millert Exp $	*/
 /*	$NetBSD: iso_errno.h,v 1.7 1996/02/13 22:10:08 christos Exp $	*/
 
 /*-

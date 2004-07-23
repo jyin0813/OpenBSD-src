3a8904abbc3c1a0c05ef409bb8db12f11ad64404
miod <miod@openbsd.org>
2004-07-23 21:34:04 +0000

    Move luna88k and mvme88k vm_machdep to m88k-agnostic code area.

--- sys/arch/mvme88k/mvme88k/Attic/vm_machdep.c
+++ sys/arch/mvme88k/mvme88k/Attic/vm_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vm_machdep.c,v 1.62 2004/05/23 20:52:16 miod Exp $	*/
+/*	$OpenBSD: vm_machdep.c,v 1.63 2004/05/23 20:53:18 miod Exp $	*/
 
 /*
  * Copyright (c) 1998 Steve Murphree, Jr.

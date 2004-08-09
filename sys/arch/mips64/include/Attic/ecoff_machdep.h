a7fd42c9875519467f057e9e5886ce8b876e1208
pefo <pefo@openbsd.org>
2004-08-09 20:57:26 +0000

    Big cleanup. Removed some unused obsolete stuff and fixed copyrights on some files. Arcbios support is now in, thus detects memorysize and cpu clock frequency.

--- sys/arch/mips64/include/Attic/ecoff_machdep.h
+++ sys/arch/mips64/include/Attic/ecoff_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ecoff.h,v 1.2 1999/01/27 04:46:05 imp Exp $	*/
+/*	$OpenBSD: ecoff_machdep.h,v 1.1 2004/08/06 20:56:01 pefo Exp $	*/
 /*	$NetBSD: ecoff.h,v 1.4 1995/06/16 02:07:33 mellon Exp $	*/
 
 /*

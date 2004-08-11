777b121935d1e91622c3a7612ae121df4760d2c2
pefo <pefo@openbsd.org>
2004-08-11 23:34:32 +0000

    bye

--- lib/libc/arch/mips/gen/Attic/fpsetmask.c
+++ lib/libc/arch/mips/gen/Attic/fpsetmask.c
@@ -4,7 +4,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: fpsetmask.c,v 1.2 1996/08/19 08:15:57 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <ieeefp.h>

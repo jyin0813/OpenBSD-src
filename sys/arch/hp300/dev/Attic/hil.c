c473e53e2ea2dc442220fa8adaf793531c9a9021
miod <miod@openbsd.org>
2005-01-15 05:39:25 +0000

    Switch OpenBSD/hp300 to wscons, still using on-board ROM fonts for now.
    Main features:
    - MI hil drivers, allowing live plug/unplug of devices, and the keyboard
      does not need to be at id 1 anymore.
    - SGC bus support on 4xx models. SGC frame buffers are supported too, but
      not as console, though you can use them as a regular glass terminal if
      you run a getty on ttyC0. Currently not compiled-in, awaiting for an sti(4)
      fix to be commited.
    - HP-UX compatibility interfaces are removed. grfinfo and hilinfo are removed.
    
    X11 support is currently broken by these changes; X.org X11R6 support will
    be available very soon.
    
    Tested on hyperion and 4 different flavours of catseye/topcat/kathmandu; other,
    older frame buffers (davinci, gatorbox and renaissance) could not been tested
    but should work; please report any regression.

--- sys/arch/hp300/dev/Attic/hil.c
+++ sys/arch/hp300/dev/Attic/hil.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hil.c,v 1.19 2003/09/23 16:51:11 millert Exp $	*/
+/*	$OpenBSD: hil.c,v 1.20 2003/09/24 06:41:43 miod Exp $	*/
 /*	$NetBSD: hil.c,v 1.34 1997/04/02 22:37:32 scottr Exp $	*/
 
 /*

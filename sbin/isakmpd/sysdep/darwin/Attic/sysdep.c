22b164fd112776815445bcc3e33c1f71fbff7bd7
deraadt <deraadt@openbsd.org>
2005-04-08 21:47:55 +0000

    if someone wants a portable, create a -p project.  clutter stops quality

--- sbin/isakmpd/sysdep/darwin/Attic/sysdep.c
+++ sbin/isakmpd/sysdep/darwin/Attic/sysdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sysdep.c,v 1.2 2003/06/03 14:53:11 ho Exp $	*/
+/*	$OpenBSD: sysdep.c,v 1.3 2004/08/10 15:59:10 ho Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Niklas Hallqvist.  All rights reserved.

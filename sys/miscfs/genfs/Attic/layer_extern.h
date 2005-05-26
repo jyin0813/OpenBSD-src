9010ef49d50c461bc24dcf509f024b0ad617cc0e
pedro <pedro@openbsd.org>
2005-05-26 06:38:50 +0000

    bye bye, rest in attic ad infinitum, amen

--- sys/miscfs/genfs/Attic/layer_extern.h
+++ sys/miscfs/genfs/Attic/layer_extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: layer_extern.h,v 1.1 2003/05/12 20:58:40 tedu Exp $ */
+/*	$OpenBSD: layer_extern.h,v 1.2 2003/06/02 23:28:10 millert Exp $ */
 /*	$NetBSD: layer_extern.h,v 1.5 2001/12/06 04:29:23 chs Exp $	*/
 
 /*

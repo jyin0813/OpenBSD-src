20ef0e72daed342adbcc52d5ccc3d4f2c3e3453f
henning <henning@openbsd.org>
2005-06-16 23:37:55 +0000

    remove remaining netccitt parts, full ports build test by pval, thanks!

--- sys/netccitt/Attic/pk_debug.c
+++ sys/netccitt/Attic/pk_debug.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pk_debug.c,v 1.3 2003/06/02 23:28:13 millert Exp $	*/
+/*	$OpenBSD: pk_debug.c,v 1.4 2003/12/10 07:22:42 itojun Exp $	*/
 /*	$NetBSD: pk_debug.c,v 1.6 1996/02/13 22:05:14 christos Exp $	*/
 
 /*

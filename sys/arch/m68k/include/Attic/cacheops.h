7f2652e77d3ce95cdf740ff43b8ccf5850db8af4
miod <miod@openbsd.org>
2005-08-14 18:51:22 +0000

    Remove the inline cache and TLB primitives - we don't use them at this point, they do not cover the HP MMU for hp300, and they do not cover 040+060 kernels (which share all cache operations) efficiently.

--- sys/arch/m68k/include/Attic/cacheops.h
+++ sys/arch/m68k/include/Attic/cacheops.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cacheops.h,v 1.2 2001/05/15 01:43:14 millert Exp $	*/
+/*	$OpenBSD: cacheops.h,v 1.3 2002/03/14 01:26:34 millert Exp $	*/
 /*	$NetBSD: cacheops.h,v 1.1 1997/06/02 20:26:37 leo Exp $	*/
 
 /*-

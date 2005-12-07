bed8d30f5f471ce125c8f679f572cd4326758286
pedro <pedro@openbsd.org>
2005-12-07 08:47:04 +0000

    Moved to regress/sys/dev/fdesc

--- regress/sys/fdescfs/confuse/Attic/confuse.c
+++ regress/sys/fdescfs/confuse/Attic/confuse.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: confuse.c,v 1.1 2002/02/08 17:33:32 art Exp $	*/
+/*	$OpenBSD: confuse.c,v 1.2 2003/07/31 21:48:07 deraadt Exp $	*/
 /*
  *	Written by Artur Grabowski <art@openbsd.org> 2002 Public Domain.
  */

3974d26aeba561f3cbcbd4dbe0fcac0ca2faed5a
xsa <xsa@openbsd.org>
2006-01-02 15:11:56 +0000

    #include's cleanup; ok joris@ niallo@.

--- usr.bin/cvs/version.c
+++ usr.bin/cvs/version.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: version.c,v 1.17 2005/06/02 20:15:03 joris Exp $	*/
+/*	$OpenBSD: version.c,v 1.18 2005/07/25 12:13:08 xsa Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -24,13 +24,7 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/types.h>
-
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "includes.h"
 
 #include "cvs.h"
 #include "log.h"

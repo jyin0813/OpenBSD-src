878d2fd4b545a5a9ab0abc59423bf954395555ba
millert <millert@openbsd.org>
2006-01-07 01:50:10 +0000

    Merge machine/ansi.h and machine/types.h into machine/_types.h and rename the types using the __ protected namespace (e.g. __size_t). Idea from FreeBSD.

--- sys/arch/zaurus/include/Attic/ansi.h
+++ sys/arch/zaurus/include/Attic/ansi.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ansi.h,v 1.1 2004/02/01 05:12:54 drahn Exp $	*/
+/*	$OpenBSD: ansi.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
 /*	$NetBSD: ansi.h,v 1.3 2001/11/25 15:55:54 thorpej Exp $	*/
 
 #include <arm/ansi.h>

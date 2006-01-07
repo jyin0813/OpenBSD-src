960dfd604776bb20951844eac0f7e1de1793f11d
millert <millert@openbsd.org>
2006-01-07 01:50:08 +0000

    Merge machine/ansi.h and machine/types.h into machine/_types.h and rename the types using the __ protected namespace (e.g. __size_t). Idea from FreeBSD.

--- sys/arch/alpha/include/Attic/types.h
+++ sys/arch/alpha/include/Attic/types.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: types.h,v 1.17 2005/12/17 07:31:23 miod Exp $	*/
+/*	$OpenBSD: types.h,v 1.18 2006/01/02 05:21:20 brad Exp $	*/
 /*	$NetBSD: types.h,v 1.6 1996/12/05 00:13:47 cgd Exp $	*/
 
 /*-

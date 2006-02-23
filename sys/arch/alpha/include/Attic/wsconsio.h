eeebcb0db876c83b755425e1db362b544e25a77f
miod <miod@openbsd.org>
2006-02-23 05:05:01 +0000

    Nuke unused includes.

--- sys/arch/alpha/include/Attic/wsconsio.h
+++ sys/arch/alpha/include/Attic/wsconsio.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wsconsio.h,v 1.1 1996/04/12 01:43:06 cgd Exp $	*/
+/*	$OpenBSD: wsconsio.h,v 1.3 1996/10/30 22:39:35 niklas Exp $	*/
 /*	$NetBSD: wsconsio.h,v 1.1 1996/04/12 01:43:06 cgd Exp $	*/
 
 /*

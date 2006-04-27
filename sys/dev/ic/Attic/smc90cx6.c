b2a5fc0e59e5959e4b5994354316e3d49127963d
claudio <claudio@openbsd.org>
2006-04-27 03:13:38 +0000

    Remove smc90cx6 files which are no longer used by anything. Found by jsg@ OK brad@ martin@

--- sys/dev/ic/Attic/smc90cx6.c
+++ sys/dev/ic/Attic/smc90cx6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smc90cx6.c,v 1.11 2005/02/14 14:00:49 brad Exp $ */
+/*	$OpenBSD: smc90cx6.c,v 1.12 2006/03/25 22:41:43 djm Exp $ */
 /*	$NetBSD: smc90cx6.c,v 1.17 1996/05/07 01:43:18 thorpej Exp $ */
 
 /*

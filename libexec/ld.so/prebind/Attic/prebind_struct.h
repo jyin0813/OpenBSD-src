2dc8d5a9fc110d16daeacbff85f2db4a2b4c3210
deraadt <deraadt@openbsd.org>
2006-05-13 05:20:53 +0000

    move the prebind code into ldconfig as the -P option. next we will want to enhance the verbose mode, fix error handling, and change the prebind delete code to use the same directory handling code ok drahn

--- libexec/ld.so/prebind/Attic/prebind_struct.h
+++ libexec/ld.so/prebind/Attic/prebind_struct.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: prebind_struct.h,v 1.4 2006/05/12 15:12:42 drahn Exp $ */
+/* $OpenBSD: prebind_struct.h,v 1.5 2006/05/12 16:37:59 drahn Exp $ */
 /*
  * Copyright (c) 2006 Dale Rahn <drahn@dalerahn.com>
  *

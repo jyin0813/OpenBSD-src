a9b9ff932ba9d623e3f194d9e46e6fbc2a1feb9a
miod <miod@openbsd.org>
2006-05-27 02:20:51 +0000

    No md math.h in OpenBSD

--- sys/arch/arm/include/Attic/math.h
+++ sys/arch/arm/include/Attic/math.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: math.h,v 1.2 2004/01/29 16:17:16 drahn Exp $	*/
+/*	$OpenBSD: math.h,v 1.1 2004/02/01 05:09:49 drahn Exp $	*/
 /*	$NetBSD: math.h,v 1.2 2002/02/19 13:08:14 simonb Exp $	*/
 
 #define	__HAVE_NANF

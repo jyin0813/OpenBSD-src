73e6ebf6555491e3c0734dbc997db20c5f575c25
miod <miod@openbsd.org>
2006-05-31 01:51:39 +0000

    Not necessary here either.

--- sys/arch/zaurus/include/Attic/irqhandler.h
+++ sys/arch/zaurus/include/Attic/irqhandler.h
@@ -1,4 +1,4 @@
-/* 	$OpenBSD: irqhandler.h,v 1.1 2004/02/01 05:12:54 drahn Exp $	*/
+/* 	$OpenBSD: irqhandler.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
 /*	$NetBSD: irqhandler.h,v 1.4 2002/04/12 18:50:32 thorpej Exp $	*/
 
 #include <arm/footbridge/footbridge_irqhandler.h>

49c389addff98d8e16c2ff360e9ed0aff770b8e8
xsa <xsa@openbsd.org>
2007-01-02 20:51:13 +0000

    skeletons for the client-side part of the editors and watchers commands. not linked to build.

--- usr.bin/cvs/edit.c
+++ usr.bin/cvs/edit.c
@@ -1,186 +1,92 @@
-/*	$OpenBSD: edit.c,v 1.12 2006/03/16 09:06:19 xsa Exp $	*/
+/*	$OpenBSD$	*/
 /*
- * Copyright (c) 2005 Jean-Francois Brousseau <jfb@openbsd.org>
- * All rights reserved.
+ * Copyright (c) 2006 Xavier Santolaria <xsa@openbsd.org>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include "includes.h"
 
 #include "cvs.h"
 #include "log.h"
-#include "proto.h"
+#include "remote.h"
 
-
-
-static int	cvs_edit_init(struct cvs_cmd *, int, char **, int *);
-static int	cvs_edit_remote(CVSFILE *, void *);
-static int	cvs_edit_local(CVSFILE *, void *);
-
-static int	cvs_editors_remote(CVSFILE *, void *);
-
-
-struct cvs_cmd cvs_cmd_edit = {
-	CVS_OP_EDIT, CVS_REQ_NOOP, "edit",
-	{ },
-	"Mark a file as being edited",
-	"[-lR] [-a action] [file ...]",
-	"a:lR",
-	NULL,
-	CF_SORT | CF_RECURSE,
-	cvs_edit_init,
-	NULL,
-	cvs_edit_remote,
-	cvs_edit_local,
-	NULL,
-	NULL,
-	0
-};
+static void	cvs_editors_local(struct cvs_file *);
 
 struct cvs_cmd cvs_cmd_editors = {
-	CVS_OP_EDITORS, CVS_REQ_EDITORS, "editors",
+	CVS_OP_EDITORS, 0, "editors",
 	{ },
-	"List editors on a file",
+	"See who is editing a watched file",
 	"[-lR] [file ...]",
 	"lR",
 	NULL,
-	CF_SORT | CF_RECURSE,
-	cvs_edit_init,
-	NULL,
-	cvs_editors_remote,
-	NULL,
-	NULL,
-	NULL,
-	CVS_CMD_SENDDIR | CVS_CMD_ALLOWSPEC | CVS_CMD_SENDARGS2
+	cvs_editors
 };
 
-
-struct cvs_cmd cvs_cmd_unedit = {
-	CVS_OP_UNEDIT, CVS_REQ_NOOP, "unedit",
-	{ },
-	"Undo an edit command",
-	"[-lR] [file ...]",
-	"lR",
-	NULL,
-	CF_SORT | CF_RECURSE,
-	cvs_edit_init,
-	NULL,
-	cvs_edit_remote,
-	cvs_edit_local,
-	NULL,
-	NULL,
-	0
-};
-
-
-
-static int
-cvs_edit_init(struct cvs_cmd *cmd, int argc, char **argv, int *arg)
+int
+cvs_editors(int argc, char **argv)
 {
 	int ch;
+	int flags;
+	struct cvs_recursion cr;
 
-	while ((ch = getopt(argc, argv, cmd->cmd_opts)) != -1) {
+	flags = CR_RECURSE_DIRS;
+
+	while ((ch = getopt(argc, argv, cvs_cmd_editors.cmd_opts)) != -1) {
 		switch (ch) {
-		case 'a':
-			/*
-			 * The `editors' and `unedit' commands do not have
-			 * the -a option. Check which command has been issued.
-			 */
-			if (cvs_cmdop != CVS_OP_EDIT)
-				return (CVS_EX_USAGE);
-			break;
 		case 'l':
-			cmd->file_flags &= ~CF_RECURSE;
+			flags &= ~CR_RECURSE_DIRS;
 			break;
 		case 'R':
-			cmd->file_flags |= CF_RECURSE;
 			break;
 		default:
-			return (CVS_EX_USAGE);
+			fatal("%s", cvs_cmd_editors.cmd_synopsis);
 		}
 	}
 
-	*arg = optind;
-	return (CVS_EX_OK);
-}
+	argc -= optind;
+	argv += optind;
 
+	if (argc == 0)
+		fatal("%s", cvs_cmd_editors.cmd_synopsis);
 
-/*
- * cvs_edit_remote()
- *
- */
-static int
-cvs_edit_remote(CVSFILE *cf, void *arg)
-{
-	return (CVS_EX_OK);
-}
+	cr.enterdir = NULL;
+	cr.leavedir = NULL;
 
+	if (current_cvsroot->cr_method != CVS_METHOD_LOCAL) {
+		cr.fileproc = cvs_client_sendfile;
 
-/*
- * cvs_edit_local()
- *
- */
-static int
-cvs_edit_local(CVSFILE *cf, void *arg)
-{
-	return (CVS_EX_OK);
-}
-
-
-/*
- * cvs_editors_remote()
- *
- */
-static int
-cvs_editors_remote(CVSFILE *cf, void *arg)
-{
-	struct cvsroot *root;
-
-	root = CVS_DIR_ROOT(cf);
-
-	if (cf->cf_type == DT_DIR) {
-		if (cf->cf_cvstat == CVS_FST_UNKNOWN)
-			cvs_sendreq(root, CVS_REQ_QUESTIONABLE, cf->cf_name);
-		else
-			cvs_senddir(root, cf);
-		return (0);
+		if (!(flags & CR_RECURSE_DIRS))
+			cvs_client_send_request("Argument -l");
+	} else {
+		cr.fileproc = cvs_editors_local;
 	}
 
-	cvs_sendentry(root, cf);
+	cr.flags = flags;
 
-	switch (cf->cf_cvstat) {
-	case CVS_FST_UNKNOWN:
-		cvs_sendreq(root, CVS_REQ_QUESTIONABLE, cf->cf_name);
-		break;
-	case CVS_FST_UPTODATE:
-		cvs_sendreq(root, CVS_REQ_UNCHANGED, cf->cf_name);
-		break;
-	case CVS_FST_ADDED:
-	case CVS_FST_MODIFIED:
-		cvs_sendreq(root, CVS_REQ_ISMODIFIED, cf->cf_name);
-		break;
-	default:
-		break;
+	cvs_file_run(argc, argv, &cr);
+
+	if (current_cvsroot->cr_method != CVS_METHOD_LOCAL) {
+		cvs_client_send_files(argv, argc);
+		cvs_client_senddir(".");
+		cvs_client_send_request("editors");
+		cvs_client_get_responses();
 	}
 
 	return (0);
+}
+
+static void
+cvs_editors_local(struct cvs_file *cf)
+{
 }

7c359f1855a74c612c70c8b3aec065dbcfdde1da
niallo <niallo@openbsd.org>
2007-02-15 00:54:16 +0000

    remove GNU RCS implementation from the tree.  Open RCS has been used for over a release now.
    prompted by deraadt@, ok joris@

--- gnu/usr.bin/rcs/src/Attic/rcsdiff.c
+++ gnu/usr.bin/rcs/src/Attic/rcsdiff.c
@@ -29,6 +29,9 @@ Report problems and direct all questions to:
 
 /*
  * $Log: rcsdiff.c,v $
+ * Revision 1.1  1996/08/12 04:08:15  millert
+ * rcs 5.7 + OpenBSD changes
+ *
  * Revision 5.19  1995/06/16 06:19:24  eggert
  * Update FSF address.
  *
@@ -152,7 +155,7 @@ static int exitstatus;
 static RILE *workptr;
 static struct stat workstat;
 
-mainProg(rcsdiffId, "rcsdiff", "$Id: rcsdiff.c,v 5.19 1995/06/16 06:19:24 eggert Exp $")
+mainProg(rcsdiffId, "rcsdiff", "$Id: rcsdiff.c,v 1.1 1996/08/12 04:08:15 millert Exp $")
 {
     static char const cmdusage[] =
 	    "\nrcsdiff usage: rcsdiff -ksubst -q -rrev1 [-rrev2] -Vn -xsuff -zzone [diff options] file ...";

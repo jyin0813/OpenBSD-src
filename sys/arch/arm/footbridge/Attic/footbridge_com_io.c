d9848c6c84170fe79c2cd0f83b56773838b6c771
miod <miod@openbsd.org>
2007-08-02 03:28:32 +0000

    I have a dream of cvs not missing the files I have cvs rm'd with love.

--- sys/arch/arm/footbridge/Attic/footbridge_com_io.c
+++ sys/arch/arm/footbridge/Attic/footbridge_com_io.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: footbridge_com_io.c,v 1.1 2004/02/01 05:09:49 drahn Exp $	*/
+/*	$OpenBSD: footbridge_com_io.c,v 1.2 2007/05/25 16:45:59 krw Exp $	*/
 /*	$NetBSD: footbridge_com_io.c,v 1.4 2002/09/27 15:35:44 provos Exp $	*/
 
 /*

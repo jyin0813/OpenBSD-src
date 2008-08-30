b85f3d9f5758a9018c7508dc0d359fe9ccdc2cca
jsg <jsg@openbsd.org>
2008-08-30 08:46:56 +0000

    Remove the header that exposed the old wdt ioctl interface to userland, wdt was converted to use the common watchdog code some time ago so this is no longer needed.
    "yes, we should remove it" mbalmer@

--- sys/dev/pci/Attic/wdt50x.h
+++ sys/dev/pci/Attic/wdt50x.h
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$OpenBSD$
+ *	$OpenBSD: wdt50x.h,v 1.1 1999/04/28 23:21:05 alex Exp $
  */
 
 #ifndef WDT_H

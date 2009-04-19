5456ccdb6eb861ea3f54f724472e63b0dd6aa83b
martynas <martynas@openbsd.org>
2009-04-19 22:42:05 +0000

    - ldexp implementation has issues.  switch to the one from libm - remove frexp in hppa64, cloned from hppa - move generic ieee754 implementations of modf and ldexp to gen ok kettenis@, "looks good" millert@

--- lib/libc/arch/m88k/gen/Attic/ldexp.c
+++ lib/libc/arch/m88k/gen/Attic/ldexp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ldexp.c,v 1.4 2003/06/02 20:18:31 millert Exp $	*/
+/*	$OpenBSD: ldexp.c,v 1.5 2005/08/07 16:40:14 espie Exp $	*/
 /*
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.

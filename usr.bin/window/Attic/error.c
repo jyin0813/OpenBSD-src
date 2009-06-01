580646ba61848b804ed977a5a3f1c996e5e40fa8
deraadt <deraadt@openbsd.org>
2009-06-01 22:57:27 +0000

    RIP; ok millert

--- usr.bin/window/Attic/error.c
+++ usr.bin/window/Attic/error.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: error.c,v 1.5 2001/11/19 19:02:18 mpech Exp $	*/
+/*	$OpenBSD: error.c,v 1.6 2003/06/03 02:56:23 millert Exp $	*/
 /*	$NetBSD: error.c,v 1.3 1995/09/28 10:34:20 tls Exp $	*/
 
 /*
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)error.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: error.c,v 1.5 2001/11/19 19:02:18 mpech Exp $";
+static char rcsid[] = "$OpenBSD: error.c,v 1.6 2003/06/03 02:56:23 millert Exp $";
 #endif
 #endif /* not lint */
 

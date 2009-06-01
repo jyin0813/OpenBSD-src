3e12794c436b501bb163994d3bfdec8478797490
deraadt <deraadt@openbsd.org>
2009-06-01 22:57:27 +0000

    RIP; ok millert

--- usr.bin/window/Attic/win.c
+++ usr.bin/window/Attic/win.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: win.c,v 1.6 2001/11/19 19:02:18 mpech Exp $	*/
+/*	$OpenBSD: win.c,v 1.7 2003/06/03 02:56:23 millert Exp $	*/
 /*	$NetBSD: win.c,v 1.8 1996/02/08 21:07:57 mycroft Exp $	*/
 
 /*
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)win.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: win.c,v 1.6 2001/11/19 19:02:18 mpech Exp $";
+static char rcsid[] = "$OpenBSD: win.c,v 1.7 2003/06/03 02:56:23 millert Exp $";
 #endif
 #endif /* not lint */
 

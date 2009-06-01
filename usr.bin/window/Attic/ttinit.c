f28ad773fd9baa1d4960def2ecf94709b55155d0
deraadt <deraadt@openbsd.org>
2009-06-01 22:57:27 +0000

    RIP; ok millert

--- usr.bin/window/Attic/ttinit.c
+++ usr.bin/window/Attic/ttinit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ttinit.c,v 1.9 2001/11/19 19:02:18 mpech Exp $	*/
+/*	$OpenBSD: ttinit.c,v 1.10 2003/06/03 02:56:23 millert Exp $	*/
 /*	$NetBSD: ttinit.c,v 1.3 1995/09/28 10:34:50 tls Exp $	*/
 
 /*
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)ttinit.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: ttinit.c,v 1.9 2001/11/19 19:02:18 mpech Exp $";
+static char rcsid[] = "$OpenBSD: ttinit.c,v 1.10 2003/06/03 02:56:23 millert Exp $";
 #endif
 #endif /* not lint */
 

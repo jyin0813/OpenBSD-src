22daef9454fc2034a7dfa48ae7b7586517cfe034
deraadt <deraadt@openbsd.org>
2009-06-01 22:57:27 +0000

    RIP; ok millert

--- usr.bin/window/Attic/ttwyse60.c
+++ usr.bin/window/Attic/ttwyse60.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ttwyse60.c,v 1.3 1997/02/25 00:04:27 downsj Exp $	*/
+/*	$OpenBSD: ttwyse60.c,v 1.4 2003/06/03 02:56:23 millert Exp $	*/
 /*	$NetBSD: ttwyse60.c,v 1.3 1995/09/28 10:34:55 tls Exp $	*/
 
 /*
@@ -44,7 +44,7 @@
 #if 0
 static char sccsid[] = "@(#)ttwyse60.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: ttwyse60.c,v 1.3 1997/02/25 00:04:27 downsj Exp $";
+static char rcsid[] = "$OpenBSD: ttwyse60.c,v 1.4 2003/06/03 02:56:23 millert Exp $";
 #endif
 #endif /* not lint */
 

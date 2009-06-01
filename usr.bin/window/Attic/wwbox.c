fa3a1f517725f0606e0218fc42ce35d061e4853b
deraadt <deraadt@openbsd.org>
2009-06-01 22:57:27 +0000

    RIP; ok millert

--- usr.bin/window/Attic/wwbox.c
+++ usr.bin/window/Attic/wwbox.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wwbox.c,v 1.4 2001/11/19 19:02:18 mpech Exp $	*/
+/*	$OpenBSD: wwbox.c,v 1.5 2003/06/03 02:56:23 millert Exp $	*/
 /*	$NetBSD: wwbox.c,v 1.3 1995/09/28 10:35:11 tls Exp $	*/
 
 /*
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)wwbox.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: wwbox.c,v 1.4 2001/11/19 19:02:18 mpech Exp $";
+static char rcsid[] = "$OpenBSD: wwbox.c,v 1.5 2003/06/03 02:56:23 millert Exp $";
 #endif
 #endif /* not lint */
 

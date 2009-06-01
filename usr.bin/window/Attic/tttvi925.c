98db647058ac120a7f78b30d2ffbd1461cb24a2f
deraadt <deraadt@openbsd.org>
2009-06-01 22:57:27 +0000

    RIP; ok millert

--- usr.bin/window/Attic/tttvi925.c
+++ usr.bin/window/Attic/tttvi925.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tttvi925.c,v 1.3 1997/02/25 00:04:26 downsj Exp $	*/
+/*	$OpenBSD: tttvi925.c,v 1.4 2003/06/03 02:56:23 millert Exp $	*/
 /*	$NetBSD: tttvi925.c,v 1.3 1995/09/28 10:34:54 tls Exp $	*/
 
 /*
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)tttvi925.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: tttvi925.c,v 1.3 1997/02/25 00:04:26 downsj Exp $";
+static char rcsid[] = "$OpenBSD: tttvi925.c,v 1.4 2003/06/03 02:56:23 millert Exp $";
 #endif
 #endif /* not lint */
 

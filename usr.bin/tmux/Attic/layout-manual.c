1205e4a8271bef046ec567b36a236b7dd2186036
nicm <nicm@openbsd.org>
2009-07-19 19:21:40 +0000

    Improved layout code.
    Each window now has a tree of layout cells associated with it. In this tree,
    each node is either a horizontal or vertical cell containing a list of other
    cells running from left-to-right or top-to-bottom, or a leaf cell which is
    associated with a pane.
    
    The major functional changes are:
    
    - panes may now be split arbitrarily both horizontally (splitw -h, C-b %) and
      vertically (splitw -v, C-b ");
    - panes may be resized both horizontally and vertically (resizep -L/-R/-U/-D,
      bound to C-b left/right/up/down and C-b M-left/right/up/down);
    - layouts are now applied and then may be modified by resizing or splitting
      panes, rather than being fixed and reapplied when the window is resized or
      panes are added;
    - manual-vertical layout is no longer necessary, and active-only layout is gone
      (but may return in future);
    - the main-pane layouts now reduce the size of the main pane to fit all panes
      if possible.
    
    Thanks to all who tested.

--- usr.bin/tmux/Attic/layout-manual.c
+++ usr.bin/tmux/Attic/layout-manual.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: layout-manual.c,v 1.1 2009/06/01 22:58:49 nicm Exp $ */
+/* $OpenBSD: layout-manual.c,v 1.2 2009/07/14 07:23:36 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>

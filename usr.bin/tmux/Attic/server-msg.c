72abf81ed324be2edc9e2507391c5a7709b6f690
nicm <nicm@openbsd.org>
2009-10-23 01:41:51 +0000

    Split the server code handling clients, jobs and windows off into separate files from server.c (merging server-msg.c into the client file) and rather than iterating over each set after poll(), allow a callback to be specified when the fd is added and just walk once over the returned pollfds calling each callback where needed.
    More to come, getting this in so it is tested.

--- usr.bin/tmux/Attic/server-msg.c
+++ usr.bin/tmux/Attic/server-msg.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: server-msg.c,v 1.23 2009/10/10 09:46:11 nicm Exp $ */
+/* $OpenBSD: server-msg.c,v 1.24 2009/10/21 20:16:40 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>

8ed25aa7bd2e2794a284368a60ac29f4adde1c2b
markus <markus@openbsd.org>
2010-02-08 19:48:38 +0000

    remove obsole scard code

--- usr.bin/ssh/Attic/scard.h
+++ usr.bin/ssh/Attic/scard.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: scard.h,v 1.13 2006/03/25 22:22:43 djm Exp $ */
+/* $OpenBSD: scard.h,v 1.14 2006/08/03 03:34:42 deraadt Exp $ */
 
 /*
  * Copyright (c) 2001 Markus Friedl.  All rights reserved.

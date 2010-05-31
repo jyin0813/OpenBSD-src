fba021286d9f687f913d7aa60b0817cc58960005
drahn <drahn@openbsd.org>
2010-05-31 23:27:01 +0000

    OpenBSD/moko development has stopped, it effectively stopped long ago when the not-publicly documented video driver needed to be written, however between that documentation issue and developer resouces, moko will be moved to the Attic.

--- sys/arch/moko/moko/Attic/moko_machdep.c
+++ sys/arch/moko/moko/Attic/moko_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: moko_machdep.c,v 1.3 2009/01/02 06:38:25 drahn Exp $	*/
+/*	$OpenBSD: moko_machdep.c,v 1.4 2009/07/26 18:48:55 miod Exp $	*/
 /*	$NetBSD: lubbock_machdep.c,v 1.2 2003/07/15 00:25:06 lukem Exp $ */
 
 /*

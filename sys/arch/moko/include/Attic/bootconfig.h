b42f3dcd22908ac35347df6c648ca2ab5d41970e
drahn <drahn@openbsd.org>
2010-05-31 23:27:01 +0000

    OpenBSD/moko development has stopped, it effectively stopped long ago when the not-publicly documented video driver needed to be written, however between that documentation issue and developer resouces, moko will be moved to the Attic.

--- sys/arch/moko/include/Attic/bootconfig.h
+++ sys/arch/moko/include/Attic/bootconfig.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bootconfig.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
+/*	$OpenBSD: bootconfig.h,v 1.1 2008/11/26 14:47:50 drahn Exp $	*/
 /*	$NetBSD: bootconfig.h,v 1.2 2001/06/21 22:08:28 chris Exp $	*/
 
 /*

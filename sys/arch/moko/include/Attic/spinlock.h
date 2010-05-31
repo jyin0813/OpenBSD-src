469104fd136fbab802bd0d1a4adf3c8f1a22d57a
drahn <drahn@openbsd.org>
2010-05-31 23:27:01 +0000

    OpenBSD/moko development has stopped, it effectively stopped long ago when the not-publicly documented video driver needed to be written, however between that documentation issue and developer resouces, moko will be moved to the Attic.

--- sys/arch/moko/include/Attic/spinlock.h
+++ sys/arch/moko/include/Attic/spinlock.h
@@ -1,5 +1,5 @@
 
-/*	$OpenBSD: spinlock.h,v 1.3 2005/12/01 22:09:59 deraadt Exp $	*/
+/*	$OpenBSD: spinlock.h,v 1.1 2008/11/26 14:47:50 drahn Exp $	*/
 #ifndef _MOKO_SPINLOCK_H_
 #define _MOKO_SPINLOCK_H_
 #include <arm/spinlock.h>

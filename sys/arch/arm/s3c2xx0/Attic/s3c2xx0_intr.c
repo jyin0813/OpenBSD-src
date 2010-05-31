80364002b726670f3fb9c728f8b0e0ad6125b6ff
drahn <drahn@openbsd.org>
2010-05-31 23:29:25 +0000

    OpenBSD/moko development has stopped, it effectively stopped long ago when the not-publicly documented video driver needed to be written, however between that documentation issue and developer resouces, moko will be moved to the Attic.

--- sys/arch/arm/s3c2xx0/Attic/s3c2xx0_intr.c
+++ sys/arch/arm/s3c2xx0/Attic/s3c2xx0_intr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: s3c2xx0_intr.c,v 1.2 2008/12/08 20:50:20 drahn Exp $ */
+/*	$OpenBSD: s3c2xx0_intr.c,v 1.3 2009/07/14 13:59:49 drahn Exp $ */
 /* $NetBSD: s3c2xx0_intr.c,v 1.13 2008/04/27 18:58:45 matt Exp $ */
 
 /*

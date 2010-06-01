513b47623d0403978da99353961f3e9536e7cca7
jacekm <jacekm@openbsd.org>
2010-06-01 05:38:56 +0000

    Rewrite entire queue code.
    Major goals:
    
    1) Fix bad performance caused by the runner process doing full queue
    read in 1s intervals.  My Soekris can now happily accept >50 msg/s
    while having multi-thousand queue; before, one hundred queue would
    bring the system to its knees.
    
    2) Introduce Qmail-like scheduler that doesn't write as much to the
    disk so that it needs less code for servicing error conditions,
    which in some places can be tricky to get right.
    
    3) Introduce separation between the scheduler and the backend; these
    two queue aspects shouldn't be too tied too each other.  This means
    that eg. storing queue in SQL requires rewrite of just queue_backend.c.
    
    4) Make on-disk queue format architecture independent, and more
    easily extensible, to reduce number of flag days in the future.
    
    Minor goals:
    
    ENOSPC no longer prevents delivery attempts, fixed session limiting
    for relayed mail, improved batching of "relay via" mails, human-readable
    mailq output, "show queue raw" command, clearer logging, sending
    of single bounce about multiple recipients, exact delay= computation,
    zero delay between deliveries while within session limit (currently
    1s delay between re-scheduling is enforced), mta no longer requests
    content fd, corrected session limit for bounce submissions, tiny
    <100B queue files instead of multi-KB, detect loops before accepting
    mail, reduce traffic on imsg channels by killing enormous struct
    submit_status.

--- usr.sbin/smtpd/runner.c
+++ usr.sbin/smtpd/runner.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: runner.c,v 1.86 2010/04/22 12:56:33 jacekm Exp $	*/
+/*	$OpenBSD: runner.c,v 1.87 2010/05/19 20:57:10 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>

fbe0e6bb4297755f368cda5f5f808982c8c7b0dd
nicm <nicm@openbsd.org>
2010-06-29 22:41:57 +0000

    Bye-bye acu which even UUCP-using naddy agrees "was obsolete 20 years ago" :-).

--- usr.bin/tip/aculib/Attic/biz22.c
+++ usr.bin/tip/aculib/Attic/biz22.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: biz22.c,v 1.13 2006/03/17 19:17:13 moritz Exp $	*/
+/*	$OpenBSD: biz22.c,v 1.14 2009/10/27 23:59:45 deraadt Exp $	*/
 /*	$NetBSD: biz22.c,v 1.6 1997/02/11 09:24:11 mrg Exp $	*/
 
 /*

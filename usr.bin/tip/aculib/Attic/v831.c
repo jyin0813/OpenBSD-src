743580081a012adf669a6a23d7ea425251d6eeaf
nicm <nicm@openbsd.org>
2010-06-29 22:41:57 +0000

    Bye-bye acu which even UUCP-using naddy agrees "was obsolete 20 years ago" :-).

--- usr.bin/tip/aculib/Attic/v831.c
+++ usr.bin/tip/aculib/Attic/v831.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: v831.c,v 1.11 2006/03/17 19:17:13 moritz Exp $	*/
+/*	$OpenBSD: v831.c,v 1.12 2009/10/27 23:59:45 deraadt Exp $	*/
 /*	$NetBSD: v831.c,v 1.5 1996/12/29 10:42:01 cgd Exp $	*/
 
 /*

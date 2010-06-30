6d2c3ab040b42db060e8732e8ff6ef05747db56e
reyk <reyk@openbsd.org>
2010-06-30 01:50:16 +0000

    Replace the hand-crafted Diffie-Hellman implementation in isakmpd with the smaller implementation from iked that is using libcrypto instead. This allows to remove a lot of code (which is always good), get rid of some custom crypto code by using libcrypto, theoretically adds support for many new MODP and EC2N/ECP modes (but it is not configurable yet), and allows to share the dh.c/dh.h code in different codebases (it is identical in isakmpd and iked, but could also be used elsewhere).
    ok deraadt@

--- sbin/isakmpd/Attic/math_group.h
+++ sbin/isakmpd/Attic/math_group.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: math_group.h,v 1.11 2006/05/04 14:37:51 djm Exp $	 */
+/* $OpenBSD: math_group.h,v 1.12 2006/06/02 19:35:55 hshoexer Exp $	 */
 /* $EOM: math_group.h,v 1.7 1999/04/17 23:20:40 niklas Exp $	 */
 
 /*

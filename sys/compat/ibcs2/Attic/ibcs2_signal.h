2ee61c65d00d739ae2af3903c68451f5711587b3
tedu <tedu@openbsd.org>
2010-07-01 23:30:27 +0000

    another day, another compat gets removed.  today is ibcs2's turn

--- sys/compat/ibcs2/Attic/ibcs2_signal.h
+++ sys/compat/ibcs2/Attic/ibcs2_signal.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ibcs2_signal.h,v 1.5 2002/03/14 01:26:50 millert Exp $	*/
+/*	$OpenBSD: ibcs2_signal.h,v 1.6 2009/06/01 20:11:38 weingart Exp $	*/
 /*	$NetBSD: ibcs2_signal.h,v 1.8 1996/05/03 17:05:28 christos Exp $	*/
 
 /*

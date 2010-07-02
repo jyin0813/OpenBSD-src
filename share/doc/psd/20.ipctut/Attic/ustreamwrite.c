66688f8dd2a6de598c4ae6ef0c0f0d97e96c4fae
tedu <tedu@openbsd.org>
2010-07-02 02:04:10 +0000

    remove old documentation

--- share/doc/psd/20.ipctut/Attic/ustreamwrite.c
+++ share/doc/psd/20.ipctut/Attic/ustreamwrite.c
@@ -1,4 +1,4 @@
-.\"	$OpenBSD: ustreamwrite.c,v 1.2 2001/02/03 08:15:05 niklas Exp $
+.\"	$OpenBSD: ustreamwrite.c,v 1.3 2003/06/02 23:30:10 millert Exp $
 .\"
 .\" Copyright (c) 1986, 1993
 .\"	The Regents of the University of California.  All rights reserved.

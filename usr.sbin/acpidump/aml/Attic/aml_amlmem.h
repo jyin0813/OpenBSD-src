05c61cb7f792a17d85ac1e0d11f8526becadd225
marco <marco@openbsd.org>
2010-08-05 23:33:12 +0000

    die aml parser die
    ok deraadt

--- usr.sbin/acpidump/aml/Attic/aml_amlmem.h
+++ usr.sbin/acpidump/aml/Attic/aml_amlmem.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: aml_amlmem.h,v 1.1 2005/06/02 20:09:39 tholo Exp $	*/
 /*-
  * Copyright (c) 1999 Mitsuru IWASAKI <iwasaki@FreeBSD.org>
  * All rights reserved.
@@ -24,7 +24,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: aml_amlmem.h,v 1.12 2000/08/08 14:12:05 iwasaki Exp $
+ *	$Id: aml_amlmem.h,v 1.1 2005/06/02 20:09:39 tholo Exp $
  *	$FreeBSD: src/usr.sbin/acpi/amldb/aml/aml_amlmem.h,v 1.2 2000/11/09 06:24:45 iwasaki Exp $
  */
 

d3a890ea0c024515f5016bdbbe1425905c219cde
pirofti <pirofti@openbsd.org>
2010-09-24 00:59:35 +0000

    Kill tn3270.
    Okay deraadt@, millert@ and all around general approval.

--- usr.bin/tn3270/sys_curses/Attic/terminal.h
+++ usr.bin/tn3270/sys_curses/Attic/terminal.h
@@ -1,4 +1,4 @@
-/* *	$OpenBSD: terminal.h,v 1.2 1996/06/26 05:41:32 deraadt Exp $*/
+/* *	$OpenBSD: terminal.h,v 1.3 2003/06/03 02:56:19 millert Exp $*/
 /*-
  * Copyright (c) 1988 The Regents of the University of California.
  * All rights reserved.

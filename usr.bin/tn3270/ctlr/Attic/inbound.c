f308ebd61489cd334712e20a73959337fa54c11e
pirofti <pirofti@openbsd.org>
2010-09-24 00:59:33 +0000

    Kill tn3270.
    Okay deraadt@, millert@ and all around general approval.

--- usr.bin/tn3270/ctlr/Attic/inbound.c
+++ usr.bin/tn3270/ctlr/Attic/inbound.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: inbound.c,v 1.6 2003/06/03 02:56:19 millert Exp $	*/
+/*	$OpenBSD: inbound.c,v 1.7 2009/10/27 23:59:45 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1988 The Regents of the University of California.

8b780254c103f2f222590d85ebddab0f0767edf4
pirofti <pirofti@openbsd.org>
2010-09-24 00:59:36 +0000

    Kill tn3270.
    Okay deraadt@, millert@ and all around general approval.

--- usr.bin/tn3270/tools/mkastosc/Attic/mkastosc.c
+++ usr.bin/tn3270/tools/mkastosc/Attic/mkastosc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mkastosc.c,v 1.4 2007/10/17 20:10:44 chl Exp $	*/
+/*	$OpenBSD: mkastosc.c,v 1.5 2009/10/27 23:59:45 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1988 The Regents of the University of California.

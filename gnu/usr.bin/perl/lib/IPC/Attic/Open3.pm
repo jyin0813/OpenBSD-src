8e332354c28d7693d9e5d5896e957d12d1d52e9d
millert <millert@openbsd.org>
2010-09-24 20:59:41 +0000

    remove files no longer in perl 5.12.2 (or that have moved)

--- gnu/usr.bin/perl/lib/IPC/Attic/Open3.pm
+++ gnu/usr.bin/perl/lib/IPC/Attic/Open3.pm
@@ -121,7 +121,7 @@ The order of arguments differs from that of open2().
 # allow fd numbers to be used, by Frank Tobin
 # allow '-' as command (c.f. open "-|"), by Adam Spiers <perl@adamspiers.org>
 #
-# $Id: open3.pl,v 1.1 1993/11/23 06:26:15 marc Exp $
+# $Id: Open3.pm,v 1.11 2009/10/12 18:24:41 millert Exp $
 #
 # usage: $pid = open3('wtr', 'rdr', 'err' 'some cmd and args', 'optarg', ...);
 #

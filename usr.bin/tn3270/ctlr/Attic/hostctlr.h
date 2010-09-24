381ab2ea6985f3cb91a79e62afdd6ebca6272a44
pirofti <pirofti@openbsd.org>
2010-09-24 00:59:33 +0000

    Kill tn3270.
    Okay deraadt@, millert@ and all around general approval.

--- usr.bin/tn3270/ctlr/Attic/hostctlr.h
+++ usr.bin/tn3270/ctlr/Attic/hostctlr.h
@@ -1,4 +1,4 @@
-/* *	$OpenBSD: hostctlr.h,v 1.2 1996/06/26 05:41:18 deraadt Exp $*/
+/* *	$OpenBSD: hostctlr.h,v 1.3 2003/06/03 02:56:19 millert Exp $*/
 /*-
  * Copyright (c) 1988 The Regents of the University of California.
  * All rights reserved.

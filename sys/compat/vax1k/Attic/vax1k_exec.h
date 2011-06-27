180ceadd17d8e65a49516ed9949370c19cba83a0
tedu <tedu@openbsd.org>
2011-06-27 09:28:11 +0000

    compat_vax1k goes to the attic

--- sys/compat/vax1k/Attic/vax1k_exec.h
+++ sys/compat/vax1k/Attic/vax1k_exec.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vax1k_exec.h,v 1.2 2002/03/14 01:26:51 millert Exp $	*/
+/*	$OpenBSD: vax1k_exec.h,v 1.3 2008/06/26 05:42:14 ray Exp $	*/
 /*	$NetBSD: vax1k_exec.h,v 1.1 1998/08/21 13:25:47 ragge Exp $	*/
 
 /*-

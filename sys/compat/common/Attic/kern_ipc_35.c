dce3e4994ec7aaf0ef42fc2dd147af0da767955a
tedu <tedu@openbsd.org>
2011-06-30 19:45:00 +0000

    remove compat_35 files

--- sys/compat/common/Attic/kern_ipc_35.c
+++ sys/compat/common/Attic/kern_ipc_35.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_ipc_35.c,v 1.3 2004/07/15 11:00:12 millert Exp $	*/
+/*	$OpenBSD: kern_ipc_35.c,v 1.4 2011/01/03 23:08:07 guenther Exp $	*/
 
 /*
  * Copyright (c) 2004 Todd C. Miller <Todd.Miller@courtesan.com>

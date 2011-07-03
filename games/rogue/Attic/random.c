7df8250e20ff9d716387327627ad8d554c12f501
martynas <martynas@openbsd.org>
2011-07-03 11:12:15 +0000

    Remove rogue.  Its license is not acceptable for our tree;  been unlinked from the builds for 8 years.  I've been talking with the author 2 years ago, he didn't release a proper license.  It's clear that the issue won't be resolved.  OK tedu@.

--- games/rogue/Attic/random.c
+++ games/rogue/Attic/random.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: random.c,v 1.6 2006/03/08 09:44:35 otto Exp $	*/
+/*	$OpenBSD: random.c,v 1.7 2009/10/27 23:59:26 deraadt Exp $	*/
 /*	$NetBSD: random.c,v 1.3 1995/04/22 10:28:06 cgd Exp $	*/
 
 /*

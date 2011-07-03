db8105f818c49c9f0bb136d36456c5bd957210b9
martynas <martynas@openbsd.org>
2011-07-03 11:12:15 +0000

    Remove rogue.  Its license is not acceptable for our tree;  been unlinked from the builds for 8 years.  I've been talking with the author 2 years ago, he didn't release a proper license.  It's clear that the issue won't be resolved.  OK tedu@.

--- games/rogue/Attic/object.c
+++ games/rogue/Attic/object.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: object.c,v 1.10 2006/03/08 09:44:35 otto Exp $	*/
+/*	$OpenBSD: object.c,v 1.11 2009/10/27 23:59:26 deraadt Exp $	*/
 /*	$NetBSD: object.c,v 1.3 1995/04/22 10:27:50 cgd Exp $	*/
 
 /*

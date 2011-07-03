a2bc816bbe6aa2104312b36f039ebaa792a7b741
martynas <martynas@openbsd.org>
2011-07-03 11:12:15 +0000

    Remove rogue.  Its license is not acceptable for our tree;  been unlinked from the builds for 8 years.  I've been talking with the author 2 years ago, he didn't release a proper license.  It's clear that the issue won't be resolved.  OK tedu@.

--- games/rogue/Attic/trap.c
+++ games/rogue/Attic/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.7 2006/03/08 09:44:35 otto Exp $	*/
+/*	$OpenBSD: trap.c,v 1.8 2009/10/27 23:59:26 deraadt Exp $	*/
 /*	$NetBSD: trap.c,v 1.3 1995/04/22 10:28:35 cgd Exp $	*/
 
 /*

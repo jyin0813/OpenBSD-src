8afc2af9e4fcdb5fb723bfb2950d7839df3a1f58
miod <miod@openbsd.org>
2011-07-07 00:32:58 +0000

    Remove support for non-microVAX, non-VAXstation, non-VXT hardware in the VAX port. This means, 11/7xx, VAX6000 and VAX8x00 systems, as well as SBI, CMI, BI, XMI and MASSBUS drivers. Most of these files were not being compiled or kept in compilable state over the years anyway.

--- sys/arch/vax/bi/Attic/bi.c
+++ sys/arch/vax/bi/Attic/bi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bi.c,v 1.7 2004/07/07 23:10:43 deraadt Exp $ */
+/*	$OpenBSD: bi.c,v 1.8 2008/05/15 22:17:08 brad Exp $ */
 /*	$NetBSD: bi.c,v 1.17 2001/11/13 12:51:34 lukem Exp $ */
 /*
  * Copyright (c) 1996 Ludd, University of Lule}, Sweden.

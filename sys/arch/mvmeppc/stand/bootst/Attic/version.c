c8315a26e1f67b1fe8623fbb740f7888025ca37a
deraadt <deraadt@openbsd.org>
2011-07-08 01:16:43 +0000

    remove mvmeppc; it is really rough shape.  ok drahn miod

--- sys/arch/mvmeppc/stand/bootst/Attic/version.c
+++ sys/arch/mvmeppc/stand/bootst/Attic/version.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD: version.c,v 1.2 1998/12/15 06:09:51 smurph Exp $ */
+/*	$OpenBSD: version.c,v 1.1 2001/06/26 21:58:00 smurph Exp $ */
 
 /*
  * make a random change to this file when you want the bootblock
  * revision to increase. like change this x to an q, or something.
  */
 
-char *version = "$Revision: 1.2 $";
+char *version = "$Revision: 1.1 $";

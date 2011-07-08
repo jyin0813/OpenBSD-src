0a7150873bacbab1061d2c508e2c0cccaa46a2b1
deraadt <deraadt@openbsd.org>
2011-07-08 01:16:43 +0000

    remove mvmeppc; it is really rough shape.  ok drahn miod

--- sys/arch/mvmeppc/mvmeppc/Attic/ppc1_machdep.c
+++ sys/arch/mvmeppc/mvmeppc/Attic/ppc1_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ppc1_machdep.c,v 1.17 2007/11/04 13:43:39 martin Exp $	*/
+/*	$OpenBSD: ppc1_machdep.c,v 1.18 2008/01/23 16:37:57 jsing Exp $	*/
 /*	$NetBSD: ofw_machdep.c,v 1.1 1996/09/30 16:34:50 ws Exp $	*/
 
 /*

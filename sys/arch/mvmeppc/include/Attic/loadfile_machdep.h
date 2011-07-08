60ae6c3b460ad79c61705229626223faaafc5b9f
deraadt <deraadt@openbsd.org>
2011-07-08 01:16:42 +0000

    remove mvmeppc; it is really rough shape.  ok drahn miod

--- sys/arch/mvmeppc/include/Attic/loadfile_machdep.h
+++ sys/arch/mvmeppc/include/Attic/loadfile_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: loadfile_machdep.h,v 1.1 2004/01/24 22:15:32 miod Exp $	*/
+/*	$OpenBSD: loadfile_machdep.h,v 1.2 2008/06/26 05:42:12 ray Exp $	*/
 /*	$NetBSD: loadfile_machdep.h,v 1.1 1999/04/29 03:17:12 tsubai Exp $	*/
 
 /*-

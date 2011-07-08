28f81bd3941372cc8bae8d70d78a17bedf20d85c
deraadt <deraadt@openbsd.org>
2011-07-08 03:00:59 +0000

    timed's time is up.  use ntpd(8).  Even our own fossil developers switched a while back. ok miod, kettenis

--- usr.sbin/timed/timed/Attic/candidate.c
+++ usr.sbin/timed/timed/Attic/candidate.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: candidate.c,v 1.7 2009/10/27 23:59:57 deraadt Exp $	*/
+/*	$OpenBSD: candidate.c,v 1.8 2011/03/19 23:40:11 okan Exp $	*/
 
 /*-
  * Copyright (c) 1985, 1993 The Regents of the University of California.

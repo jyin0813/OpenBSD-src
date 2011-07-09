2bb5b527280b5072922a1f03df042c8193cee7e7
deraadt <deraadt@openbsd.org>
2011-07-09 00:07:16 +0000

    extern.h no longer needed

--- bin/date/Attic/extern.h
+++ bin/date/Attic/extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: extern.h,v 1.3 2002/02/16 21:27:06 millert Exp $	*/
+/*	$OpenBSD: extern.h,v 1.4 2003/06/02 23:32:07 millert Exp $	*/
 /*	$NetBSD: extern.h,v 1.3 1995/03/21 09:03:52 cgd Exp $	*/
 
 /*-

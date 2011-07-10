5303c3a014492d61ce3da83737f0473aa3f7fa89
deraadt <deraadt@openbsd.org>
2011-07-10 23:31:40 +0000

    Move the vax/boot code to vax/stand.  A cvs 'mv' with de-tagging has been done behind the scenes to retain history.  Having all the bootblocks in the same architecture sub-directory satisfies several principles. ok miod

--- sys/arch/vax/boot/common/Attic/vaxstand.h
+++ sys/arch/vax/boot/common/Attic/vaxstand.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vaxstand.h,v 1.2 2011/03/13 00:13:53 deraadt Exp $ */
+/*	$OpenBSD: vaxstand.h,v 1.3 2011/07/06 18:32:59 miod Exp $ */
 /*	$NetBSD: vaxstand.h,v 1.1 1999/03/06 16:36:05 ragge Exp $ */
 /*
  * Copyright (c) 1994 Ludd, University of Lule}, Sweden.

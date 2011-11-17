768b1241587047f2dfe35dbdf4affc9b01e8a522
deraadt <deraadt@openbsd.org>
2011-11-17 03:50:19 +0000

    Make userret() MI.  On architectures which jammed stuff into it in the past, pull that code out seperately. ok guenther miod

--- sys/arch/sh/include/Attic/userret.h
+++ sys/arch/sh/include/Attic/userret.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: userret.h,v 1.1.1.1 2006/10/06 21:02:55 miod Exp $	*/
+/*	$OpenBSD: userret.h,v 1.2 2007/05/14 07:05:49 art Exp $	*/
 /*	$NetBSD: userret.h,v 1.9 2006/02/16 20:17:15 perry Exp $	*/
 
 /*

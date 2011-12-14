5affe914c137563caf368c7e11237eeb78cd7240
eric <eric@openbsd.org>
2011-12-14 05:09:53 +0000

    merge map_backend.c into map.c
    ok gilles@

--- usr.sbin/smtpd/Attic/map_backend.c
+++ usr.sbin/smtpd/Attic/map_backend.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: map_backend.c,v 1.6 2011/05/21 18:43:08 gilles Exp $	*/
+/*	$OpenBSD: map_backend.c,v 1.7 2011/08/30 11:19:51 chl Exp $	*/
 
 /*
  * Copyright (c) 2010 Gilles Chehade <gilles@openbsd.org>

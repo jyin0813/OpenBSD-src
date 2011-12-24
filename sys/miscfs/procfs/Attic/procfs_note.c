707a2622e7c6de1eca8d0605d5b041f2ba5e8a67
guenther <guenther@openbsd.org>
2011-12-24 11:34:20 +0000

    Remove the ability to ptrace via procfs, as no one is using it, so fixing and extending it to support rthreads would be a waste of time.  Also, don't show rthreads in the process listing in procfs.
    Reassurances that compat_linux doesn't need this from ajacoutot@
    ok kettenis@

--- sys/miscfs/procfs/Attic/procfs_note.c
+++ sys/miscfs/procfs/Attic/procfs_note.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: procfs_note.c,v 1.3 2003/06/02 23:28:11 millert Exp $	*/
+/*	$OpenBSD: procfs_note.c,v 1.4 2007/06/18 08:30:07 jasper Exp $	*/
 /*	$NetBSD: procfs_note.c,v 1.8 1994/06/29 06:34:53 cgd Exp $	*/
 
 /*

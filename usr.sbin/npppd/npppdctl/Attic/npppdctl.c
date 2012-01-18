7e48d3362c29f777a535e946e5c998b9b0fe8dfd
yasuoka <yasuoka@openbsd.org>
2012-01-18 10:13:04 +0000

    Replace npppdctl(8) by new npppctl(8).  npppctl was written from scratch, it uses parser.c derived from ikectl(8) to have OpenBSD's fashion.  This includes related changes listed below: - changed npppd control IPC heavyly. - support IPv6 as tunnel source address. - deleted support changing the configuration of npppd_ctl on running.   Because it is not so needed but it requires privilege operations. - refactors.
    man page helps from jmc.  tested by sebastia.
    ok deraadt sebastia sthen

--- usr.sbin/npppd/npppdctl/Attic/npppdctl.c
+++ usr.sbin/npppd/npppdctl/Attic/npppdctl.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: npppdctl.c,v 1.5 2010/07/31 09:33:09 yasuoka Exp $ */
+/* $OpenBSD: npppdctl.c,v 1.6 2010/09/22 00:32:48 jsg Exp $ */
 
 /*-
  * Copyright (c) 2009 Internet Initiative Japan Inc.
@@ -25,7 +25,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-/* $Id: npppdctl.c,v 1.5 2010/07/31 09:33:09 yasuoka Exp $ */
+/* $Id: npppdctl.c,v 1.6 2010/09/22 00:32:48 jsg Exp $ */
 #include <sys/types.h>
 #include <sys/param.h>
 #include <sys/socket.h>

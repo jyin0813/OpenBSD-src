0b97582ba4d8d9a27d670fb5618425ac26f28470
pirofti <pirofti@openbsd.org>
2012-04-12 17:51:12 +0000

    tedu(tm) pcc.
    Message sent. Okay deraadt@.

--- usr.bin/pcc/ccom/Attic/stabs.c
+++ usr.bin/pcc/ccom/Attic/stabs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: stabs.c,v 1.7 2008/04/11 20:45:52 stefan Exp $	*/
+/*	$OpenBSD: stabs.c,v 1.8 2008/08/17 18:40:13 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).

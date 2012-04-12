5e7aeffe3d16731b1b6b2bc322c2d168697a963a
pirofti <pirofti@openbsd.org>
2012-04-12 17:51:13 +0000

    tedu(tm) pcc.
    Message sent. Okay deraadt@.

--- usr.bin/pcc/nova/Attic/macdefs.h
+++ usr.bin/pcc/nova/Attic/macdefs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: macdefs.h,v 1.1 2007/10/07 17:58:51 otto Exp $	*/
+/*	$OpenBSD: macdefs.h,v 1.2 2007/12/22 13:13:27 stefan Exp $	*/
 /*
  * Copyright (c) 2006 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
